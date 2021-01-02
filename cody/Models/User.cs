ea85e48b454c2cd96927eb1abd49ac9b962deb14
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-02 01:48:01 +0100

    Create user model

---
+++ cody/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace cody.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
