d4bc89ed987810c19d1a2f7b29f3989a45f6ea37
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-02 01:48:26 +0100

    Create user controller

---
+++ cody/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using cody.Contexts;
+using cody.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cody.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly CodyContext _context;
+
+        public UserController(CodyContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult TryLoginAsync([FromQuery] User inUser)
+        {
+            var maybeUser =
+                from user in _context.Users
+                where
+                    user.Email == inUser.Email &&
+                    Password.AreEqual(inUser.Password, user.Password)
+                select user;
+
+            var userExists = maybeUser.Any();
+            if (userExists)
+                return Ok();
+
+            return ValidationProblem();
+        }
+    }
+}
