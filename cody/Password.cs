d6f0b00c8db72c8e05fe3c08b9e6301b48943793
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-02 01:47:53 +0100

    Provide an interface to hash and compare passwords

---
+++ cody/Password.cs
@@ -0,0 +1,17 @@
+﻿using BCryptAlgorithm = BCrypt.Net.BCrypt;
+
+namespace cody
+{
+    public class Password
+    {
+        public static string CreateFrom(string plainText)
+        {
+            return BCryptAlgorithm.HashPassword(plainText);
+        }
+
+        public static bool AreEqual(string plainTextPassword, string hashedPassword)
+        {
+            return BCryptAlgorithm.Verify(plainTextPassword, hashedPassword);
+        }
+    }
+}
