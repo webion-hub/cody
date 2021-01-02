6ac82c04a9f751da15f24710e262a6768696cb4f
matteo2437 <matteo.budriesi@gmail.com>
2021-01-02 15:44:12 +0100

    Introduce sidebar

--- cody/ClientApp/src/components/nav_menu.js
+++ cody/ClientApp/src/components/nav_menu.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles } from '@material-ui/core';
-import { MenuRounded, PersonRounded } from '@material-ui/icons';
+import {Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Fab } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { Tooltip } from '@material-ui/core';
 
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import ClassRoundedIcon from '@material-ui/icons/ClassRounded';
+
+function AppBarItem(props){
+  return (
+    <Box
+      mt={1}
+      ml={1}
+    >
+      <Tooltip 
+        title={          
+          <Typography 
+            variant="subtitle2" 
+          >
+            {props.title}
+          </Typography>
+        }
+        aria-label={props.title}
+        placement="right"              
+        arrow
+        interactive
+      >
+        <Fab color={props.color}>
+          {props.icon}
+        </Fab>
+      </Tooltip>  
+    </Box>
+  );
+}
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
   
   render () {
-    return null;
-    /*(
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuRounded/>
-          </IconButton>
-          <Typography variant="h6">
-            News
-          </Typography>
-          <IconButton color="inherit">
-            <PersonRounded/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    );*/
+    return (
+      <Box
+        position="fixed"
+      >
+        <Grid
+          container
+          direction="column"
+        >
+          <AppBarItem
+            title="Home"
+            color="primary"     
+            icon={<HomeRoundedIcon />}     
+          />              
+           <AppBarItem
+            title="Classi"
+            color="secondary"     
+            icon={<ClassRoundedIcon />}     
+          />           
+          <AppBarItem
+            title="Account"
+            color="secondary"     
+            icon={<AccountCircleRoundedIcon />}     
+          />                                 
+        </Grid>
+      </Box>
+    );
   }
 }
