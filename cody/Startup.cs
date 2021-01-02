9f8e1093722437dc705f517ea84ea03e6eed7ff6
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-02 01:46:20 +0100

    Add db connection

--- cody/Startup.cs
+++ cody/Startup.cs
@@ -1,7 +1,9 @@
+using cody.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,20 +19,25 @@ namespace cody
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
+            services.AddSpaStaticFiles(configuration => 
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddDbContext<CodyContext>(options =>
+            {
+                var connectionString =
+                    Configuration.GetConnectionString("CodyContext");
+
+                options.UseNpgsql(connectionString);
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +47,6 @@ namespace cody
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
