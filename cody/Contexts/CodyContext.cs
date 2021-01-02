467b4150ebb497e29ed110ae5778cbf13afc21de
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-02 01:48:14 +0100

    Create db context

---
+++ cody/Contexts/CodyContext.cs
@@ -0,0 +1,26 @@
+﻿using cody.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cody.Contexts
+{
+    public sealed class CodyContext : DbContext
+    {
+        public CodyContext(DbContextOptions<CodyContext> options)
+            : base(options)
+        { }
+
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<User>()
+                .Property(u => u.Password)
+                .HasConversion(
+                    pw => Password.CreateFrom(pw),
+                    pw => pw
+                );
+        }
+    }
+}
