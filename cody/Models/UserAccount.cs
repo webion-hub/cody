fe19e6bd4fad5094ca96282c916dd621175694d9
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-04 00:10:57 +0100

    Map user_account_detail

--- cody/Models/UserAccount.cs
+++ cody/Models/UserAccount.cs
@@ -3,14 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cody.Models
 {
-    [Table("useraccount")]
+    [Table("user_account")]
     public class UserAccount
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string Username { get; set; }
+
+
+        [EmailAddress(ErrorMessage = "Invalid email")]
         public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
+
+
+        public UserAccountDetail AccountDetail { get; set; }
     }
 }
---
+++ cody/Models/UserAccountDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cody.Models
+{
+    [Table("user_account_detail")]
+    public class UserAccountDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+
+        public int UserAccountId { get; set; }
+        public UserAccount UserAccount { get; set; }
+
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int? SchoolId { get; set; }
+    }
+}
