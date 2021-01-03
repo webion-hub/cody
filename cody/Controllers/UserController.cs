3b2cbeccdf5fa1aa92da0c5cb36ab4c3e0cea3d9
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-04 00:11:49 +0100

    Add user registration

--- cody/Controllers/UserController.cs
+++ cody/Controllers/UserController.cs
@@ -48,5 +48,38 @@ namespace cody.Controllers
 
             return Ok();
         }
+
+
+        /// <returns>
+        /// <see cref="StatusCodes.Status200OK"/> if the registration was successfull <br/>
+        /// <see cref="StatusCodes.Status400BadRequest"/> along with the reject reasons <br/>
+        /// </returns>
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> TryRegisterUser([FromBody] UserAccount account) 
+        {
+            var rejectReasons = new List<string>();
+            rejectReasons.AddRange(
+                MaybeGetAccountRejectReasons(account));
+
+            if (rejectReasons.Any())
+                return BadRequest(rejectReasons);
+
+            await _context.UsersAccounts.AddAsync(account);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        private IEnumerable<string> MaybeGetAccountRejectReasons(UserAccount account)
+        {
+            if (account.Password.Length is < 6 or > 16)
+                yield return "password";
+
+            if (account.Username.Length is < 4 or > 28)
+                yield return "username";
+
+            yield break;
+        }
     }
 }
