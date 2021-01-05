d944db030c93403f2969cede2d732683369a2500
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-05 17:19:29 +0100

    Add validation utilities

---
+++ cody/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cody
+{
+    public static class Validation
+    {
+        public static bool IsValidNameOrSurname(string name)
+        {
+            return Regex.IsMatch(
+                input: name, 
+                pattern: @"^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ'-]+([ A-Za-zÀ-ÿ][A-Za-zÀ-ÿ'-]+)*$", 
+                options: RegexOptions.None, 
+                matchTimeout: TimeSpan.FromMilliseconds(250)
+            );
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            return MailAddress.TryCreate(email, out var _);
+        }
+    }
+}
