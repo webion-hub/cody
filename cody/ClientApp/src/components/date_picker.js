b4d1fcdda0b1807716b0c42761c396f24fa7de0f
matteo2437 <matteo.budriesi@gmail.com>
2021-01-05 21:09:52 +0100

    add italian language

--- cody/ClientApp/src/components/date_picker.js
+++ cody/ClientApp/src/components/date_picker.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
+import itLocale from "date-fns/locale/it";
 
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { KeyboardDatePicker } from '@material-ui/pickers';
 
 export function DatePicker() {
     const newDate = new Date()
-    const [selectedDate, setSelectedDate] = React.useState(newDate.getDate());
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2000-01-01T21:11:54'));
   
     const handleDateChange = (date) => {
       setSelectedDate(date);
     };
   
     return (
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={itLocale}>
         <KeyboardDatePicker
           inputVariant="outlined"
           variant="inline"
