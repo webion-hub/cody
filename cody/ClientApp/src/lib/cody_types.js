3fb6ba671d001974d3075bb93830bee876863523
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-05 17:14:10 +0100

    Add common cody types

---
+++ cody/ClientApp/src/lib/cody_types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {object} UserAccount
+ * @property {string} username
+ * @property {string} email
+ * @property {string} password
+ * @property {UserAccountDetail} accountDetail
+ */
+
+/**
+ * @typedef {object} UserAccountDetail
+ * @property {string} name
+ * @property {string} surname
+ * @property {Date} birthDate
+ */
