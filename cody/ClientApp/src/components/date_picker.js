0c192ba12b3ae1972e7218f41228bdbb36e58792
matteo2437 <matteo.budriesi@gmail.com>
2021-01-06 15:52:47 +0100

    Add date picker callback

--- cody/ClientApp/src/components/date_picker.js
+++ cody/ClientApp/src/components/date_picker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
 import itLocale from "date-fns/locale/it";
@@ -6,15 +6,25 @@ import itLocale from "date-fns/locale/it";
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { KeyboardDatePicker } from '@material-ui/pickers';
 
-export function DatePicker() {
-    const newDate = new Date()
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2000-01-01T21:11:54'));
-  
-    const handleDateChange = (date) => {
-      setSelectedDate(date);
-    };
-  
-    return (
+export class DatePicker extends Component{
+  constructor(props){
+    super(props);
+    this.handleDateChange = this.handleDateChange.bind(this);
+
+    this.state = {
+      selectedDate: new Date('2000-01-01T21:11:54'),
+    }
+  }
+
+  handleDateChange = (date) => {
+    this.setState({selectedDate: date});
+
+    const {onChange} = this.props;
+    onChange(date);
+  };
+
+  render(){
+    return(
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={itLocale}>
         <KeyboardDatePicker
           inputVariant="outlined"
@@ -25,8 +35,8 @@ export function DatePicker() {
           id="date-picker"
           label="Data di nascita"
           fullWidth={true}
-          value={selectedDate}
-          onChange={handleDateChange}
+          value={this.props.value == "" ? this.state.selectedDate : this.props.value}
+          onChange={this.handleDateChange}
           KeyboardButtonProps={{
             'aria-label': 'Data di nascita',
           }}
@@ -34,3 +44,4 @@ export function DatePicker() {
       </MuiPickersUtilsProvider>
     );
   }
+}
