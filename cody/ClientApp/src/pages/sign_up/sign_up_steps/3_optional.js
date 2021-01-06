aee8c9c8cd2c43cbb566cb53865a619d7619d639
matteo2437 <matteo.budriesi@gmail.com>
2021-01-06 21:31:39 +0100

    implement add_school

--- cody/ClientApp/src/pages/sign_up/sign_up_steps/3_optional.js
+++ cody/ClientApp/src/pages/sign_up/sign_up_steps/3_optional.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Grid } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
@@ -7,110 +7,146 @@ import { Box } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
-import { makeStyles } from '@material-ui/core/styles';
+import { Link } from '@material-ui/core';
 
 import { SignUpBase } from '../sign_up_base'
+import { AddSchoolDialog } from './add_school'
 import { AddPhoto } from '../../../components/add_photo';
 import { Colors } from '../../../index'
 
 import { Step3 } from '../../../components/illustrations/step3';
 
-const useStyles = makeStyles({
-  '@global': {
-    '.MuiAutocomplete-option[data-focus="true"]': {
-        background: 'rgb(150, 150, 150, 0.1)'
-    },
-    '.MuiAutocomplete-option': {
-      background: 'rgb(255,255,255, 0.1)'
-  }
-  }
-});
+export class OptionalDatas extends Component{
+  constructor(props){
+    super(props);
+    this.handleOpen = this.handleOpen.bind(this);    
+    this.getSchool = this.getSchool.bind(this);
 
-export function OptionalDatas(props){
-  const classes = useStyles(props);
+    this.state = {
+      open: false,
+      school: {
+        name: '',
+        city: '',
+        country: '',
+      },
+    }
+  }
 
-  return (
-    <SignUpBase
-      image={<Step3 size={props.imageWidth}/>}
-      formWidth={props.formWidth}
-      margin={1}
-      bottomMargin={2}
-      items={[
-        <Box mb={3}>
-          <Paper 
-            elevation={3}
-            style={{
-              backgroundColor: Colors.background
-            }}
-          >
-            <Box p={2}>
-              <Grid
-                container
-                justify="center"
-              >
-                <AddPhoto
-                  size={150}
-                  iconSize={90}
-                  image="https://personale.unimore.it/foto.aspx?id=724"
-                />
-                <Box
-                  mt={2}
+  handleOpen(value){
+    this.setState({open: value});
+  }
+
+  getSchool(value){
+    this.setState({school: value});
+
+    const {school} = this.props;
+    school(value);
+  } 
+
+  render(){
+    return (
+      <SignUpBase
+        image={<Step3 size={this.props.imageWidth}/>}
+        formWidth={this.props.formWidth}
+        margin={1}
+        bottomMargin={2}
+        items={[
+          <Box mb={3}>
+            <Paper 
+              elevation={3}
+              style={{
+                backgroundColor: Colors.background
+              }}
+            >
+              <Box p={2}>
+                <Grid
+                  container
+                  justify="center"
                 >
-                  <Typography
-                    variant="subtitle1"
+                  <AddPhoto
+                    size={150}
+                    iconSize={90}
+                    image="https://personale.unimore.it/foto.aspx?id=724"
+                  />
+                  <Box
+                    mt={2}
                   >
-                    Aggiungi un'immagine profilo
-                  </Typography>
-                </Box>
-              </Grid>
-            </Box>
-          </Paper>
-        </Box>,
-        <Box>
-          <Typography
-            variant="body2"
-            color="secondary"
-          >
-            Sei uno studente?
-          </Typography>
-          <Box m={1}/>
-          <Autocomplete
-            id="school"
-            selectOnFocus            
-            handleHomeEndKeys
-            clearOnBlur
-            freeSolo
-            options={schoolsList}
-            getOptionLabel={(option) => (option.name + " - " + option.city)}
-            renderOption={(option) => (
-              <Grid
-                container
-                direction="column"
-              >
-                {option.name}
-                <Typography
-                  variant="caption"
+                    <Typography
+                      variant="subtitle1"
+                    >
+                      Aggiungi un'immagine profilo
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Box>
+            </Paper>
+          </Box>,
+          <Box>
+            <Typography
+              variant="body2"
+              color="secondary"
+            >
+              Sei uno studente?
+            </Typography>
+            <Box m={1}/>
+            <Autocomplete
+              id="school"
+              selectOnFocus            
+              handleHomeEndKeys
+              clearOnBlur
+              freeSolo
+              onChange={(event, value) => this.getSchool(value)}
+              options={schoolsList}
+              getOptionLabel={(option) => (option.name + " - " + option.city)}
+              renderOption={(option) => (
+                <Grid
+                  container
+                  direction="column"
                 >
-                  {option.country} - {option.city}
-                </Typography>                
-              </Grid>
-            )}
-            style={{ width: 300 }}
-            renderInput={
-              (params) => 
-              <TextField 
-                {...params} 
-                label="Istituto" 
-                variant="outlined" 
-                color="secondary"
-                fullWidth={true}
-              />
-            }
-          />
-        </Box>,
-      ]}
-    />
-  );
+                  {option.name}
+                  <Typography
+                    variant="caption"
+                  >
+                    {option.country} - {option.city}
+                  </Typography>                
+                </Grid>
+              )}
+              style={{ width: 300 }}
+              renderInput={
+                (params) => 
+                <TextField 
+                  {...params} 
+                  label="Istituto" 
+                  variant="outlined" 
+                  color="secondary"
+                  fullWidth={true}
+                />
+              }
+            />
+            <Link
+              style={{
+                fontSize: 12,
+                color: Colors.lightGrey
+              }}
+              component="button"
+              variant="caption"
+              onClick={() => {
+                this.handleOpen(true);
+              }}
+            >
+              Non trovi il tuo Istituto? Aggiungilo
+            </Link>
+            <AddSchoolDialog
+              open = {this.state.open}
+              onClose = {this.handleOpen}
+              imageWidth = {this.props.imageWidth} 
+              formWidth={this.props.formWidth}
+            />
+          </Box>,
+        ]}
+      />
+    );
+  }
 }
 
 
