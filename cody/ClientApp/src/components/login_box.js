85d67eb0a29b9dcef8160d0df455cad4fba09c87
matteo2437 <matteo.budriesi@gmail.com>
2021-01-07 15:46:55 +0100

    add navigation and move loading

--- cody/ClientApp/src/components/login_box.js
+++ cody/ClientApp/src/components/login_box.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import  { Redirect } from 'react-router-dom'
+
 import { TextField } from '@material-ui/core';
 import { Link } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
@@ -29,6 +31,7 @@ export class LoginBox extends Component{
       wrongUsername: false,
       wrongPassword: false,
       loading: false,
+      navigatoToHome: false,
     }
   }
 
@@ -48,142 +51,144 @@ export class LoginBox extends Component{
 
   render(){
     return(
-    <Box width={this.props.size}
-      justifyContent="center"
-      display="flex"
-      position="relative"
-    >
-      {
-        this.state.loading ? (
-          <Box
-            position="absolute"
-            top="50%"
-            left="50%"
-            style={{
-              transform: "translate(-50%, -50%)"
-            }}
-          >
-            <CircularProgress
-              color="secondary"
-            />
-          </Box> 
-        ) : (
-          null
-        )
-      }
-
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
+    <div>
+      <Box width={this.props.size}
+        justifyContent="center"
+        display="flex"
+        position="relative"
+      >     
         <Grid
           container
           direction="column"
           justify="center"
           alignItems="center"
         >
-          <TextField
-            id="username"
-            label="Username"
-            variant="outlined"
-            color="secondary"
-            fullWidth={true}
-            onChange={this._updateUsername}
-            error={this.state.wrongUsername}
-          />
-          <Box m={0.5}/>
-          <Password
-            label="Password"
-            labelWidth={70}
-            onChange={this._updatePassword}
-            error={this.state.wrongPassword}
-          />
           <Grid
             container
-            justify="flex-start"
+            direction="column"
+            justify="center"
+            alignItems="center"
           >
-            <FormControlLabel
-              value="end"
-              control={
-                <Checkbox
-                  color="secondary"
-                  size="small"
-                  style={{
-                    color: Colors.lightGrey
-                  }}
-                />
-              }
-              label={
-                <Typography
-                  variant="body2"
-                  style={{
-                    color: Colors.lightGrey
-                  }}
-                >
-                  Ricordami
-                </Typography>
-              }
-              labelPlacement="end"
-            />
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth={true}
-            endIcon={<AccountCircleRoundedIcon/>}
-            onClick={() => {
-              this.setState({wrongUsername: false});
-              this.setState({wrongUsername: false});
-
-              if(this.state.username != 0 && this.state.password != 0)
-              {
-                this.setState({loading: true});
-                User.tryLogin({
-                  username: this.state.username,
-                  password: this.state.password,
-                  onSuccess: _ => console.log('success'),
-                  onUserNotFound: _ => this.setState({wrongUsername: true}),
-                  onPasswordMismatch: _ => this.setState({wrongPassword: true}),
-                }).then(
-                  _=> this.setState({loading: false})
-                );
-              }
-              else
-              {
-                this.setState({wrongUsername: true});
-                this.setState({wrongPassword: true});
-              }
-            }}
-          >
-            Accedi
-          </Button>
-          <Box mt={1}>
-            <Link
-              style={{
-                fontSize: 12,
-              }}
-              component="button"
-              variant="body2"
+            <TextField
+              id="username"
+              label="Username"
+              variant="outlined"
               color="secondary"
+              fullWidth={true}
+              onChange={this._updateUsername}
+              error={this.state.wrongUsername}
+            />
+            <Box m={0.5}/>
+            <Password
+              label="Password"
+              labelWidth={70}
+              onChange={this._updatePassword}
+              error={this.state.wrongPassword}
+            />
+            <Grid
+              container
+              justify="flex-start"
+            >
+              <FormControlLabel
+                value="end"
+                control={
+                  <Checkbox
+                    color="secondary"
+                    size="small"
+                    style={{
+                      color: Colors.lightGrey
+                    }}
+                  />
+                }
+                label={
+                  <Typography
+                    variant="body2"
+                    style={{
+                      color: Colors.lightGrey
+                    }}
+                  >
+                    Ricordami
+                  </Typography>
+                }
+                labelPlacement="end"
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={this.state.loading}
+              fullWidth={true}
+              endIcon={<AccountCircleRoundedIcon/>}
               onClick={() => {
-                console.info("I'm a button.");
+                this.setState({wrongUsername: false});
+                this.setState({wrongUsername: false});
+
+                if(this.state.username != 0 && this.state.password != 0)
+                {
+                  this.setState({loading: true});
+                  User.tryLogin({
+                    username: this.state.username,
+                    password: this.state.password,
+                    onSuccess: _ => this.setState({navigatoToHome: true}),
+                    onUserNotFound: _ => this.setState({wrongUsername: true}),
+                    onPasswordMismatch: _ => this.setState({wrongPassword: true}),
+                  }).then(
+                    _=> {
+                      this.setState({loading: false})
+                    }
+                  );
+                }
+                else
+                {
+                  this.setState({wrongUsername: true});
+                  this.setState({wrongPassword: true});
+                }
               }}
             >
-              Password dimenticata?
-            </Link>
-          </Box>
+              {
+                this.state.loading ? 
+                (              
+                  <CircularProgress
+                    color="secondary"
+                    size={25}
+                    style={{
+                      position: "absolute"
+                    }}
+                  />   
+                ): null
+              }
+
+              Accedi
+            </Button>                
+            {
+              this.state.navigatoToHome ? (<Redirect to="/"/>) : null
+            }     
+            <Box mt={1}>
+              <Link
+                style={{
+                  fontSize: 12,
+                }}
+                component="button"
+                variant="body2"
+                color="secondary"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+              >
+                Password dimenticata?
+              </Link>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
     )
   }
 }
