9502a74ef31cfae07e139875d9237638f0c38455
matteo2437 <matteo.budriesi@gmail.com>
2021-01-08 21:08:15 +0100

    fix typo

--- cody/ClientApp/src/App.js
+++ cody/ClientApp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Layout } from './components/layout';
-import { Login } from './pages/login';
+import { Login } from './pages/login/login';
 import { SignUp } from './pages/sign_up/sign_up';
 
 import './custom.css'
