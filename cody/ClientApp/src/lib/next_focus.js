3f4b8bb44c5637442a361f6e3a7bfb832dad2b5a
matteo2437 <matteo.budriesi@gmail.com>
2021-01-08 19:41:52 +0100

    Add next focus class

---
+++ cody/ClientApp/src/lib/next_focus.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export class NextFocus{
+  constructor(elements){  
+    this.elements = elements;
+    this.elementsNumber = elements.length;
+
+    this.inputs = new Array(this.elementsNumber);
+    
+    this.setInput();
+    this.focusOn = this.focusOn.bind(this); 
+  }
+
+  setInput(){
+    for(var input = 0; input < this.elementsNumber; input++)
+      this.inputs[input] = React.createRef();
+  }
+
+  getInputNumber(elementSTR){
+    for(var element = 0; element < this.elementsNumber; element++){
+      if(this.elements[element] === elementSTR)
+        return element;
+    }
+  }
+
+  getInput(element){
+    const elementNumb = this.getInputNumber(element);
+    return this.inputs[elementNumb];
+  }
+
+  focusOn(element){
+    const elementNumb = this.getInputNumber(element);
+    this.inputs[elementNumb].current.focus();
+  }
+
+  removeFocus(e){
+    document.activeElement.blur();
+  }
+
+}
