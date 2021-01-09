ccd3499298e7437e13e3942a3a351ad226301182
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-09 04:37:56 +0100

    Add school controller

---
+++ cody/Controllers/SchoolController.cs
@@ -0,0 +1,51 @@
+﻿using cody.Contexts;
+using cody.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cody.Controllers
+{
+    [ApiController]
+    [Route("school")]
+    public class SchoolController : ControllerBase
+    {
+        private readonly CodyContext _context;
+
+        public SchoolController(CodyContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        [Route("create_new")]
+        public async Task<IActionResult> CreateNew([FromBody] SchoolAccount school)
+        {
+            var maybeExisting =
+                from s in _context.Schools
+                where
+                    s.Name == school.Name &&
+                    s.City == school.City &&
+                    s.Country == school.Country
+                select s;
+
+            if (maybeExisting.Any())
+                return BadRequest(maybeExisting.First().Id);
+
+            await _context.Schools.AddAsync(school);
+            await _context.SaveChangesAsync();
+            return Ok(school.Id);
+        }
+
+
+        [HttpGet]
+        [Route("get_all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Schools);
+        }
+    }
+}
