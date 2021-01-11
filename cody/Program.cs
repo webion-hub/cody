6129832b92f955c571d004b7f3367737aef3b37a
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-11 03:43:41 +0100

    Configure logging

--- cody/Program.cs
+++ cody/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,13 @@ namespace cody
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("cody_server.log")
+                .MinimumLevel.Information()
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,6 +29,7 @@ namespace cody
         {
             return Host
                 .CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
