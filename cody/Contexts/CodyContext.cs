a6928d3de20f271c14f07d6ec1687239c25a3879
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-11 03:46:19 +0100

    Add profile pictures to model

--- cody/Contexts/CodyContext.cs
+++ cody/Contexts/CodyContext.cs
@@ -12,6 +12,7 @@ namespace cody.Contexts
         public DbSet<UserAccount> UsersAccounts { get; set; }
         public DbSet<UserAccountDetail> UserDatails { get; set; }
         public DbSet<SchoolAccount> Schools { get; set; }
+        public DbSet<UserProfilePicture> ProfilePictures { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
