803ae877f2031e1bffd2b0b23c2a13543fc0b874
matteo2437 <matteo.budriesi@gmail.com>
2021-01-12 16:24:17 +0100

    introduce custom colors

---
+++ cody/ClientApp/src/lib/default_values/custom_colors.js
@@ -0,0 +1,8 @@
+export const Colors = {
+    primary: 'rgb(51, 102, 255)',
+    secondary: 'rgb(102, 153, 255)',
+    background: 'rgb(40, 40, 40)', 
+    lightGrey: 'rgba(255,255,255,0.6)',
+    errorRed: 'rgb(244,67,54)',
+    disable: 'rgba(100,100,100)'
+  }
