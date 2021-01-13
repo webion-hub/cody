30c4bf5928c85a0a9476dda79f6f16b79f3018e1
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-13 20:02:16 +0100

    Add 404 not found route

--- cody/ClientApp/src/App.js
+++ cody/ClientApp/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Layout } from './components/layout';
 import { Login } from './pages/login/login';
 import { SignUp } from './pages/sign_up/sign_up';
 
 import './custom.css'
+
 
 export default class App extends Component {
   static displayName = App.name;
@@ -13,8 +14,12 @@ export default class App extends Component {
     return (
       <Layout>
         <Router>
-          <Route exact path='/login' component={Login} />
-          <Route path='/sign-up' component={SignUp} />
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route path='/sign-up' component={SignUp} />
+            
+            <Route component={<div>Error 404</div>} />
+          </Switch>
         </Router>
       </Layout>
     );
