e863c2bf166a028523ac39e5923f9a6aceebb05c
matteo2437 <matteo.budriesi@gmail.com>
2021-01-14 19:26:22 +0100

    Create drawer sections

---
+++ cody/ClientApp/src/components/bars/sidebar/sections.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
+import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
+
+export const sections = [
+    [
+      {
+        label: 'Home',
+        icon: (<HomeRoundedIcon />),
+        href: "/"
+      },
+      {
+        label: 'Classi',
+        icon: (<SchoolRoundedIcon />)
+      },
+      {
+        label: 'Notifiche',
+        icon: (<NotificationsRoundedIcon />)
+      },
+    ],
+    [
+      {
+        label: 'Account',
+        icon: (
+          <Avatar 
+            alt="Profile Image" 
+            src="/images/bulb.jpeg" 
+            style={{
+              width: 40,
+              height: 40,
+            }}
+          />
+        ),
+        padding: 8,
+      },
+      {
+        label: 'Esci',
+        icon: (<ExitToAppRoundedIcon />),
+        href: "/login"
+      },
+    ],    
+  ]
