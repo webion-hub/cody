f2d3b315b021f7433b7153c4bce538b5a2d587e9
matteo2437 <matteo.budriesi@gmail.com>
2021-01-17 17:47:59 +0100

    add theme to app.js

--- cody/ClientApp/src/App.js
+++ cody/ClientApp/src/App.js
@@ -6,25 +6,47 @@ import { SignUp } from './pages/sign_up/sign_up';
 import { Error404Page } from './pages/error/error404_page';
 import { EmailValid } from './pages/email_valid';
 
+import { MuiThemeProvider } from '@material-ui/core/styles';  
+import { CssBaseline } from "@material-ui/core";
+import { useMediaQuery } from '@material-ui/core';
+
+import { ThemeController } from "./lib/default_values/themes/theme_controller";
+
 import './custom.css';
 
 
 export default class App extends Component {
   static displayName = App.name;
-
   render () {
     return (
-      <Layout>
-        <Router>
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route path='/sign-up' component={SignUp} />
-            <Route path='/email-valid' component={EmailValid} />
+      <Theme>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route path='/sign-up' component={SignUp} />
+              <Route path='/email-valid' component={EmailValid} />
 
-            <Route component={Error404Page} />
-          </Switch>
-        </Router>
-      </Layout>
+              <Route component={Error404Page} />
+            </Switch>
+          </Router>
+        </Layout>
+      </Theme>
     );
   }
+}
+
+function Theme(props){
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const themeMode = prefersDarkMode ? "dark" : "light";
+
+  const themeController = new ThemeController();
+  themeController.setThemeMode(themeMode);
+
+  return (
+    <MuiThemeProvider theme = {themeController.getTheme()}>
+      <CssBaseline />
+      {props.children}
+    </MuiThemeProvider>
+  );
 }
