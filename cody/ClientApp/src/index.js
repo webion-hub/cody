a7a7718746233cb0634380837ab86f34a9e101d1
matteo2437 <matteo.budriesi@gmail.com>
2021-01-17 17:47:41 +0100

    remove theme from index

--- cody/ClientApp/src/index.js
+++ cody/ClientApp/src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
-import { MuiThemeProvider } from '@material-ui/core/styles';  
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { MainTheme } from "./lib/default_values/themes/main_theme";
- 
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
-
-
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <MuiThemeProvider theme = { MainTheme }>
-      <CssBaseline />
-      <App />
-    </MuiThemeProvider>
+    <App />
   </BrowserRouter>,
   rootElement
 );
