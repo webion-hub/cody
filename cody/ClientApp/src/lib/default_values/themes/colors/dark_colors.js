105dc02b2f7c8bf7636d9255c810940a2823cb8e
matteo2437 <matteo.budriesi@gmail.com>
2021-01-17 17:47:05 +0100

    add dark colors

---
+++ cody/ClientApp/src/lib/default_values/themes/colors/dark_colors.js
@@ -0,0 +1,11 @@
+import { Colors } from "./main_colors"
+
+export const darkColors = {
+  primary: Colors.primary,
+  secondary: Colors.secondary,
+  tertiary: 'rgb(70, 70, 90)',
+  backgroundLight: 'rgb(55, 55, 60)', 
+  background: 'rgb(40, 40, 45)', 
+  backgroundDark: 'rgb(30, 30, 35)', 
+  appBar: 'rgb(45, 45, 55)',  
+}
