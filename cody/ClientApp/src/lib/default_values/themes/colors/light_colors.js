63daa604d7f0b76df94af13a03c41a459c3d44cd
matteo2437 <matteo.budriesi@gmail.com>
2021-01-17 17:47:12 +0100

    add light colors

---
+++ cody/ClientApp/src/lib/default_values/themes/colors/light_colors.js
@@ -0,0 +1,11 @@
+import { Colors } from "./main_colors"
+
+export const lightColors = {
+  primary: Colors.primary,
+  secondary: Colors.secondary,
+  tertiary: 'rgb(170, 170, 170)',
+  backgroundLight: 'rgb(250, 250, 250)', 
+  background: 'rgb(245, 245, 245)', 
+  backgroundDark: 'rgb(230, 230, 230)', 
+  appBar: 'rgb(245, 245, 245)',  
+}
