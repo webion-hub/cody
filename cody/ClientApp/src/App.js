06508380c6b6af3306d2c907bd5ea8712eb025c3
matteo2437 <matteo.budriesi@gmail.com>
2021-01-20 15:59:38 +0100

    add home route

--- cody/ClientApp/src/App.js
+++ cody/ClientApp/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Layout } from './components/layout';
+
 import { Login } from './pages/login/login';
 import { SignUp } from './pages/sign_up/sign_up';
-import { Error404Page } from './pages/error/error404_page';
+import { Error404Page } from './pages/error404_page';
 import { EmailValid } from './pages/email_valid';
+import { Home } from './pages/home/home';
 
 import { MuiThemeProvider } from '@material-ui/core/styles';  
 import { CssBaseline } from "@material-ui/core";
@@ -17,13 +19,15 @@ import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
+
   render () {
     return (
       <Theme>
         <Layout>
           <Router>
             <Switch>
-              <Route exact path='/login' component={Login} />
+              <Route exact path='/' component={Home} />
+              <Route path='/login' component={Login} />
               <Route path='/sign-up' component={SignUp} />
               <Route path='/email-valid' component={EmailValid} />
 
@@ -40,11 +44,16 @@ function Theme(props){
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const themeMode = prefersDarkMode ? "dark" : "light";
 
-  const themeController = new ThemeController();
-  themeController.setThemeMode(themeMode);
+  let themeController = new ThemeController();
+  let theme = themeController.getTheme();
 
+  useEffect(() => {
+    //execute before render
+    themeController.setThemeMode(themeMode);
+  }, []);
+  
   return (
-    <MuiThemeProvider theme = {themeController.getTheme()}>
+    <MuiThemeProvider theme = {theme}>
       <CssBaseline />
       {props.children}
     </MuiThemeProvider>
