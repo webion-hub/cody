13a5acaf2a9380e8c185ab0ced2bc0b94f0caf9b
matteo2437 <matteo.budriesi@gmail.com>
2021-01-20 15:58:32 +0100

    create coding languages list

---
+++ cody/ClientApp/src/lib/default_values/lists/coding_languages.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Angular } from '../../../components/icons/angular';
+import { C } from '../../../components/icons/c';
+import { Cpp } from '../../../components/icons/cpp';
+import { CSharp } from '../../../components/icons/csharp';
+import { Css } from '../../../components/icons/css';
+import { Html } from '../../../components/icons/html';
+import { JavaScript } from '../../../components/icons/java_script';
+import { Java } from '../../../components/icons/java';
+import { Python } from '../../../components/icons/python';
+import { ReactIcon } from '../../../components/icons/react';
+
+export const languages =[
+  {
+    id: 1,
+    icon: <Angular/>,
+    title: "Angular"
+  },
+  {
+    id: 2,
+    icon: <C/>,
+    title: "C"
+  },
+  {
+    id: 3,
+    icon: <Cpp/>,
+    title: "C++"
+  },
+  {
+    id: 4,
+    icon: <CSharp/>,
+    title: "C#"
+  },
+  {
+    id: 5,
+    icon: <Css/>,
+    title: "CSS"
+  },
+  {
+    id: 6,
+    icon: <Html/>,
+    title: "HTML"
+  },
+  {
+    id: 7,
+    icon: <JavaScript/>,
+    title: "JavaScript"
+  },
+  {
+    id: 8,
+    icon: <Java/>,
+    title: "Java"
+  },
+  {
+    id: 9,
+    icon: <Python/>,
+    title: "Python"
+  },
+  {
+    id: 10,
+    icon: <ReactIcon/>,
+    title: "React"
+  }
+]
