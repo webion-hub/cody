e30067cc3f60be9aa1a2e8ebd133dbfd683f8949
matteo2437 <matteo.budriesi@gmail.com>
2021-01-20 19:31:15 +0100

    replace id with key

--- cody/ClientApp/src/lib/default_values/lists/coding_languages.js
+++ cody/ClientApp/src/lib/default_values/lists/coding_languages.js
@@ -12,52 +12,52 @@ import { ReactIcon } from '../../../components/icons/react';
 
 export const languages =[
   {
-    id: 1,
+    key: 1,
     icon: <Angular/>,
     title: "Angular"
   },
   {
-    id: 2,
+    key: 2,
     icon: <C/>,
     title: "C"
   },
   {
-    id: 3,
+    key: 3,
     icon: <Cpp/>,
     title: "C++"
   },
   {
-    id: 4,
+    key: 4,
     icon: <CSharp/>,
     title: "C#"
   },
   {
-    id: 5,
+    key: 5,
     icon: <Css/>,
     title: "CSS"
   },
   {
-    id: 6,
+    key: 6,
     icon: <Html/>,
     title: "HTML"
   },
   {
-    id: 7,
+    key: 7,
     icon: <JavaScript/>,
     title: "JavaScript"
   },
   {
-    id: 8,
+    key: 8,
     icon: <Java/>,
     title: "Java"
   },
   {
-    id: 9,
+    key: 9,
     icon: <Python/>,
     title: "Python"
   },
   {
-    id: 10,
+    key: 10,
     icon: <ReactIcon/>,
     title: "React"
   }
