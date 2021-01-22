a7d0da242b14aee1c9eb518171028ef74456b3a3
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-22 21:45:30 +0100

    Add configuration extensions

---
+++ cody/Extensions/IConfigurationExtension.cs
@@ -0,0 +1,27 @@
+﻿using Cody.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Extensions
+{
+    public static class IConfigurationExtension
+    {
+        public static SftpConnection GetSftpConnection(this IConfiguration configuration)
+        {
+            return configuration
+                .GetSection("SftpConnection")
+                .Get<SftpConnection>();
+        }
+
+
+        public static EmailServiceInfo GetEmailServiceInfo(this IConfiguration configuration)
+        {
+            return configuration
+                .GetSection("EmailServiceInfo")
+                .Get<EmailServiceInfo>();
+        }
+    }
+}
