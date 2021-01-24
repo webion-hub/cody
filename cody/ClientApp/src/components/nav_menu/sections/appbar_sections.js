e2f4eb082ab0285fd232e6c8f888fc42f57eed33
matteo2437 <matteo.budriesi@gmail.com>
2021-01-24 18:33:29 +0100

    implement user avatar

--- cody/ClientApp/src/components/nav_menu/sections/appbar_sections.js
+++ cody/ClientApp/src/components/nav_menu/sections/appbar_sections.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Avatar } from '@material-ui/core';
 import { Hidden } from '@material-ui/core';
 import { Box } from '@material-ui/core';
 
 import { SearchBar } from '../../search_bar/search_bar';
 import { CompressedSearchBar } from '../../search_bar/compressed_search_bar';
+import { UserAvatar } from '../../user_avatar';
 
 import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded';
 
@@ -39,22 +39,9 @@ const centerAppBar =  (
 
 const rightAppBar = [
   {
-    label: 'Notifiche',
-    icon: (<NotificationsRoundedIcon />),
-  },
-  {
-    label: 'Account',
-    avatar: (
-      <Avatar 
-        alt="Profile Image" 
-        src="/images/bulb.jpeg" 
-        style={{
-          width: 36,
-          height: 36,
-        }}
-      />
-    ),
-    padding: 6,
+    tooltip: false,
+    element: (<UserAvatar/>),
+    showAlways: true,
   },
 ]
 
