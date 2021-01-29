249fd020b86b82e44a0068dd0074665efd755c36
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-29 17:29:13 +0100

    Add HttpRequest extension

---
+++ cody/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,26 @@
+﻿using Cody.Security;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Extensions
+{
+    public static class HttpRequestExtension
+    {
+        public static bool TryGetLoginCookies(this HttpRequest request, out int cookieId, out string token)
+        {
+            var cookies = new PersistentLoginCookies { 
+                Request = request,
+            };
+
+            token = cookies.Token;
+            cookieId = cookies.Id ?? 0;
+
+            return
+                cookies.IsIdValid &&
+                cookies.IsTokenValid;
+        }
+    }
+}
