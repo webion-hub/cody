d2e5f2bbed6bc6292c6d5a215008604bc910f445
matteo2437 <matteo.budriesi@gmail.com>
2021-01-29 21:45:36 +0100

    add class to scrollable chips array

--- cody/ClientApp/src/components/scrollable_chips_array.js
+++ cody/ClientApp/src/components/scrollable_chips_array.js
@@ -41,42 +41,44 @@ export function ScrollableChipsArray(props){
   }
 
   return(
-    <ScrollContainer>
-      <ul 
-        className={classes.root}
-      >
-        {
-          chipData.length === 0 ?
-            <Typography
-              variant="body2"
-            >
-              {props.emptyMessage}
-            </Typography>
-            :
-            chipData.map((data) => {
-              return (
-                <li key={data.key} className={classes.li}>
-                  <Chip
-                    clickable={props.clickables}
-                    color={props.color}
-                    onClick={props.getValue ? () => handleOnClick(data) : props.onClick} 
-                    icon={
-                      data.icon? (
-                        <div className={classes.icon}>
-                          {data.icon}
-                        </div>
-                      ) : null
-                    }
-                    label={data.title}
-                    onDelete={props.delete ? handleDelete(data) : null}
-                    className={classes.chip}
-                  />
-                </li>
-              );
-            })
-        }
-      </ul>
-    </ScrollContainer>
+    <div className="scrollableChipsArray">
+      <ScrollContainer>
+        <ul 
+          className={classes.root}
+        >
+          {
+            chipData.length === 0 ?
+              <Typography
+                variant="body2"
+              >
+                {props.emptyMessage}
+              </Typography>
+              :
+              chipData.map((data) => {
+                return (
+                  <li key={data.key} className={classes.li}>
+                    <Chip
+                      clickable={props.clickables}
+                      color={props.color}
+                      onClick={props.getValue ? () => handleOnClick(data) : props.onClick} 
+                      icon={
+                        data.icon? (
+                          <div className={classes.icon}>
+                            {data.icon}
+                          </div>
+                        ) : null
+                      }
+                      label={data.title}
+                      onDelete={props.delete ? handleDelete(data) : null}
+                      className={classes.chip}
+                    />
+                  </li>
+                );
+              })
+          }
+        </ul>
+      </ScrollContainer>
+    </div>
   );
 }
 
