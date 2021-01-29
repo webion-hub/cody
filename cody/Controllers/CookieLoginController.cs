9bb99a36cc859e6be3d88894154b8d16a55de2a1
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-29 17:31:23 +0100

    Refactor cookie login controller

--- cody/Controllers/CookieLoginController.cs
+++ cody/Controllers/CookieLoginController.cs
@@ -1,36 +1,25 @@
 ﻿using Cody.Contexts;
 using Cody.Extensions;
 using Cody.Models;
+using Cody.Security;
 using Cody.Services;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Cody.Controllers
 {
     [ApiController]
     [Route("user")]
+    [Authorize]
     public class CookieLoginController : ControllerBase
     {
         private readonly ILogger<CookieLoginController> _logger;
         private readonly CodyContext _dbContext;
         private readonly PersistentLoginCookieEmitterService _cookieEmitter;
-
-
-        private struct Cookies
-        {
-            public const string ID = "login_cookie_id";
-            public const string TOKEN = "login_cookie_token";
-        }
 
 
         public CookieLoginController(
@@ -44,8 +33,7 @@ namespace Cody.Controllers
         }
 
 
-        [HttpGet]
-        [Route("remember_me/{userId}")]
+        [HttpGet("remember_me/{userId}")]
         [Authorize]
         public async Task<IActionResult> TryRememberMe(int userId)
         {
@@ -61,37 +49,22 @@ namespace Cody.Controllers
         }
 
 
-        [HttpGet]
-        [Route("login_with_cookie")]
+        [HttpGet("login_with_cookie")]
+        [AllowAnonymous]
         public async Task<IActionResult> LoginWithCookie()
         {
-            if (!TryGetLoginCookies(out var cookieId, out var token))
+            if (!Request.TryGetLoginCookies(out var cookieId, out var token))
                 return BadRequest();
 
-            var maybeUser =
+            var user =
                 await _cookieEmitter.TryLoginAsync(cookieId, token);
 
-            if (maybeUser is null)
+            if (user is null)
                 return BadRequest();
 
-            await HttpContext.UserSignInAsync(maybeUser);
-            await GenerateUserLoginCookies(maybeUser);
+            await HttpContext.SignInAsync(user);
+            await GenerateUserLoginCookies(user);
             return Ok();
-        }
-
-
-        private bool TryGetLoginCookies(out int cookieId, out string token)
-        {
-            var cookieIdString = Request.Cookies[Cookies.ID];
-            token = Request.Cookies[Cookies.TOKEN];
-
-            if (!int.TryParse(cookieIdString, out cookieId))
-                return false;
-
-            if (string.IsNullOrWhiteSpace(token))
-                return false;
-
-            return true;
         }
 
 
@@ -100,13 +73,7 @@ namespace Cody.Controllers
             var (id, token) =
                 await _cookieEmitter.EmitPersistentLoginCookieForAsync(user);
 
-            Response.Cookies.Append(Cookies.ID, id.ToString());
-            Response.Cookies.Append(Cookies.TOKEN, token, new CookieOptions
-            {
-                HttpOnly = true,
-                Secure = true,
-                SameSite = SameSiteMode.Strict,
-            });
+            Response.SetLoginCookies(id, token);
         }
     }
 }
