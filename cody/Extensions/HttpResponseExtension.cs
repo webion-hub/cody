2cbae94be49c0c29f43c1aed42fc8a0a1ea7716a
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-01-29 17:29:21 +0100

    Add HttpResponse extension

---
+++ cody/Extensions/HttpResponseExtension.cs
@@ -0,0 +1,29 @@
+﻿using Cody.Security;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Extensions
+{
+    public static class HttpResponseExtension
+    {
+        public static void SetLoginCookies(this HttpResponse response, int id, string token)
+        {
+            var cookies = new PersistentLoginCookies {
+                Response = response,
+            };
+
+            cookies.Id = id;
+            cookies.Token = token;
+        }
+
+
+        public static void DeleteLoginCookies(this HttpResponse response)
+        {
+            response.Cookies.Delete(PersistentLoginCookies.ID);
+            response.Cookies.Delete(PersistentLoginCookies.TOKEN);
+        }
+    }
+}
