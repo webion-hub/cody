9bf36d6ffdf4e50fe2a75140745aa4da4266c367
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-02-05 22:21:43 +0100

    Create UserAccountInfo controller

---
+++ cody/Controllers/Helpers/UserAccountInfoProps.cs
@@ -0,0 +1,41 @@
+﻿using Cody.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers.Helpers
+{
+    public class UserAccountInfoProps
+    {
+        public const string Username = "username";
+        public const string Email = "email";
+        public const string Name = "name";
+        public const string Surname = "surname";
+        public const string BirthDate = "birthDate";
+
+
+        public static string GetPropFor(string prop, UserAccount user) => prop switch
+        {
+            Username  => user.Username,
+            Email     => user.Email,
+            Name      => user.AccountDetail.Name,
+            Surname   => user.AccountDetail.Surname,
+            BirthDate => user.AccountDetail.BirthDate.ToString(),
+
+            _ => null,
+        };
+
+        public static void SetPropFor(string prop, string value, UserAccount user)
+        {
+            switch (prop)
+            {
+                case Username:  user.Username = value;                                  break;
+                case Email:     user.Email = value;                                     break;
+                case Name:      user.AccountDetail.Name = value;                        break;
+                case Surname:   user.AccountDetail.Surname = value;                     break;
+                case BirthDate: user.AccountDetail.BirthDate = DateTime.Parse(value);   break;
+            }
+        }
+    }
+}
---
+++ cody/Controllers/UserAccountInfoController.cs
@@ -0,0 +1,64 @@
+﻿using Cody.Contexts;
+using Cody.Controllers.Helpers;
+using Cody.Extensions;
+using Cody.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers
+{
+    [ApiController]
+    [Route("user/info")]
+    [Authorize]
+    public class UserAccountInfoController : ControllerBase
+    {
+        private readonly ILogger<UserAccountInfoController> _logger;
+        private readonly CodyContext _dbContext;
+
+
+        public UserAccountInfoController(ILogger<UserAccountInfoController> logger, CodyContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Post([FromBody] List<string> getters)
+        {
+            var user = await HttpContext.GetLoggedUserFromAsync(_dbContext);
+            var result = getters.ToDictionary(
+                prop => prop,
+                prop => UserAccountInfoProps.GetPropFor(prop, user)
+            );
+
+            return Ok(result);
+        }
+
+
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> Put([FromBody] Dictionary<string, string> setters)
+        {
+            var user = 
+                await HttpContext.GetLoggedUserFromAsync(_dbContext);
+            
+            foreach (var (prop, value) in setters)
+            {
+                UserAccountInfoProps.SetPropFor(prop, value, user);
+            }
+
+            await _dbContext.SaveChangesAsync();
+            return Ok(true);
+        }
+    }
+}
