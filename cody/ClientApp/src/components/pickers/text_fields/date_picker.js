ae016302b2c01d5a0ccfaddc28b98b3bc95ff766
matteo2437 <matteo.budriesi@gmail.com>
2021-02-05 17:31:29 +0100

    add min and max date for picker and change date format

--- cody/ClientApp/src/components/pickers/text_fields/date_picker.js
+++ cody/ClientApp/src/components/pickers/text_fields/date_picker.js
@@ -4,6 +4,8 @@ import itLocale from "date-fns/locale/it";
 
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { KeyboardDatePicker } from '@material-ui/pickers';
+import { InputAdornment, TextField } from '@material-ui/core';
+import UndoRounded from '@material-ui/icons/UndoRounded';
 
 export class DatePicker extends Component{
   constructor(props){
@@ -26,12 +28,16 @@ export class DatePicker extends Component{
     return(
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={itLocale}>
         <KeyboardDatePicker
+          minDate={new Date('01/01/1920')}
+          maxDate={new Date()}
+          minDateMessage="La data non deve inferiore al minimo valore"
+          maxDateMessage="La data non deve superare il massimo valore"
+          error={this.props.error}
           inputVariant={this.props.variant}
           variant="inline"
           color="secondary"
-          format="MM/dd/yyyy"
+          format="dd/MM/yyyy"
           margin="normal"
-          id="date-picker"
           label="Data di nascita"
           fullWidth={true}
           value={this.state.selectedDate}
