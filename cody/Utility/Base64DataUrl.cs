5f41f55f6d3d7fd40408d8abfd32f18d9a73693c
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-02-08 03:09:57 +0100

    Handle base64 data urls

---
+++ cody/Utility/Base64DataUrl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cody.Utility
+{
+    public class Base64DataUrl
+    {
+        private string _base64Url;
+        public string Base64Url { 
+            get => _base64Url;
+            set {
+                _base64Url = value;
+                RefreshContentTypeAndData();
+            }
+        }
+
+        public string ContentType { get; private set; }
+        public string Data { get; private set; }
+
+        public Stream AsDecodedStream() => new MemoryStream(AsDecodedByteArray());
+        public byte[] AsDecodedByteArray() => Convert.FromBase64String(Data);
+
+
+        public Base64DataUrl(string base64Url)
+        {
+            Base64Url = base64Url;
+        }
+
+
+        protected virtual void RefreshContentTypeAndData()
+        {
+            var matchedGroups = MatchContentTypeAndData();
+
+            ContentType = matchedGroups["content_type"].Value;
+            Data = matchedGroups["data"].Value;
+        }
+
+        private GroupCollection MatchContentTypeAndData()
+        {
+            return Regex.Match(
+                input: Base64Url, 
+                pattern: @"data:(?<content_type>.+);base64,(?<data>.+)", 
+                options: RegexOptions.None,
+                matchTimeout: TimeSpan.FromMilliseconds(250)
+            ).Groups;
+        }
+    }
+}
---
+++ cody/Utility/Base64Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Utility
+{
+    public class Base64Image: Base64DataUrl
+    {
+        public Base64Image(string base64Url): base(base64Url) { }
+
+        protected override void RefreshContentTypeAndData()
+        {
+            base.RefreshContentTypeAndData();
+
+            if (!ContentType.StartsWith("image"))
+                throw new FormatException("The provided data url does not represent an image.");
+        }
+    }
+}
