7d0d61d1a9c6a82cdcd4a2c695a90d87f3bfc3d4
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-02-08 03:10:11 +0100

    Refactor using Base64Image class

--- cody/Controllers/Requests/ProfilePicturePutRequest.cs
+++ cody/Controllers/Requests/ProfilePicturePutRequest.cs
@@ -1,14 +1,10 @@
-﻿using ImageProcessor;
+﻿using Cody.Utility;
+using ImageProcessor;
 using ImageProcessor.Plugins.WebP.Imaging.Formats;
 using Microsoft.AspNetCore.Http;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Cody.Controllers.Requests
 {
@@ -46,18 +42,7 @@ namespace Cody.Controllers.Requests
 
         private Stream GetBase64Stream()
         {
-            var contents = RemoveBase64Url();
-            var decoded = Convert.FromBase64String(contents);
-
-            return new MemoryStream(decoded);
-        }
-
-        private string RemoveBase64Url()
-        {
-            return Regex
-                .Match(Base64, @"data:image/.+;base64,(.+)")
-                .Groups[1]
-                .Value;
+            return new Base64Image(Base64).AsDecodedStream();
         }
     }
 }
