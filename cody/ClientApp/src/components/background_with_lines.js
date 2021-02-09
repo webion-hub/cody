190602cbc69470f9d2ad1372a60d593cada13dba
matteo2437 <matteo.budriesi@gmail.com>
2021-02-09 13:42:57 +0100

    create background with lines component

---
+++ cody/ClientApp/src/components/background_with_lines.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	line: {
+		height: 80,
+		borderRadius: 40,
+		background: "rgba(0,0,0,0.1)",
+		margin: 10,
+	},
+	lines: {
+		[theme.breakpoints.up('sm')]: {
+			paddingRight: 48,
+    },
+	}
+}));
+
+export function BackgroundWithLines(props){
+  const classes = useStyles();
+
+	const lines = [];
+	for(var i=0; i < 10; i++){
+		lines.push(
+			<Grid
+				key={i}
+				container
+				direction="row"
+				justify="space-between"
+				className={classes.lines}
+			>
+				<Line/>
+				<Line/>
+			</Grid>
+		);
+	}
+
+  return(
+		<div
+			style={{
+				position: "absolute",
+				top: 0,
+				zIndex: -2,
+				width: "100%",
+				background: "linear-gradient(180deg, rgb(26 42 79 / 85%) 0%, rgb(23 36 63 / 45%) 50%, rgba(17, 26, 37, 0) 100%)"
+			}}
+		>
+			{
+				lines.map(line => line)
+			}
+		</div>
+  );
+}
+
+
+function Line(props){
+  const classes = useStyles();
+
+	const randomValue = (min, max) => {	
+		return min + Math.random() * (max - min);
+	}
+
+	return(
+		<div 
+			className={classes.line} 
+			style={{
+				maxWidth: `${randomValue(10, 45)}vw`,
+				width: "100%"
+			}}
+		/>
+	);
+}
