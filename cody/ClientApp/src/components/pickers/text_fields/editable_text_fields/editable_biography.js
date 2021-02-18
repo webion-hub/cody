6fc1bc9089c0213dae48799478d3e7b3ebbf327b
matteo2437 <matteo.budriesi@gmail.com>
2021-02-18 20:45:21 +0100

    create editable biography

---
+++ cody/ClientApp/src/components/pickers/text_fields/editable_text_fields/editable_biography.js
@@ -0,0 +1,117 @@
+import React, { useEffect } from 'react';
+import { Grid, Typography, Link, Button, TextField } from '@material-ui/core';
+import { DialogBase } from 'src/components/bases/dialog_base';
+
+import { Reading } from 'src/components/illustrations/reading'
+import { Form } from 'src/lib/default_values/sizes/form_size';
+
+export function EditableBiography(props){
+	const [editMode, setEditMode] = React.useState(false);
+
+	const [value, setValue] = React.useState(props.value);
+	const [tempValue, setTempValue] = React.useState(props.value);
+  const maxCharacters = 75;
+  const error = tempValue.length > maxCharacters;
+
+	const {onChange} = props;
+	
+	useEffect(() => {
+    setValue(props.value)
+    setTempValue(props.value)
+	}, [props.value])
+
+	const handleEdit = () => {
+
+		setEditMode(true);
+	}
+	
+	const handleUndo = () => {
+		setEditMode(false);
+		setValue(props.value);
+		onChange(props.value);
+	}
+	
+	const handleChange = (event) => {
+		setTempValue(event.target.value);
+	}
+
+	const handleSubmit = () => {
+    if(!error){
+      setEditMode(false);
+      setValue(tempValue);
+      onChange(tempValue);
+    }
+	}
+
+	return (
+    <div>
+      <Typography
+        variant="caption"
+      >
+        {"Descrizione - "}
+        <Link
+          color="secondary"
+          component="button"
+          variant="caption"
+          onClick={handleEdit}
+        >
+          Modifica
+        </Link>
+      </Typography>
+      <Typography
+        variant="body2"
+        color="textSecondary"
+      >
+        {value}
+      </Typography>
+      <DialogBase
+        title={props.title}
+        open={editMode}
+        onClose={() => setEditMode(false)}
+        firstButton={
+          <Button 
+            color="secondary"
+            onClick={handleUndo}
+          >
+            Reset
+          </Button>
+        }
+        secondButton={
+          <Button 
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+          >
+            Conferma
+          </Button>
+        }
+      >
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >  
+          <Reading size={Form.imageWidth}/>
+          <TextField
+            label="Descrizione"
+            multiline
+            color="secondary"
+            variant="filled"
+            fullWidth
+            rows={6}
+            defaultValue={value}
+            onChange={handleChange}
+            error={error}
+          />
+        </Grid>
+        <Typography
+          variant="caption"
+          color={error ? "error" : "textSecondary"}
+        >
+          {tempValue.length}/{maxCharacters}
+        </Typography>
+      </DialogBase>
+    </div>
+	);
+}
