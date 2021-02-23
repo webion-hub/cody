4868dc872a4d5a100c22efc98df7ecaafde83d68
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-02-23 23:27:51 +0100

    Create users filter endpoint

---
+++ cody/Controllers/Admin/UsersController.DetailedUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers.Admin
+{
+    public partial class UsersController
+    {
+        private record DetailedUser(
+            int Id,
+            string Username,
+            string Email,
+            UserDetail Detail,
+            UserPicture ProfilePicture,
+            UserSchool School
+        );
+
+        private record UserDetail(
+            string Name,
+            string Surname,
+            DateTime BirthDate
+        );
+
+        private record UserPicture(
+            string FilePath  
+        );
+
+        private record UserSchool(
+            int Id,
+            string Name,
+            string City,
+            string Country
+        );
+    }
+}
