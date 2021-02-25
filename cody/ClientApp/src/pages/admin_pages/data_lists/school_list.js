206520586aec3d2082fb80019358f91feeef7a09
matteo2437 <matteo.budriesi@gmail.com>
2021-02-25 23:09:59 +0100

    create school list

---
+++ cody/ClientApp/src/pages/admin_pages/data_lists/school_list.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { DataTableBase } from 'src/pages/admin_pages/components/data_table_base';
+import { School } from 'src/lib/school';
+
+export function SchoolList(props){
+	const associateUsers = (list, data, index) => {
+		list.push({
+			id: index,
+			Id: data.id,
+			name: data.name,
+			city: data.city,
+			country: data.country,
+		})
+	}
+
+	const usersColumns = [
+		{	selector: 'Id', name: 'ID', sortable: true },
+		{	selector: 'name', name: 'Nome', sortable: true },
+		{	selector: 'city', name: 'Città', sortable: true },
+		{ selector: 'country', name: 'Paese', sortable: true },
+	]
+
+	return (
+		<DataTableBase
+			getData={School.getAll}
+			associateData={associateUsers}
+			columns={usersColumns}
+			title="Scuole"
+			pageElements={props.pageElements}
+		/>
+	);
+}
