35acc0929423d29e41c8781b3ca30e9db3101122
matteo2437 <matteo.budriesi@gmail.com>
2021-02-25 23:10:14 +0100

    create user list

---
+++ cody/ClientApp/src/pages/admin_pages/data_lists/user_list.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { DataTableBase } from 'src/pages/admin_pages/components/data_table_base';
+import { Admin } from 'src/lib/admin';
+
+export function UserList(props){
+	const associateUsers = (list, data, index) => {
+		const birthDate = new Date(data.detail.birthDate)
+		list.push({
+			id: index,
+			Id: data.id,
+			username: data.username,
+			email: data.email,
+			name: data.detail.name,
+			surname: data.detail.surname,
+			birthDate: birthDate.toLocaleDateString(),
+			schoolId: data.school.id,
+		})
+	}
+
+	const usersColumns = [
+		{	selector: 'Id', name: 'ID', sortable: true },
+		{	selector: 'username', name: 'Username', sortable: true },
+		{	selector: 'email', name: 'Email', sortable: true },
+		{ selector: 'name', name: 'Nome', sortable: true },
+		{ selector: 'surname', name: 'Cognome', sortable: true },
+		{ selector: 'birthDate', name: 'Data di nascita', sortable: true },
+		{ selector: 'schoolId', name: 'Id Scuola', sortable: true },
+	]
+
+	return (
+		<DataTableBase
+			getData={Admin.getUsers}
+			associateData={associateUsers}
+			columns={usersColumns}
+			title="Users"
+			pageElements={props.pageElements}
+		/>
+	);
+}
