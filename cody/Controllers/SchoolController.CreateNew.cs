28d33606671007d1fadb573a0eb0e6498b146aa4
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-02-25 18:16:10 +0100

    Split school controller

---
+++ cody/Controllers/SchoolController.CreateNew.cs
@@ -0,0 +1,50 @@
+﻿using Cody.Controllers.Requests;
+using Cody.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers
+{
+    public partial class SchoolController : ControllerBase
+    {
+        [HttpPost("create_new")]
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateNew([FromBody] SchoolCreationRequest request)
+        {
+            SchoolAccount school = request;
+            if (SchoolExists(school, out var existingSchool))
+                return BadRequest(existingSchool.Id);
+
+            school.State = new SchoolAccountState
+            {
+                HasBeenVerified = false,
+            };
+
+            await _context.Schools.AddAsync(school);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("School created - {School}", school);
+            return Ok(school.Id);
+        }
+
+
+        private bool SchoolExists(SchoolAccount school, out SchoolAccount existingSchool)
+        {
+            var maybeExisting =
+                from s in _context.Schools
+                where
+                    s.Name == school.Name &&
+                    s.City == school.City &&
+                    s.Country == school.Country
+                select s;
+
+            existingSchool = maybeExisting.FirstOrDefault();
+            return maybeExisting.Any();
+        }
+    }
+}
--- cody/Controllers/SchoolController.cs
+++ cody/Controllers/SchoolController.cs
@@ -12,7 +12,7 @@ namespace Cody.Controllers
     [ApiController]
     [Route("school")]
     [Authorize]
-    public class SchoolController : ControllerBase
+    public partial class SchoolController : ControllerBase
     {
         private readonly ILogger<SchoolController> _logger;
         private readonly CodyContext _context;
@@ -21,61 +21,6 @@ namespace Cody.Controllers
         {
             _logger = logger;
             _context = context;
-        }
-
-
-        [HttpPost]
-        [Route("create_new")]
-        [AllowAnonymous]
-        public async Task<IActionResult> CreateNew([FromBody] SchoolCreationRequest request)
-        {
-            SchoolAccount school = request;
-            if (SchoolExists(school, out var existingSchool))
-                return BadRequest(existingSchool.Id);
-
-            school.State = new SchoolAccountState {
-                HasBeenVerified = false,
-            };
-
-            await _context.Schools.AddAsync(school);
-            await _context.SaveChangesAsync();
-
-            _logger.LogInformation("School created - {School}", school);
-            return Ok(school.Id);
-        }
-
-
-        private bool SchoolExists(SchoolAccount school, out SchoolAccount existingSchool)
-        {
-            var maybeExisting =
-                from s in _context.Schools
-                where
-                    s.Name == school.Name &&
-                    s.City == school.City &&
-                    s.Country == school.Country
-                select s;
-
-            existingSchool = maybeExisting.FirstOrDefault();
-            return maybeExisting.Any();
-        }
-
-
-        [HttpGet]
-        [Route("get_all")]
-        [AllowAnonymous]
-        public IActionResult GetAll()
-        {
-            var schools = 
-                from s in _context.Schools
-                where s.State.HasBeenVerified
-                select new {
-                    s.Id,
-                    s.Name,
-                    s.City,
-                    s.Country,
-                };
-
-            return Ok(schools);
         }
     }
 }
