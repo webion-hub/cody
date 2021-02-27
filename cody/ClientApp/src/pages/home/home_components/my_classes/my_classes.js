c4d96c8d73875fef15903d7fa2385dbea4dd0752
matteo2437 <matteo.budriesi@gmail.com>
2021-02-27 14:59:30 +0100

    make responsive my classes title

--- cody/ClientApp/src/pages/home/home_components/my_classes/my_classes.js
+++ cody/ClientApp/src/pages/home/home_components/my_classes/my_classes.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useMediaQuery } from '@material-ui/core'
+import { useTheme } from '@material-ui/core'
 
 import { ThereAreClasses } from './thereAreClasses';
 import { NoClasses } from './noClasses';
@@ -6,39 +8,54 @@ import { NoClasses } from './noClasses';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
-
-
 const classesStyles = makeStyles((theme) => ({
   classesBox: {
+    position: "relative",
+    margin: "0 auto",
+  },
+  areClassesBox: {
     maxWidth: "100vw",
     margin: "0 auto",
   },
-  emptyBox: {
+  noClassesBox: {
     maxWidth: 900,
     margin: "0 auto",
   },
-  titleIcon: {
-    marginRight: 15,
-    height: 30,
-    width: 30
+  titleLeft: {
+    textAlign: "left",
+    paddingLeft: theme.spacing(2),
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(2),
   },
-  title: {
-    textAlign: "center"
+  titleCenter: {
+    textAlign: "center",
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(2),
+    maxWidth: "calc(100vw - 96px)",
+    margin: "0 auto"
   }
 }));
 
 
 export function MyClasses(props){
   const classes = classesStyles();
+  const theme = useTheme();
+
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'));
+
   const classesList = props.classesList;
   const classesNumber = classesList.length; 
 
   const areClasses = classesNumber !== 0;
 
   const title = areClasses ?
-    <Typography variant="h4" color="textSecondary" className={classes.title}>
-      <SchoolRoundedIcon className={classes.titleIcon}/>
+    <Typography 
+      variant={mobileView ? "h4" : "h3"} 
+      color="textPrimary" 
+      className={`
+        ${mobileView ? classes.titleCenter : classes.titleLeft}  
+      `}
+    >
         Le classi a cui sei iscritto
     </Typography>
     : 
@@ -53,7 +70,7 @@ export function MyClasses(props){
 
   return (
     <div className={props.className}>
-      <div className={areClasses ? classes.classesBox : classes.emptyBox}>
+      <div className={`${areClasses ? classes.areClassesBox : classes.noClassesBox} ${classes.classesBox}`}>
         {title}
         {content}
       </div>
