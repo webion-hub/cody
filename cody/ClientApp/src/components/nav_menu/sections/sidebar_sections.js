ce252457d4e09c8ad7197fd0a7735f69dc34df95
matteo2437 <matteo.budriesi@gmail.com>
2021-03-03 16:04:04 +0100

    add new sections to sidebar

--- cody/ClientApp/src/components/nav_menu/sections/sidebar_sections.js
+++ cody/ClientApp/src/components/nav_menu/sections/sidebar_sections.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
 import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
 
 import history from 'src/history'
+import { CustomAvatar } from 'src/components/custom_avatar';
 
 export const sideBarSections = [
-    [
-      {
-        label: 'Home',
-        icon: (<HomeRoundedIcon />),              
-        onClick: () => history.push('/'),
-      },
-      {
-        label: 'Classi',
-        icon: (<SchoolRoundedIcon />)
-      },
-    ],
-    [
-      {
-        label: 'Esci',
-        icon: (<ExitToAppRoundedIcon />),
-        onClick: () => history.push('/login'),
-      },
-    ],    
+    {
+      label: 'Home',
+      icon: (<HomeRoundedIcon />),              
+      onClick: () => history.push('/'),
+    },
+    {
+      label: 'Classi',
+      icon: (<SchoolRoundedIcon />),
+      onClick: () => history.push('/class'),
+    },
+    {
+      divider: true,
+      label: 'Crea o unisciti',
+      icon: (<AddRoundedIcon />),
+      onClick: () => history.push('/create_join_organization'),
+    },
+    {
+      padding: 4,
+      label: 'ITIS Fermi',
+      icon: (<CustomAvatar src="images/bulb.jpeg"/>),
+    }, 
   ]
