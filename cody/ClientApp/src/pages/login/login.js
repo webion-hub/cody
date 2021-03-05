d25f973ccc023812998045937d328e97988d2c10
matteo2437 <matteo.budriesi@gmail.com>
2021-03-05 20:54:49 +0100

    refactoring login

--- cody/ClientApp/src/pages/login/login.js
+++ cody/ClientApp/src/pages/login/login.js
@@ -1,81 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Button } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import { Paper } from '@material-ui/core';
-import { Box } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Authentication } from 'src/components/illustrations/authentication';
 import { LoginBox } from './login_box';
-
-import { Form } from 'src/lib/default_values/sizes/form_size';
 
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 import { Images } from 'src/lib/default_values/images';
 
 import history from 'src/history'
+import { BasePhotoText } from 'src/components/bases/base_photo_text';
 
-const CustomGrid = withStyles((theme) => ({
-  root: {
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    maxWidth: 632,
+    width: "100%",
     marginTop: theme.appBar.fullHeight,
     [theme.breakpoints.down('xs')]: {
       marginTop: theme.appBar.mobileHeight,
     },
   },
-}))(Grid);
+  createAccount: {
+    margin: theme.spacing(1),
+    backdropFilter: "blur(10px)",
+    background: theme.palette.background.backgroundTransparent,
+    color: "rgba(255,255,255,0.9)",
+  }
+}));
 
-export class Login extends Component {
-  static displayName = Login.name;
-  render () {
-    return (
+export function Login(){
+	const classes = useStyles();
+
+  return (
+    <Grid
+      style={{
+        minHeight: "100vh",
+        backgroundImage: `url(${Images.forestImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center"
+      }}
+      container
+      justify="center"
+      alignItems="center"
+    >
       <Grid
-        style={{
-          minHeight: "100vh",
-          backgroundImage: `url(${Images.forestImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center center"
-        }}
         container
+        direction="column"
         justify="center"
         alignItems="center"
       >
-        <CustomGrid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
+        <Paper className={classes.paper}>
+          <BasePhotoText
+            image={Authentication}
+            items={[
+              <LoginBox/>
+            ]}
+          />
+        </Paper> 
+        <Button
+          className={classes.createAccount}
+          endIcon={<ArrowForwardIcon/>}
+          onClick={() => history.push('/sign-up')}
         >
-          <Paper elevation={3}>
-            <Box
-              p={3}
-            >
-              <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-              >
-                <Authentication size={Form.imageWidth}/>
-                <LoginBox size={Form.width}/>
-              </Grid>
-            </Box>
-          </Paper> 
-          <Box m={0.5}/>
-          <Button
-            style={{
-              backdropFilter: "blur(10px)",
-              background: "rgba(0,0,0,0.2)",
-              color: "rgba(255,255,255,0.9)",
-            }}
-            endIcon={<ArrowForwardIcon/>}
-            onClick={() => history.push('/sign-up')}
-          >
-            Crea un account
-          </Button>
-        </CustomGrid>
+          Crea un account
+        </Button>
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
--- cody/ClientApp/src/pages/login/login_box.js
+++ cody/ClientApp/src/pages/login/login_box.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { TextField } from '@material-ui/core';
 import { Link } from '@material-ui/core';
@@ -6,7 +6,7 @@ import { Grid } from '@material-ui/core';
 import { Checkbox } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
-import { Box } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Password } from 'src/components/password/password_textfield';
 import { LoadingButton } from 'src/components/buttons/loading_button';
@@ -18,212 +18,192 @@ import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 import { UserContext } from 'src/components/user_controller_context';
 import { User } from 'src/lib/user';
 
-export class LoginBoxMain extends Component{
-
-  constructor(props){
-    super(props);
-    this._updatePassword = this._updatePassword.bind(this);
-    this.openForgotPw = this.openForgotPw.bind(this);
-    this.closeForgotPw = this.closeForgotPw.bind(this);
-
-    this.state = {
-      username: '',
-      password: '',
-      rememberMe: false,
-      wrongUsername: false,
-      wrongPassword: false,
-      loading: false,
-      focusPw: false,
-
-      openForgotPw: false,
-
-      loginSuccess: false,
-    }
-
-    this.nextFocus = new NextFocus(["username","password"]);
+export const useStyles = makeStyles((theme) => ({
+  usernameTextField: {
+    marginBottom: theme.spacing(1)
+  },
+  forgotPasswordLink: {
+    fontSize: 12,
+    marginTop: theme.spacing(1)
   }
+}));
 
-  _maybeLogin() {
-    this.setState({wrongUsername: false});
-    this.setState({wrongUsername: false});
-
-    if (!this.state.username)
-      this.setState({wrongUsername: true});
-
-    if (!this.state.password)
-      this.setState({wrongPassword: true});
-
-    if (!this.state.username || !this.state.password)
-      return;
-
-    this.setState({loading: true});
-    this._tryLogin();
-  }
-
-
-  async _tryLogin() {
-    this.setState({loginSuccess: false});
-    await User.tryLogin({
-      userInfo: {
-        username: this.state.username,
-        password: this.state.password,
-        rememberMe: this.state.rememberMe,
-      },
-      
-      onSuccess: _ => this.setState({loginSuccess: true}),
-      onUserNotFound: _ => this.setState({wrongUsername: true}),
-      onPasswordMismatch: _ => this.setState({wrongPassword: true}),
-    })
-    .then(_=> {
-      if(this.state.loginSuccess){
-        this.props.setLogged(true);
-      }
-      else{
-        this.setState({loading: false});
-      }
-    });
-  }
-
-  _updateUsername = e => {
-    this.setState({username: e.target.value});
-  }
-
-  _updatePassword(value) {
-    this.setState({password: value});
-  }
-
-  _updateRememberMe = e =>  {
-    this.setState({rememberMe: !this.state.rememberMe});
-  }
-  
-  openForgotPw(){
-    this.setState({openForgotPw: true});
-  }
-  closeForgotPw(){
-    this.setState({openForgotPw: false});
-  }
-
-  render(){
-    return(
-      <Box maxWidth={this.props.size}
-        justifyContent="center"
-        display="flex"
-        position="relative"
-      >     
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-          >
-            <TextField
-              id="username"
-              label="Username"
-              name="login_username"
-              variant="outlined"
-              color="secondary"
-              fullWidth={true}
-              onChange={this._updateUsername}
-              error={this.state.wrongUsername}
-              inputRef={this.nextFocus.getInput("username")}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  this.nextFocus.focusOn("password");
-                }
-             }}
-            />
-            <Box m={0.5}/>
-            <Password
-              label="Password"
-              name="login_password"
-              labelWidth={70}
-              onChange={this._updatePassword}
-              inputRef={this.nextFocus.getInput("password")}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  this._maybeLogin();
-                  this.nextFocus.removeFocus();
-                }
-             }}
-              error={this.state.wrongPassword}
-            />
-            <Grid
-              container
-              justify="flex-start"
-            >
-              <FormControlLabel
-                value="end"
-                control={
-                  <Checkbox
-                    color="secondary"
-                    size="small"
-                    onChange={this._updateRememberMe}
-                  />
-                }
-                label={
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                  >
-                    Ricordami
-                  </Typography>
-                }
-                labelPlacement="end"
-              />
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-          >
-            <LoadingButton
-              loading={this.state.loading}
-              fullWidth={true}
-              endIcon={<AccountCircleRoundedIcon/>}
-              onClick={_ => {
-                this._maybeLogin()
-              }}
-              label="Accedi"
-            />
-            <Box mt={1}>
-              <Link
-                style={{
-                  fontSize: 12,
-                }}
-                component="button"
-                variant="body2"
-                color="secondary"
-                onClick={this.openForgotPw}
-              >
-                Password dimenticata?
-              </Link>
-              <ForgotPasswordDialog
-                open={this.state.openForgotPw}
-                onClose={this.closeForgotPw}
-              />
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    )
-  }
-}
-
-export function LoginBox(props){
+export function LoginBox(){
+	const classes = useStyles();
   const { setLogged } = React.useContext(UserContext);
 
+  const [data, setData] = React.useState({
+    username: '',
+    password: '',
+  });
+  const [errors, setErrors] = React.useState({
+    wrongUsername: false,
+    wrongPassword: false,
+  });   
+  const [rememberMe, setRememberMe] = React.useState(false);   
+  const [loading, setLoading] = React.useState(false);   
+  const [openForgotPw, setOpenForgotPw] = React.useState(false);   
+
+  const nextFocus = new NextFocus(["username","password"]);
+
+  const _maybeLogin = () => {
+    setErrors({
+      wrongPassword: false,
+      wrongUsername: false,
+    })
+
+    if (!data.username)
+      setErrors({
+        ...errors,
+        wrongUsername: true,
+      })
+
+    if (!data.password)
+      setErrors({
+        ...errors,
+        wrongPassword: true,
+      })
+
+    if (!data.username || !data.password)
+      return;
+
+    setLoading(true);
+    _tryLogin();
+  }
+
+
+  const _tryLogin = async () => {
+    await User.tryLogin({
+      userInfo: data,
+      
+      onSuccess: _ => setLogged(true),
+      onUserNotFound: _ => {
+        setErrors({...errors, wrongUsername: true})
+        setLoading(false);
+      },
+      onPasswordMismatch: _ => {
+        setErrors({...errors, wrongPassword: true})
+        setLoading(false);
+      },
+    })
+  }
+
+  const _updateUsername = e => {
+    setData({
+      ...data,
+      username: e.target.value,
+    })
+  }
+
+  const _updatePassword = (value) => {
+    setData({
+      ...data,
+      password: value,
+    })
+  }
+
+  const _updateRememberMe = () =>  {
+    setRememberMe(!rememberMe)
+  }
+
   return(
-    <LoginBoxMain
-      size={props.size}
-      setLogged={setLogged}
-    />
-  );
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <TextField
+          className={classes.usernameTextField}
+          id="username"
+          label="Username"
+          name="login_username"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          onChange={_updateUsername}
+          error={errors.wrongUsername}
+          inputRef={nextFocus.getInput("username")}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              nextFocus.focusOn("password");
+            }
+        }}
+        />
+        <Password
+          label="Password"
+          name="login_password"
+          labelWidth={70}
+          onChange={_updatePassword}
+          inputRef={nextFocus.getInput("password")}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              _maybeLogin();
+              nextFocus.removeFocus();
+            }
+        }}
+          error={errors.wrongPassword}
+        />
+        <Grid
+          container
+          justify="flex-start"
+        >
+          <FormControlLabel
+            value="end"
+            control={
+              <Checkbox
+                color="secondary"
+                size="small"
+                onChange={_updateRememberMe}
+              />
+            }
+            label={
+              <Typography
+                variant="body2"
+                color="textSecondary"
+              >
+                Ricordami
+              </Typography>
+            }
+            labelPlacement="end"
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <LoadingButton
+          loading={loading}
+          fullWidth
+          endIcon={<AccountCircleRoundedIcon/>}
+          onClick={_ => {
+            _maybeLogin()
+          }}
+          label="Accedi"
+        />
+        <Link
+          className={classes.forgotPasswordLink}
+          component="button"
+          variant="body2"
+          color="secondary"
+          onClick={() => setOpenForgotPw(true)}
+        >
+          Password dimenticata?
+        </Link>
+        <ForgotPasswordDialog
+          open={openForgotPw}
+          onClose={() => setOpenForgotPw(false)}
+        />
+      </Grid>
+    </Grid>
+  )
 }
