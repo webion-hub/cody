e78b4734468bc6db32959ccf21b01e0ce7185575
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-03-05 21:07:37 +0100

    Implement Organization.CreateNew

--- cody/Controllers/Organizations/OrganizationsController.CreateNew.cs
+++ cody/Controllers/Organizations/OrganizationsController.CreateNew.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Cody.Controllers.Requests;
+using Cody.Extensions;
+using Cody.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +16,40 @@ namespace Cody.Controllers.Organizations
     {
         [HttpPost("create_new")]
         [Authorize]
-        public IActionResult CreateNew()
+        public async Task<IActionResult> CreateNew([FromBody] OrganizationCreationRequest request)
         {
-            return Ok();
+            Organization organization = request;
+            if (await OrganizationExists(organization))
+                return Conflict();
+                        
+            SetOwner(organization);
+
+            _dbContext.Organizations.Add(organization);
+            await _dbContext.SaveChangesAsync();
+            
+            return Ok(organization.Id);
+        }
+
+
+        private async Task<bool> OrganizationExists(Organization organization)
+        {
+            return await _dbContext
+                .Organizations
+                .Where(o =>
+                    o.Kind == organization.Kind &&
+                    o.Name == organization.Name
+                )
+                .AnyAsync();
+        }
+
+        private void SetOwner(Organization organization)
+        {
+            organization.Members = new() {
+                new OrganizationMember {
+                    Role = OrganizationRole.Owner,
+                    UserAccountId = HttpContext.User.GetId(),
+                }
+            };
         }
     }
 }
