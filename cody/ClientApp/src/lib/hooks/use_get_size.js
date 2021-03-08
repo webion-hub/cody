c0a98ddc5ddcff1ce09b15291fe7b511efa7a1fb
matteo2437 <matteo.budriesi@gmail.com>
2021-03-08 22:04:10 +0100

    create use get size hook

---
+++ cody/ClientApp/src/lib/hooks/use_get_size.js
@@ -0,0 +1,42 @@
+import React, { useLayoutEffect } from 'react';
+
+export function useGetSize(ref){
+  const [size, setSize] = React.useState({
+    width: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+
+  }, [ref.current]);
+
+  const updateSize = () => {
+    let width;
+    let height;
+    if(ref !== null){
+      if(ref.current !== undefined)
+        {
+          width = ref.current.offsetWidth;
+          height = ref.current.offsetHeight;
+        }
+        else
+        {
+          width = ref.innerWidth;
+          height = ref.innerHeight;
+        }
+
+      const newSize = {
+        width: width,
+        height: height,
+      }
+      
+      if(newSize !== size)
+        setSize(newSize);
+    }
+	}
+
+  return size;
+}
