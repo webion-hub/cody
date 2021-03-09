e77fb244230804b6d605bbae8d4a1b9dc4785007
matteo2437 <matteo.budriesi@gmail.com>
2021-03-09 21:13:27 +0100

    redo create organization page

--- cody/ClientApp/src/pages/create_or_join_organization/create_organization/create_organization.js
+++ cody/ClientApp/src/pages/create_or_join_organization/create_organization/create_organization.js
@@ -1,41 +1,96 @@
 import React from 'react';
 
-import { TitleInfoContentBase } from 'src/components/bases/title_info_content_base';
-import { OrganizationAction } from 'src/pages/create_or_join_organization/create_or_join_organization_components/organization_action';
+import { Grid, Typography, TextField, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
-import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import GroupRoundedIcon from '@material-ui/icons/GroupRounded';
+import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
+import BusinessCenterRoundedIcon from '@material-ui/icons/BusinessCenterRounded';
 
-import { Teacher } from 'src/components/illustrations/teacher';
 import { TeamWork } from 'src/components/illustrations/team_work';
-import { Career } from 'src/components/illustrations/career';
+import { CreateOrganizationBase } from './create_organization_base';
+
 import { PageController } from 'src/lib/page_controller';
 
+const useStyles = makeStyles((theme) => ({
+  imageContainer: {
+    width: "100%",
+    maxWidth: 300
+  },
+  organizationSelectionField: {
+    maxWidth: 300
+  },
+  menuLabel: {
+    paddingLeft: theme.spacing(2)
+  },
+}));
+
+
+const organizationKinds = [
+  {
+    value: 'team',
+    label: 'Team',
+    icon: GroupRoundedIcon,
+  },
+  {
+    value: 'school',
+    label: 'Istituto Scolastico',
+    icon: SchoolRoundedIcon,
+  },
+  {
+    value: 'company',
+    label: 'Azienda',
+    icon: BusinessCenterRoundedIcon,
+  },
+];
+
 export function CreateOrganization(props){
+  const classes = useStyles();
+  const [organizationKind, setOrganizationKind] = React.useState('team');
+
+  const handleChange = (event) => {
+    setOrganizationKind(event.target.value);
+  };
 
   return(
-    <>
-      <OrganizationAction
-        width="33%"
-        image={TeamWork}
-        buttonLabel="Crea Team"
-        endIcon={<ArrowForwardRoundedIcon/>}
-        onClick={() => PageController.updateHash("createTeam")}
-      />
-      <OrganizationAction
-        width="33%"
-        image={Teacher}
-        buttonLabel="Aggiungi Istituto"
-        endIcon={<AddRoundedIcon/>}
-        onClick={() => PageController.updateHash("createSchool")}
-      />
-      <OrganizationAction
-        width="33%"
-        image={Career}
-        buttonLabel="Aggiungi Azienda"
-        endIcon={<AddRoundedIcon/>}
-        onClick={() => PageController.updateHash("createCompany")}
-      />
-    </>
+    <CreateOrganizationBase
+      label="Avanti"
+      href={`/organization#create${organizationKind}`}
+      onClick={(e) => PageController.updateHash(`create${organizationKind}`, e)}
+    >
+      <div className={classes.imageContainer}>
+        <TeamWork size="100%"/>
+      </div>
+      <TextField
+        className={classes.organizationSelectionField}
+        select
+        color="secondary"
+        label="Organizzazione"
+        value={organizationKind}
+        onChange={handleChange}
+        helperText="Scegli il tipo di organizzazione"
+        fullWidth
+        variant="outlined"
+      >
+        {organizationKinds.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+            >
+              <option.icon fontSize="small"/>
+              <Typography
+                className={classes.menuLabel}
+                variant="inherit" 
+                noWrap
+              >
+                {option.label}
+              </Typography>  
+            </Grid>            
+          </MenuItem>
+        ))}
+      </TextField>
+    </CreateOrganizationBase>
   );
 }
