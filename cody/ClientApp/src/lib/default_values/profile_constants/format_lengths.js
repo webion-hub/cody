b8eb6f06eabe570dc83027cf516d12d57bd9aa7f
matteo2437 <matteo.budriesi@gmail.com>
2021-03-13 12:50:41 +0100

    create profile constants

---
+++ cody/ClientApp/src/lib/default_values/profile_constants/format_lengths.js
@@ -0,0 +1,19 @@
+export const formatLengths = {
+  stdLength: {
+    max: 256,
+    min: 0,
+  },
+  usernameLength: {
+    max: 256,
+    min: 2,
+  },
+  passwordLength: {
+    max: 256,
+    min: 8,
+  },
+  descriptionLength: {
+    max: 512,
+    min: 0,
+  }
+}
+
---
+++ cody/ClientApp/src/lib/default_values/profile_constants/profile_image.js
@@ -0,0 +1,1 @@
+export const profileImage = 'user/profile_picture'
