bf0753fbb24e3e959a28b0c80aa6f6e64216f509
matteo2437 <matteo.budriesi@gmail.com>
2021-03-14 18:05:35 +0100

    refactoring custom avatar and add custom loading

--- cody/ClientApp/src/components/custom_avatar.js
+++ cody/ClientApp/src/components/custom_avatar.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { Avatar, useTheme } from '@material-ui/core';
+import { Avatar, CircularProgress, useTheme } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 
 export function CustomAvatar(props){
@@ -36,37 +36,46 @@ export function CustomAvatar(props){
   }, [])
 
   const finalLoading = loading && !props.disableLoading
+  const size = props.size? props.size : 40
+
+  const extraLoadingWidth = size / 5
+  const loadingSize = size + extraLoadingWidth;
 
   return (
-    <>
+    <div
+      style={{
+        position: "relative"
+      }}
+    >
       <Skeleton
         variant="circle" 
         animation="wave"
-        width={props.width? props.width : 40} 
-        height={props.height? props.height : 40} 
+        width={size} 
+        height={size} 
         style={{
           display: finalLoading ? "block" : "none"
         }}
       />
-      <Avatar
-        alt={props.alt}
-        className={props.className}
-        classes={props.classes}
-        component={props.component}
-        imgProps={props.imgProps}
-        sizes={props.sizes}
-        srcSet={props.srcSet}
-        variant={props.variant}
-        src={props.src}
-        style={props.style}
+      <CircularProgress
+        thickness={2.5}
+        size={loadingSize}
         style={{
-          width: props.width,
-          height: props.height,
-          fontSize: `${1.25 * (props.width / 40)}rem`,
+          position: "absolute",
+          top: -extraLoadingWidth/2,
+          left: -extraLoadingWidth/2,
+          display: finalLoading ? "block" : "none",
+        }}
+      />
+      <Avatar
+        {...props}
+        style={{
+          width: size,
+          height: size,
+          fontSize: `${1.25 * (size / 40)}rem`,
           display: finalLoading ? "none" : "flex",
           boxShadow: props.shadow ? `2px 2px 6px 0px ${theme.palette.background.paperSecondary}` : null,
         }}
       />
-    </> 
+    </div> 
   );
 }
