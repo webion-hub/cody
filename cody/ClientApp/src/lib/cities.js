6336a18feb33daaaddc98273c11862bfc32f21c3
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-03-15 20:32:41 +0100

    Add support for realtime search

--- cody/ClientApp/src/lib/cities.js
+++ cody/ClientApp/src/lib/cities.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import SingleXHRRequest from './single_xhr_request';
 
 export class Cities {
   /**
@@ -6,26 +7,35 @@ export class Cities {
    * @returns {Promise<City[]>}
    */
   static async find(name) {
-    return axios.get('https://dev.virtualearth.net/REST/v1/Locations', {
-      params: {
-        key: Cities.BING_MAPS_API_KEY,
-        query: name,
-        maxResults: 10,
-        culture: 'it-IT',
-      },
-    })
-    .then(resp => resp
-      .data
+    return Cities._singleRequest.send(tokenSource => {
+      return axios.request({
+        url: 'https://dev.virtualearth.net/REST/v1/Locations',
+        method: 'GET',
+        cancelToken: tokenSource.token,
+        params: {
+          key: Cities.BING_MAPS_API_KEY,
+          query: name,
+          maxResults: 10,
+          culture: 'it-IT',
+        },
+      })
+      .then(resp => resp.data)
+      .then(Cities.formatResult);
+    });
+  }
+
+  static formatResult(bingResult) {
+    return bingResult
       .resourceSets[0]
       .resources
       .filter(r => ['PopulatedPlace', 'Neighborhood'].includes(r.entityType))
       .map(r => ({
         location: r.name,
-      }))
-    );
+      }));
   }
 }
 
+Cities._singleRequest = new SingleXHRRequest();
 Cities.BING_MAPS_API_KEY = 'Atk8WqFahBgE88OPqmaBcGtaXp-eW-oaL3VjFrtRPscXG5sq1xwk69zAWanavyH_';
 
 
