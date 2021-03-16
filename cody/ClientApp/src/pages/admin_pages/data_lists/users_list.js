2d2f989a31f1efe17f43b4a6099ac9e50b49b4d0
matteo2437 <matteo.budriesi@gmail.com>
2021-03-16 22:25:36 +0100

    update users list

--- cody/ClientApp/src/pages/admin_pages/data_lists/users_list.js
+++ cody/ClientApp/src/pages/admin_pages/data_lists/users_list.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { DataTableBase } from 'src/pages/admin_pages/components/data_table_base';
+import { RowMenu } from 'src/pages/admin_pages/components/row_menu';
 import { Admin } from 'src/lib/admin';
+import { getDeletedIcon } from 'src/pages/admin_pages/lib/get_deleted_icon';
 
 export function UsersList(props){
 	const associateUsers = (settings) => {
@@ -8,8 +10,16 @@ export function UsersList(props){
 		const data = settings.data;
 		const index = settings.index;
 		
+		const menu = 
+			<RowMenu 
+				data={data} 
+				onDelete={Admin.deleteUser} 
+				onRestore={Admin.restoreUser}
+			/>
+
 		const birthDate = new Date(data.detail.birthDate)
 		list.push({
+			menu: menu,
 			id: index,
 			Id: data.id,
 			username: data.username,
@@ -17,16 +27,19 @@ export function UsersList(props){
 			name: data.detail.name,
 			surname: data.detail.surname,
 			birthDate: birthDate.toLocaleDateString(),
+			deleted: getDeletedIcon(data.state.hasBeenDeleted),
 		})
 	}
 
 	const usersColumns = [
+		{ selector: 'menu', name: '', sortable: true, width: "48px", compact: true },
 		{	selector: 'Id', name: 'ID', sortable: true },
 		{	selector: 'username', name: 'Username', sortable: true },
 		{	selector: 'email', name: 'Email', sortable: true },
 		{ selector: 'name', name: 'Nome', sortable: true },
 		{ selector: 'surname', name: 'Cognome', sortable: true },
 		{ selector: 'birthDate', name: 'Data di nascita', sortable: true },
+		{ selector: 'deleted', name: 'Stato' },
 	]
 
 	return (
