4ef62344d0fca805e796e984ff1b19759bea6e74
matteo2437 <matteo.budriesi@gmail.com>
2021-03-16 22:25:26 +0100

    update organizations list

--- cody/ClientApp/src/pages/admin_pages/data_lists/organizations_list.js
+++ cody/ClientApp/src/pages/admin_pages/data_lists/organizations_list.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import { DataTableBase } from 'src/pages/admin_pages/components/data_table_base';
+import { RowMenu } from 'src/pages/admin_pages/components/row_menu';
 import { Organizations } from 'src/lib/organizations';
-
-import GroupRoundedIcon from '@material-ui/icons/GroupRounded';
-import SchoolRoundedIcon from '@material-ui/icons/SchoolRounded';
-import BusinessCenterRoundedIcon from '@material-ui/icons/BusinessCenterRounded';
-import BeenhereRoundedIcon from '@material-ui/icons/BeenhereRounded';
-import TimerRoundedIcon from '@material-ui/icons/TimerRounded';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  noVerified: {
-    color: "red",
-  },
-	verified: {
-    color: "green",
-  },
-}));
+import { getDeletedIcon } from 'src/pages/admin_pages/lib/get_deleted_icon';
+import { getVerificationIcon } from 'src/pages/admin_pages/lib/get_verification_icon';
+import { getKindIcon } from 'src/pages/admin_pages/lib/get_kind_icon';
 
 export function OrganizationsList(props){
-  const classes = useStyles();
-
-	const getKindIcon = (kind) => {
-		switch(kind){
-			case "Team":
-				return <GroupRoundedIcon fontSize="small"/>
-			case "School":
-				return <SchoolRoundedIcon fontSize="small"/>
-			case "Company":
-				return <BusinessCenterRoundedIcon fontSize="small"/>
-		}
-	}
 
 	const associateOrganizations = (settings) => {
 		const list = settings.list;
@@ -38,25 +14,34 @@ export function OrganizationsList(props){
 		const data = settings.data;
 		const index = settings.index;
 
+		const menu = 
+			<RowMenu 
+				data={data} 
+				onVerify={Organizations.verify} 
+				onDelete={Organizations.delete} 
+				onRestore={Organizations.restore}
+			/>
+
 		list.push({
+			menu: menu,
 			id: index,
 			Id: data.id,
 			kind: getKindIcon(data.kind),
 			name: data.name,
 			location: data.detail.location,
-			verified: data.state.hasBeenVerified ? 
-				<BeenhereRoundedIcon fontSize="small" className={classes.verified}/> 
-				: 
-				<TimerRoundedIcon fontSize="small" className={classes.noVerified}/>,
+			verified: getVerificationIcon(data.state.hasBeenVerified),
+			deleted: getDeletedIcon(data.state.hasBeenDeleted),
 		})
 	}
 
 	const usersColumns = [
+		{ selector: 'menu', name: '', sortable: true, width: "48px", compact: true },
 		{	selector: 'Id', name: 'ID', sortable: true },
-		{	selector: 'kind', name: 'Kind', sortable: true },
+		{	selector: 'kind', name: 'Kind' },
 		{	selector: 'name', name: 'Nome', sortable: true },
 		{	selector: 'location', name: 'Luogo', sortable: true },
-		{ selector: 'verified', name: 'Verificato', sortable: true },
+		{ selector: 'verified', name: 'Verificato' },
+		{ selector: 'deleted', name: 'Stato' },
 	]
 
 	return (
@@ -69,3 +54,4 @@ export function OrganizationsList(props){
 		/>
 	);
 }
+
