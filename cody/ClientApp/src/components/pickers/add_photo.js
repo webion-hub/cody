788b3a7d0c44a35e49f361488ae5c1ed97b8a91d
matteo2437 <matteo.budriesi@gmail.com>
2021-03-17 11:43:39 +0100

    transform add_photo to functional component

--- cody/ClientApp/src/components/pickers/add_photo.js
+++ cody/ClientApp/src/components/pickers/add_photo.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-import { Box } from '@material-ui/core';
 import { Fab } from '@material-ui/core';
 import { Badge } from '@material-ui/core';
-import { Skeleton } from '@material-ui/lab';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { ImageCropperDialog } from 'src/components/dialogs/image_cropper_dialog';
 import { ImageOrientation } from 'src/lib/image_orientation';
@@ -13,52 +13,51 @@ import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 import EditRoundedIcon from '@material-ui/icons/EditRounded';
 import { CustomAvatar } from '../custom_avatar';
 
-export class AddPhoto extends Component{
+export function AddPhoto(props){
+  const [image, setImage] = React.useState(null);
+  const [openEditDialog, setOpenEditDialog] = React.useState(false);
+  const [croppedImage, setCroppedImage] = React.useState(props.value);
 
-  constructor(props){
-    super(props);
+  const theme = useTheme();  
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'));
+
+  const iconSize = props.iconSize? 
+    props.iconSize : 40;
     
-    this.state = {
-      image: null,
-      
-      openEditDialog: false,
-      croppedImage: this.props.value,
-    }
+  const responsiveDefaultImageSize = mobileView ? 75 : 100;
+  const imageSize = props.imageSize? props.imageSize : responsiveDefaultImageSize;
+
+  useEffect(() => {
+    setCroppedImage(props.value)
+  }, [props.value])
+
+  const resetImage = () => {
+    setImage(props.value);
+    const {image} = props;
+    image(props.value);
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.value !== this.props.value) {
-      this.setState({croppedImage: this.props.value})
-    }
-  }
-
-  resetImage = () => {
-    this.setState({image: this.props.value});
-    const {image} = this.props;
-    image(this.props.value);
-  }
-
-  deleteImage = () => {
-    this.setState({croppedImage: null});
-    const {image} = this.props;
+  const deleteImage = () => {
+    setCroppedImage(null);
+    const {image} = props;
     image(null);
   };
 
-  handleCloseEditDialog = () => {
-    this.setState({openEditDialog: false});
+  const handleCloseEditDialog = () => {
+    setOpenEditDialog(false);
   }
 
   
-  getCroppedImage = (value) => {
-    this.setState({croppedImage: value})
-    const {image} = this.props;
+  const getCroppedImage = (value) => {
+    setCroppedImage(value)
+    const {image} = props;
     image(value); 
   }
 
   /**
    * @param {React.ChangeEvent<HTMLInputElement>} event
    */
-  fileSelectedHandler = (event) => {
+   const fileSelectedHandler = (event) => {
     let file = event.target.files[0];
     if(event.target.files[0] !== null){ 
       const imageOrientation = new ImageOrientation()
@@ -66,102 +65,95 @@ export class AddPhoto extends Component{
       imageOrientation.
         fixOrientation(file)
         .then(blob => {
-          this.setState({
-            image: blob,
-            openEditDialog: true
-          });
+          setImage(blob);
+          setOpenEditDialog(true);
         })
     }
 
     event.target.value = null; //reset input file
   }
 
-  render(){
-    const iconSize = this.props.iconSize? this.props.iconSize : 40;
-    const imageSize = this.props.imageSize? this.props.imageSize : 100;
+  const badgeContent = (
+    <div>
+      {
+        croppedImage? (
+          <Fab 
+            color="primary"
+            onClick={deleteImage}
+            style={{
+              width: iconSize,
+              height: iconSize
+            }}
+          >
+            <DeleteRoundedIcon />
+          </Fab>
+        ):(
+          <Fab 
+            component="label"
+            color="primary"
+            style={{
+              width: iconSize,
+              height: iconSize
+            }}
+          >
+            <AddRoundedIcon />
+            <input
+              type="file"
+              accept="image/*"
+              onChange={fileSelectedHandler}
+              hidden
+            />
+          </Fab>
+        )
+      }
+    </div>
+  )
 
-    const badgeContent = (
-      <div>
-        {
-          this.state.croppedImage? (
-            <Fab 
-              color="primary"
-              onClick={this.deleteImage}
-              style={{
-                width: iconSize,
-                height: iconSize
-              }}
-            >
-              <DeleteRoundedIcon />
-            </Fab>
-          ):(
-            <Fab 
-              component="label"
-              color="primary"
-              style={{
-                width: iconSize,
-                height: iconSize
-              }}
-            >
-              <AddRoundedIcon />
-              <input
-                type="file"
-                accept="image/*"
-                onChange={this.fileSelectedHandler}
-                hidden
-              />
-            </Fab>
-          )
-        }
-      </div>
-    )
-
-    const editableBadgeContent = (
-      <div>
-        <Fab 
-          component="label"
-          color="primary"
-          style={{
-            width: iconSize,
-            height: iconSize
-          }}
-        >
-          <EditRoundedIcon />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={this.fileSelectedHandler}
-            hidden
-          />
-        </Fab>
-      </div>
-    )
-
-    return (
-      <>    
-        <Badge
-          className={this.props.className}
-          overlap="circle"
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
-          badgeContent={this.props.accountEdit ? editableBadgeContent : badgeContent}
-        >
-          <CustomAvatar
-            disableLoading={this.props.disableLoading}
-            alt={this.props.alt}
-            src={this.state.croppedImage}
-            size={imageSize}
-          />
-        </Badge>
-        <ImageCropperDialog
-          open={this.state.openEditDialog}
-          handleClose={this.handleCloseEditDialog}
-          image={this.state.image}
-          croppedImage={this.getCroppedImage}
+  const editableBadgeContent = (
+    <div>
+      <Fab 
+        component="label"
+        color="primary"
+        style={{
+          width: iconSize,
+          height: iconSize
+        }}
+      >
+        <EditRoundedIcon />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={fileSelectedHandler}
+          hidden
         />
-      </>
-    )
-  }
+      </Fab>
+    </div>
+  )
+
+  return (
+    <>    
+      <Badge
+        className={props.className}
+        overlap="circle"
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        badgeContent={props.accountEdit ? editableBadgeContent : badgeContent}
+      >
+        <CustomAvatar
+          disableLoading={props.disableLoading}
+          alt={props.alt}
+          src={croppedImage}
+          size={imageSize}
+        />
+      </Badge>
+      <ImageCropperDialog
+        open={openEditDialog}
+        handleClose={handleCloseEditDialog}
+        image={image}
+        croppedImage={getCroppedImage}
+      />
+    </>
+  )
 }
