46adfc114402ad87d077fcee7e6cf39c80d0dddf
matteo2437 <matteo.budriesi@gmail.com>
2021-03-19 20:05:46 +0100

    add waved background

--- cody/ClientApp/src/components/bases/dialog_base.js
+++ cody/ClientApp/src/components/bases/dialog_base.js
@@ -6,15 +6,25 @@ import { DialogContent } from '@material-ui/core';
 import { DialogTitle } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
+import { waves } from 'src/lib/default_values/images/svg_backgrounds';
 
 const useStyles = makeStyles((theme) => ({
   dialogContainer: {
     [theme.breakpoints.up('sm')]: {
       transform: `translate(${theme.drawer.width / 2}px, 0px)`
     },
+    background: theme.palette.background.paperSecondary,
+    backgroundImage: `url(${theme.palette.type === "dark" ? waves.dark : waves.light})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
   },
   title: {
     color: theme.palette.text.secondary,
+  },
+  centeredButtons: {
+    display: "block",
+    textAlign: "center"
   }
 }));
 
@@ -22,8 +32,8 @@ export function DialogBase(props){
   const classes = useStyles();
 
   const areButtons = props.firstButton || props.secondButton
-  const buttons = areButtons ?
-    <DialogActions>
+  const buttons = areButtons ?      
+      <DialogActions className={props.centeredButtons ? classes.centeredButtons : ""}>
       {props.firstButton}
       {props.secondButton}
     </DialogActions>
