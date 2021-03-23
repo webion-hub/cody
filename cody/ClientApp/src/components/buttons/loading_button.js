c51c38825f71cd6d082f7d2724a59286fb420a3e
matteo2437 <matteo.budriesi@gmail.com>
2021-03-23 23:46:48 +0100

    refactoring loading button

--- cody/ClientApp/src/components/buttons/loading_button.js
+++ cody/ClientApp/src/components/buttons/loading_button.js
@@ -4,26 +4,20 @@ import { Button } from '@material-ui/core';
 import { Box } from '@material-ui/core';
 import { CircularProgress } from '@material-ui/core';
 
-
 export function LoadingButton(props){
+  const {variant, color, loading, label, fullWidth, ...otherProps} = props;
   const disabledButton = props.loading || props.disabled
 
   const mainButton = 
     <Button
-      variant={props.variant ? props.variant : "contained"}
-      color={props.color ? props.color : "primary"}
+      {...otherProps}
+      variant={variant ? variant : "contained"}
+      color={color ? color : "primary"}
       disabled={disabledButton}
-      className={props.className}
-      fullWidth={props.fullWidth}
-      endIcon={props.endIcon}
-      startIcon={props.startIcon}
-      onClick={props.onClick}
-      href={props.href}
-      style={props.style}
+      fullWidth={fullWidth}
     >
       {
-        props.loading ? 
-        (
+        loading &&        
           <CircularProgress
             color="secondary"
             size={25}
@@ -31,13 +25,12 @@ export function LoadingButton(props){
               position: "absolute" 
             }}
           />
-        ): null
       }
-      {props.label}
+      {label}
     </Button>
 
   return (
-    props.fullWidth ? 
+    fullWidth ? 
       <Box width={1}>
         {mainButton}
       </Box>
