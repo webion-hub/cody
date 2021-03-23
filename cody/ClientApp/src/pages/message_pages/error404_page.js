171967f185bee6d37fc7ef8fed79b57acc94b293
matteo2437 <matteo.budriesi@gmail.com>
2021-03-23 17:50:01 +0100

    update message pages

--- cody/ClientApp/src/pages/message_pages/error404_page.js
+++ cody/ClientApp/src/pages/message_pages/error404_page.js
@@ -3,14 +3,18 @@ import React from 'react';
 import { Error404 } from 'src/components/illustrations/error404';
 import { MessagePageBase } from 'src/components/bases/message_page_base';
 
+export const errorPageSettings = {
+  image: Error404,
+  title: "Pagina non trovata",
+  subTitle: "Ci scusiamo, probabilmente l'url inserita è sbagliata o non esiste più.",
+  buttonLabel: "Torna alla home",
+  href: '/'
+}
+
 export function Error404Page() {
   return (
     <MessagePageBase
-      image={Error404}
-      title="Pagina non trovata"
-      subTitle="Ci scusiamo, probabilmente l'url inserita è sbagliata o non esiste più."
-      buttonLabel="Torna alla home"
-      href='/'
+      {...errorPageSettings}
     />
   );
 }
--- cody/ClientApp/src/pages/message_pages/unauthorized_page.js
+++ cody/ClientApp/src/pages/message_pages/unauthorized_page.js
@@ -3,14 +3,18 @@ import React from 'react';
 import { Error } from 'src/components/illustrations/error';
 import { MessagePageBase } from 'src/components/bases/message_page_base';
 
+export const errorPageSettings = {
+  image: Error,
+  title: "Errore",
+  subTitle: "Non hai i permessi per accedere a questa pagina.",
+  buttonLabel: "Torna alla home",
+  href: '/'
+}
+
 export function UnauthorizedPage() {
   return (
     <MessagePageBase
-      image={Error}
-      title="Errore"
-      subTitle="Non hai i permessi per accedere a questa pagina"
-      buttonLabel="Torna alla home"
-      href='/'
+      {...errorPageSettings}
     />
   );
 }
