38f465abd47528f689e6e03d01a47d6e72ef7f0e
matteo2437 <matteo.budriesi@gmail.com>
2021-03-24 18:49:45 +0100

    refactoring scrollable chips array

--- cody/ClientApp/src/components/scrollable_chips_array.js
+++ cody/ClientApp/src/components/scrollable_chips_array.js
@@ -41,39 +41,39 @@ export function ScrollableChipsArray(props){
     value(chip); 
   }
 
-  const content = <ul 
+  const emptyListMessage = 
+    <Typography
+      variant="body2"
+    >
+      {props.emptyMessage}
+    </Typography>
+
+  const chipList = chipData.map((data) => {
+    return (
+      <li key={data.key} className={classes.li}>
+        <Chip
+          clickable={props.clickables}
+          color={props.color}
+          onClick={props.getValue ? () => handleOnClick(data) : props.onClick} 
+          icon={
+            data.icon &&
+              <div className={classes.icon}>
+                {data.icon}
+              </div>
+          }
+          label={data.title}
+          onDelete={props.delete ? handleDelete(data) : null}
+          className={classes.chip}
+        />
+      </li>
+    );
+  })
+
+  const content = 
+    <ul 
       className={classes.root}
     >
-      {
-        chipData.length === 0 ?
-          <Typography
-            variant="body2"
-          >
-            {props.emptyMessage}
-          </Typography>
-          :
-          chipData.map((data) => {
-            return (
-              <li key={data.key} className={classes.li}>
-                <Chip
-                  clickable={props.clickables}
-                  color={props.color}
-                  onClick={props.getValue ? () => handleOnClick(data) : props.onClick} 
-                  icon={
-                    data.icon? (
-                      <div className={classes.icon}>
-                        {data.icon}
-                      </div>
-                    ) : null
-                  }
-                  label={data.title}
-                  onDelete={props.delete ? handleDelete(data) : null}
-                  className={classes.chip}
-                />
-              </li>
-            );
-          })
-      }
+      {chipData.length === 0 ? emptyListMessage : chipList}
     </ul>
 
   return(
