79a7f882207f1f0ec49f7130c91df45ee04b52ce
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-03-24 22:41:47 +0100

    Get profile picture by userId

--- cody/Controllers/ProfilePictureController.Get.cs
+++ cody/Controllers/ProfilePictureController.Get.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Cody.Security.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,30 @@ namespace Cody.Controllers
 {
     public partial class ProfilePictureController
     {
+        [HttpGet("{userId}")]
+        [Authorize(Roles = Roles.Admin)]
+        public async Task<IActionResult> GetFor(int userId)
+        {
+            var user = await _dbContext
+                .UserAccounts
+                .Include(u => u.AccountDetail)
+                    .ThenInclude(ad => ad.ProfilePicture)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user is null)
+                return NotFound();
+
+            var picture = user.AccountDetail.ProfilePicture;
+            if (picture is null)
+                return NoContent();
+
+            var fileStream =
+                await _sftp.DownloadFileAsync(picture.FilePath);
+
+            return File(fileStream, picture.ContentType);
+        }
+
+
         [HttpGet]
         [Authorize]
         public async Task<IActionResult> Get()
