63f3eb0e7d75417ab7a4a9928317564e8730994c
matteo2437 <matteo.budriesi@gmail.com>
2021-03-25 20:41:52 +0100

    refactoring create or join organization page

--- cody/ClientApp/src/pages/create_or_join_organization/create_or_join_organization_page.js
+++ cody/ClientApp/src/pages/create_or_join_organization/create_or_join_organization_page.js
@@ -2,23 +2,23 @@ import React, { useEffect } from 'react';
 
 import { BackgroundWithLines } from 'src/components/background_with_lines/background_with_lines';
 
-import { OrganizationsInfo } from 'src/pages/create_or_join_organization/components/organizations_info';
 import { selectActionSettings } from 'src/pages/create_or_join_organization/pages/select_action/select_action';
 
 import { createOrganizationSettings } from 'src/pages/create_or_join_organization/pages/create/create_organization';
 import { createTeamSettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_team';
 import { createSchoolSettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_school';
 import { createCompanySettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_company';
+import { useOrganizationsInfoSettings } from 'src/pages/create_or_join_organization/pages/info/organizations_info';
 
 import { joinOrganizationSettings } from 'src/pages/create_or_join_organization/pages/join/join_organization';
 import { CenterComponentPageBase } from 'src/components/bases/center_component_page_base';
 
-import { TitleInfoContentBase } from 'src/components/bases/title_info_content_base';
+import { TitleInfoContentBase } from 'src/components/bases/title_info_content_base/title_info_content_base';
 
 
 export function CreateOrJoinOrganization(){
-  const infoRef = React.createRef();
   const [contentSetting, setContentSetting] = React.useState(selectActionSettings);
+  const organizationsInfoSettings = useOrganizationsInfoSettings()
 
   useEffect(() => {
     const hash = window.location.hash
@@ -35,6 +35,7 @@ export function CreateOrJoinOrganization(){
       'createschool': () => setContentSetting(createSchoolSettings),
       'createcompany': () => setContentSetting(createCompanySettings),
       'join': () => setContentSetting(joinOrganizationSettings),
+      'info': () => setContentSetting(organizationsInfoSettings),
     }[hash];
 
     action ? 
@@ -42,25 +43,15 @@ export function CreateOrJoinOrganization(){
   }
 
   return(
-    <>
-      <CenterComponentPageBase>
-        <TitleInfoContentBase
-          title={contentSetting.title}
-          width={contentSetting.width}
-          height={contentSetting.height}
-          href={contentSetting.href}
-          onBack={contentSetting.onBack}
-          infoRef={infoRef}
-        >
-          <contentSetting.component/>
-        </TitleInfoContentBase>
-        <BackgroundWithLines 
-          height={1}
-        /> 
-      </CenterComponentPageBase>
-      <div ref={infoRef}>
-        <OrganizationsInfo/>
-      </div>
-    </>
+    <CenterComponentPageBase>
+      <TitleInfoContentBase
+        {...contentSetting}
+      >
+        <contentSetting.component/>
+      </TitleInfoContentBase>
+      <BackgroundWithLines 
+        height={1}
+      /> 
+    </CenterComponentPageBase>
   );
 }
