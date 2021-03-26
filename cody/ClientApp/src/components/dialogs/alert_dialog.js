ec180c32eeb78446e0ffa162cbe68a0aa103c3d6
matteo2437 <matteo.budriesi@gmail.com>
2021-03-26 22:36:50 +0100

    fix bug

--- cody/ClientApp/src/components/dialogs/alert_dialog.js
+++ cody/ClientApp/src/components/dialogs/alert_dialog.js
@@ -3,17 +3,25 @@ import React from 'react';
 
 import { Button } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
-import { Divider } from '@material-ui/core';
 import { List } from '@material-ui/core';
 import { ListItem } from '@material-ui/core';
 import { ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { BasePhotoText } from 'src/components/bases/base_photo_text'
 import { DialogBase } from 'src/components/bases/dialog_base';
 
 import { Error } from 'src/components/illustrations/error';
 
+export const useStyles = makeStyles((theme) => ({
+  paperClassName: {
+    maxWidth: 632,
+    width: "100%"
+  }
+}));
+
 export function AlertDialog(props){
+	const classes = useStyles();
  
   const handleClose = () => {
     const {onClose} = props;
@@ -25,11 +33,10 @@ export function AlertDialog(props){
     const cleanItems = items.filter(item => item !== undefined && item !== null)
     const getItems = 
       cleanItems.map((item, index) => 
-        <div key={item}>
+        <div key={index}>
           <ListItem>
             <ListItemText primary={item} />
           </ListItem>
-          <Divider />
         </div>
       );
     
@@ -38,6 +45,7 @@ export function AlertDialog(props){
 
   return(
     <DialogBase
+      paperClassName={classes.paperClassName}
       title="C'è stato un errore"
       open={props.open}
       onClose={handleClose}
@@ -59,7 +67,6 @@ export function AlertDialog(props){
             Ci sono i seguenti errori:
           </Typography>,
           <List component="nav">
-            <Divider />
             {getItems()}
           </List>
         ]}
