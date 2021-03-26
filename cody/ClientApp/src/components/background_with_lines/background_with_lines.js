90a36e845414447ff911a8cb2ac461c2941e3761
matteo2437 <matteo.budriesi@gmail.com>
2021-03-26 22:36:16 +0100

    refactoring background with lines

--- cody/ClientApp/src/components/background_with_lines/background_with_lines.js
+++ cody/ClientApp/src/components/background_with_lines/background_with_lines.js
@@ -1,9 +1,10 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 
 import { Line } from './components/line';
+import { useGetSize } from 'src/lib/hooks/use_get_size';
 
 const useStyles = makeStyles((theme) => ({
 	linesBox: {
@@ -21,53 +22,26 @@ const useStyles = makeStyles((theme) => ({
 export function BackgroundWithLines(props){
   const classes = useStyles();
 	const box = useRef();
-
-  const [elementsNumber, setElementsNumber] = React.useState(0);
-  useLayoutEffect(() => {
-    window.addEventListener('resize', updateElementsNumber);
-    updateElementsNumber();
-    return () => window.removeEventListener('resize', updateElementsNumber);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-	const updateElementsNumber = () => {
-		const elementsFromBox = box.current? 
-			Math.round(box.current.offsetHeight/100) : 0
-
-		const height = props.height === 1 ? window.innerHeight : props.height;
-		const elementsFromHeight = Math.round(height/100)
-
-		const extraLine = props.extraLine ? 1 : 0
-		const removeLine = props.removeLine ? -1 : 0
-
-		const elements = props.height? 
-			elementsFromHeight : elementsFromBox
-
-		setElementsNumber(elements + extraLine + removeLine);
-	}
-
-	const lines = [];
-	for(var i=0; i < elementsNumber; i++){
-		lines.push(
-			<Grid
-				key={i}
-				container
-				direction="row"
-				justify="space-between"
-			>
-				<Line/>
-				<Line/>
-			</Grid>
-		);
-	}
+	const boxHeight = useGetSize(box).height;
+	const elementsNumber = Math.round(boxHeight / 100);
 
   return(
 		<div
 			ref={box}
 			className={classes.linesBox}
 		>
-			{lines.map(line => line)}
+			{[...Array(elementsNumber)]
+					.map((_, index) => (
+						<Grid
+							key={index}
+							container
+							direction="row"
+							justify="space-between"
+						>
+							<Line/>
+							<Line/>
+						</Grid>
+				))}
 		</div>
   );
 }
