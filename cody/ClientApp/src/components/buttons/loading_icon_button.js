e4ed2d150becb38a71935c0bbbe413f0aded065f
matteo2437 <matteo.budriesi@gmail.com>
2021-03-26 19:04:29 +0100

    create loading icon button

---
+++ cody/ClientApp/src/components/buttons/loading_icon_button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { IconButton } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loadingIconButton :{
+    position: "absolute",
+    top: 0,
+  }
+}));
+
+export function LoadingIconButton(props){
+  const classes = useStyles();
+
+  const {variant, color, loading, ...otherProps} = props;
+  const disabledButton = props.loading || props.disabled
+
+  return (
+    <>
+      <IconButton
+        {...otherProps}
+        variant={variant ? variant : "contained"}
+        color={color ? color : "primary"}
+        disabled={disabledButton}
+      >
+        {props.icon}
+      </IconButton>
+      {
+        loading &&
+          <div className={classes.loadingIconButton}>
+            <CircularProgress size={48}/>
+          </div>
+      }
+    </>
+  ) 
+}
