edc526175bab092f81c4c402325bfe7e5eb79a17
matteo2437 <matteo.budriesi@gmail.com>
2021-03-28 00:00:18 +0100

    create list with virtualizer

---
+++ cody/ClientApp/src/components/list_with_virtualizer/list_with_virtualizer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FixedSizeList } from 'react-window';
+
+const renderRow = (getListItem) => (props) => {
+  const { index, style } = props;
+
+  return getListItem(index, style);
+}
+
+renderRow.propTypes = {
+  index: PropTypes.number.isRequired,
+  style: PropTypes.object.isRequired,
+};
+
+export function ListWithVirtualized(props) {
+  const {getListItem, ...others} = props;
+
+  return (
+    <FixedSizeList
+      {...others}
+    >
+      {renderRow(props.getListItem)}
+    </FixedSizeList>
+  );
+}
+
