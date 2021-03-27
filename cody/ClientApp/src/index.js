b361ca4cc996a600360d82fb155b22d57a107712
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-03-27 17:42:19 +0100

    Register Requests error handler

--- cody/ClientApp/src/index.js
+++ cody/ClientApp/src/index.js
@@ -5,8 +5,15 @@ import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+import Requests from 'src/lib/requests';
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+
+Requests.onError = (reason) => {
+  console.log(reason);
+};
+
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
