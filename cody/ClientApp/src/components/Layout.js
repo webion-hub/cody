6102d576ee82482c6980ff20c0f074a797c8ad82
matteo2437 <matteo.budriesi@gmail.com>
2021-03-28 23:02:01 +0200

    add nav menu component

--- cody/ClientApp/src/components/Layout.js
+++ cody/ClientApp/src/components/Layout.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { CustomizableMenu } from 'src/components/nav_menu/customizable_menu';
+import { CustomizableMenu } from 'src/components/nav_menu/nav_menu';
 import { MuiThemeProvider } from '@material-ui/core/styles';  
 import { CssBaseline } from "@material-ui/core";
 import { useMediaQuery } from '@material-ui/core';
 
 import { ThemeController } from "src/lib/default_values/themes/theme_controller";
+
+import { CustomAppBar } from 'src/components/nav_menu/custom_appbar/custom_appbar';
+import { CustomSideBar } from 'src/components/nav_menu/custom_sidebar/custom_sidebar';
+import { CustomSideBarOnMobile } from 'src/components/nav_menu/custom_sidebar_on_mobile/custom_sidebar_on_mobile';
+
+import { appBarElements } from 'src/components/nav_menu/sections/appbar_elements';
+import { sideBarItems } from 'src/components/nav_menu/sections/sidebar_items/sidebar_items';
+
+import { NavMenu } from 'src/components/nav_menu/nav_menu';
 
 const themeController = new ThemeController();
 
@@ -15,9 +24,15 @@ export function Layout(props){
   return (
     <MuiThemeProvider theme = {themeController.getTheme(theme)}>
       <CssBaseline />
-      <CustomizableMenu>
+      <NavMenu
+        appBarElements={appBarElements}
+        appBar={CustomAppBar}
+        sideBarItems={sideBarItems}
+        sideBar={CustomSideBar}
+        sideBarOnMobile={CustomSideBarOnMobile}
+      >
         {props.children}
-      </CustomizableMenu>
+      </NavMenu>
     </MuiThemeProvider>        
   );
 }
---
+++ cody/ClientApp/src/components/nav_menu/nav_menu.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+  },
+	children: {
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: theme.drawer.width,
+      width: "100%",
+      transition: "all 0.25s",
+    },
+  }
+}));
+
+export function NavMenu(props){
+  const classes = useStyles();
+  const theme = useTheme();
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'), { noSsr: true });
+  const [openMobileDrawer, setOpenMobileDrawer] = React.useState(false);
+
+  const children = 
+    <main className={classes.content}>
+      <div className={classes.children}>
+        {props.children}
+      </div>
+    </main>
+
+  return (
+    <div className={classes.root}>
+      <props.appBar
+        toggleMobileDrawer={() => setOpenMobileDrawer(!openMobileDrawer)}
+        appBarElements={props.appBarElements}
+      >
+        {
+          mobileView ? 
+            <props.sideBarOnMobile
+              onOpenMobileDrawer={() => setOpenMobileDrawer(true)}
+              onCloseMobileDrawer={() => setOpenMobileDrawer(false)}
+              openMobileDrawer={openMobileDrawer}
+              sideBarItems={props.sideBarItems}
+            >
+              {children}
+            </props.sideBarOnMobile>
+            :
+            <props.sideBar
+              sideBarItems={props.sideBarItems}
+            >
+              {children}
+            </props.sideBar>
+        }
+      </props.appBar>
+    </div>
+  )
+}
