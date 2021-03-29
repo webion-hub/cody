d474f7187ae174c051b86f0c76f5b3779521b816
matteo2437 <matteo.budriesi@gmail.com>
2021-03-29 19:42:22 +0200

    complete sidebar on mobile

--- cody/ClientApp/src/components/nav_menu/custom_sidebar_on_mobile/custom_sidebar_on_mobile.js
+++ cody/ClientApp/src/components/nav_menu/custom_sidebar_on_mobile/custom_sidebar_on_mobile.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer, Button } from '@material-ui/core';
+import { List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer, Button, Divider } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { PageController } from 'src/lib/page_controller';
 import { DialogBase } from 'src/components/bases/dialog_base';
 
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    [theme.breakpoints.down('xs')]: {
+      width: "80vw"
+    },
+  }
+}));
+
 export function CustomSideBarOnMobile(props){
+  const classes = useStyles();
   const theme = useTheme();
   const sideBarItems = props.sideBarItems;
   const [dialogContet, setDialogContet] = React.useState("")
@@ -45,30 +55,39 @@ export function CustomSideBarOnMobile(props){
         }}
       >
         <List>
-          {sideBarItems.map((element, index) =>
-            <ListItem 
-              key={index}
-              button
-              onClick={handleOnClick(element)}  
-            >
-              <ListItemIcon>
-                {element.icon}
-              </ListItemIcon>
-              <ListItemText>
-                {element.label}
-              </ListItemText>
-            </ListItem>
-          )}
+          {sideBarItems.map((element, index) => {
+					  const isHidden = element.skipOnMobile || element.hideWhen
+            if(isHidden)
+              return;
+            return (
+              <>
+                <ListItem 
+                  key={index}
+                  button
+                  onClick={handleOnClick(element)}  
+                >
+                  <ListItemIcon>
+                    {element.icon}
+                  </ListItemIcon>
+                  <ListItemText>
+                    {element.label}
+                  </ListItemText>
+                </ListItem>
+                <Divider/>
+              </>
+            )
+          })}
         </List>
       </SwipeableDrawer>
       {props.children}
       <DialogBase
+        className={classes.dialog}
         open={dialogContet !== ""}
         onClose={() => setDialogContet("")}
         firstButton={
           <Button
             variant="contained"
-            color="secondary"
+            color="primary"
             onClick={() => setDialogContet("")}
           >
             Chiudi
