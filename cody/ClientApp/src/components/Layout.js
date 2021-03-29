3bef54440418434c214d9c31fa1f31b5e00fa08e
matteo2437 <matteo.budriesi@gmail.com>
2021-03-29 19:44:00 +0200

    update paths

--- cody/ClientApp/src/components/Layout.js
+++ cody/ClientApp/src/components/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { CustomizableMenu } from 'src/components/nav_menu/nav_menu';
 import { MuiThemeProvider } from '@material-ui/core/styles';  
 import { CssBaseline } from "@material-ui/core";
 import { useMediaQuery } from '@material-ui/core';
@@ -11,13 +10,14 @@ import { CustomSideBar } from 'src/components/nav_menu/custom_sidebar/custom_sid
 import { CustomSideBarOnMobile } from 'src/components/nav_menu/custom_sidebar_on_mobile/custom_sidebar_on_mobile';
 
 import { appBarElements } from 'src/components/nav_menu/sections/appbar_elements';
-import { sideBarItems } from 'src/components/nav_menu/sections/sidebar_items/sidebar_items';
+import { useSideBarItems } from 'src/components/nav_menu/sections/sidebar_items/sidebar_items';
 
 import { NavMenu } from 'src/components/nav_menu/nav_menu';
 
 const themeController = new ThemeController();
 
 export function Layout(props){
+  const sideBarItems = useSideBarItems()
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const theme = prefersDarkMode ? "dark" : "light";
 
