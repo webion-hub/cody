336a419e13db99ce39000b17f281761f705ffdb6
matteo2437 <matteo.budriesi@gmail.com>
2021-03-30 21:26:41 +0200

    create organization list item

---
+++ cody/ClientApp/src/components/organization_list_item.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ListItem } from '@material-ui/core'
+
+import { PageController } from 'src/lib/page_controller';
+
+export function OrganizationListItem(props){
+  const organizationId = props.organizationId;
+  const organizationUrl = `/organizations/${organizationId}`
+
+  const openOrganization = (e) => {
+    PageController.push(organizationUrl, e)
+  }
+   
+  return(
+    <ListItem 
+      ContainerProps={{ style: props.style }}
+      ContainerComponent="div" 
+      key={props.index}
+      className={props.className}
+      button
+      onClick={openOrganization}
+      href={organizationUrl}
+      component="a"
+      disabled={props.disabled}
+    >  
+      {props.children}
+    </ListItem>
+  )
+}
