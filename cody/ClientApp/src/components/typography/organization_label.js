4b95e74df2488a2791e62b2a487499b137e8948e
matteo2437 <matteo.budriesi@gmail.com>
2021-03-31 20:26:41 +0200

    add translateIconY prop to organization label

--- cody/ClientApp/src/components/typography/organization_label.js
+++ cody/ClientApp/src/components/typography/organization_label.js
@@ -4,23 +4,23 @@ import { makeStyles } from '@material-ui/core/styles';
 import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleRounded';
 
 const useStyles = makeStyles((theme) => ({
-  verifiedOrganization: {
-    transform: "translate(0px, 4px)",
+  verifiedOrganization: props => ({
+    transform: props.translateIconY ? `translate(0px, ${props.translateIconY}px)` : "translate(0px, 4px)",
     marginLeft: theme.spacing(1),
     fontSize: 18,
     color: theme.palette.text.secondary
-  }
+  })
 }));
 
-export const OrganizationLabel = ({organization}) => {
-  const classes = useStyles();
+export const OrganizationLabel = ({organization, translateIconY}) => {
+  const classes = useStyles({translateIconY});
 
-  if(organization.state.hasBeenVerified)
+  if(organization?.state.hasBeenVerified)
     return (
       <>
-        {organization.name}<CheckCircleRoundedIcon className={classes.verifiedOrganization} fontSize="small"/>
+        {organization?.name}<CheckCircleRoundedIcon className={classes.verifiedOrganization} fontSize="small"/>
       </>
     )
   else
-    return organization.name
+    return organization? organization.name : null
 }
