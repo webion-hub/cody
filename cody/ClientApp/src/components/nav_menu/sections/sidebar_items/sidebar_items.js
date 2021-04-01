c7cd9288f04fb4b3c1050c7baebbfe8c7684a7cf
matteo2437 <matteo.budriesi@gmail.com>
2021-04-01 15:37:49 +0200

    remove unused parameters in sidebar items

--- cody/ClientApp/src/components/nav_menu/sections/sidebar_items/sidebar_items.js
+++ cody/ClientApp/src/components/nav_menu/sections/sidebar_items/sidebar_items.js
@@ -5,13 +5,7 @@ import { SideBarThemeIcon } from './items/sidebar_theme_icon';
 import { SideBarOrganizationList } from './items/sidebar_ogranizations_list/sidebar_organizations_list';
 import { SideBarOrganizationListDrawerContent } from './items/sidebar_ogranizations_list/drawer_content/sidebar_organizations_list_drawer_content';
 
-import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import GroupWorkRoundedIcon from '@material-ui/icons/GroupWorkRounded';
-import ListRoundedIcon from '@material-ui/icons/ListRounded';
-import BrightnessMediumIcon from '@material-ui/icons/BrightnessMedium';
-
 import { UserContext } from 'src/components/user_controller_context/user_controller_context';
-import { SideBarCreateOrJoinOrganizationIcon } from './items/sidebar_create_or_join_organization_icon';
 
 
 export const useSideBarItems = () => {
@@ -22,31 +16,17 @@ export const useSideBarItems = () => {
   const sideBarItems = [
     {
       item: SideBarHomeIcon,
-      label: "Home",
       href: "/",
-      icon: <HomeRoundedIcon/>
     },
     {
       identifier: "organizationList",
       item: SideBarOrganizationList,
       drawerContent: <SideBarOrganizationListDrawerContent/>,
       width: 500,
-      label: "Le tue organizzazioni",
-      icon: <ListRoundedIcon/>,
       hideWhen: isNotLogged
     },
     {
-      item: SideBarCreateOrJoinOrganizationIcon,
-      label: "Crea o unisciti ad una organizzazione",
-      icon: <GroupWorkRoundedIcon/>,
-      href: "/organization",
-      skipOnBigScreen: isLogged,
-    },
-    {
       item: SideBarThemeIcon,
-      label: "Theme",
-      href: "/",
-      icon: <BrightnessMediumIcon/>
     },
   ]
 
