cb4df326fc11be98a8b9d4dbd36344a7dc240f07
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-01 22:11:14 +0200

    Update db query

--- cody/DbQueries/create_bookmarked_organization.sql
+++ cody/DbQueries/create_bookmarked_organization.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS favorite_organization;
+DROP TABLE IF EXISTS bookmarked_organization;
 
-CREATE TABLE favorite_organization (
+CREATE TABLE bookmarked_organization (
 	user_account_id int NOT NULL REFERENCES user_account(id) ON DELETE CASCADE,
 	organization_id int NOT NULL REFERENCES organization(id) ON DELETE CASCADE,
 	
