5482424aab50b92769df1cc36105c4d217702ca1
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-02 18:02:56 +0200

    Add matchExact filters to organizations

--- cody/QueryExtensions/Organization.QueryFilters.cs
+++ cody/QueryExtensions/Organization.QueryFilters.cs
@@ -9,16 +9,21 @@ namespace Cody.QueryExtensions
     {
         public static IQueryable<Organization> DefaultMatch(this QueryFilter<Organization> self)
         {
-            return self.Where(k => o =>
-                (k.MustHave("logo") && o.Detail.Logo != null) ||
-                (k.MustHave("cover") && o.Detail.Cover != null) ||
-                (k.MustHave("website") && o.Detail.Website != null) ||
+            return self.Where(k => k.RequestedProperty.IsRequested
+                ? k.RequestedProperty.MustNotBeNull
+                    ? o =>
+                        (k.RequestedProperty.PropertyName == "logo" && o.Detail.Logo != null) ||
+                        (k.RequestedProperty.PropertyName == "cover" && o.Detail.Cover != null) ||
+                        (k.RequestedProperty.PropertyName == "website" && o.Detail.Website != null)
+                    : o =>
+                        (k.RequestedProperty.PropertyName == "id" && o.Id.ToString() == k.RequestedProperty.Value) ||
+                        (k.RequestedProperty.PropertyName == "name" && o.Name == k.RequestedProperty.Value)
+                : o =>
+                    k.AsEnum<OrganizationKind>() == o.Kind ||
 
-                k.AsEnum<OrganizationKind>() == o.Kind ||
-
-                Regex.IsMatch(o.Name, k.Pattern, RegexOptions.IgnoreCase) ||
-                Regex.IsMatch(o.Detail.Location, k.Pattern, RegexOptions.IgnoreCase) ||
-                Regex.IsMatch(o.Detail.Website, k.Pattern, RegexOptions.IgnoreCase)
+                    Regex.IsMatch(o.Name, k.Pattern, RegexOptions.IgnoreCase) ||
+                    Regex.IsMatch(o.Detail.Location, k.Pattern, RegexOptions.IgnoreCase) ||
+                    Regex.IsMatch(o.Detail.Website, k.Pattern, RegexOptions.IgnoreCase)
             );
         }
     }
