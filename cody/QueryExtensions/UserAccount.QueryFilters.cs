7ff3599150f290c85b254acf5e7f5b03d1a675a4
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-02 18:23:57 +0200

    Match exact id for users

--- cody/QueryExtensions/UserAccount.QueryFilters.cs
+++ cody/QueryExtensions/UserAccount.QueryFilters.cs
@@ -9,15 +9,20 @@ namespace Cody.QueryExtensions
     {
         public static IQueryable<UserAccount> DefaultMatch(this QueryFilter<UserAccount> self)
         {
-            return self.Where(k => u =>
-                u.AccountDetail.BirthDate == k ||
-                u.AccountDetail.RegistrationDate == k ||
+            return self.Where(k => k.RequestedProperty.IsRequested
+                ? k.RequestedProperty.MustMatchExact
+                    ? u => 
+                        (k.RequestedProperty.PropertyName == "id" && u.Id.ToString() == k.RequestedProperty.Value)
+                    : u => false
+                : u =>
+                    u.AccountDetail.BirthDate == k ||
+                    u.AccountDetail.RegistrationDate == k ||
 
-                Regex.IsMatch(u.Id.ToString(), k.Pattern) ||
-                Regex.IsMatch(u.Username, k.Pattern, RegexOptions.IgnoreCase) ||
-                Regex.IsMatch(u.Email, k.Pattern, RegexOptions.IgnoreCase) ||
-                Regex.IsMatch(u.AccountDetail.Name, k.Pattern, RegexOptions.IgnoreCase) ||
-                Regex.IsMatch(u.AccountDetail.Surname, k.Pattern, RegexOptions.IgnoreCase)
+                    Regex.IsMatch(u.Id.ToString(), k.Pattern) ||
+                    Regex.IsMatch(u.Username, k.Pattern, RegexOptions.IgnoreCase) ||
+                    Regex.IsMatch(u.Email, k.Pattern, RegexOptions.IgnoreCase) ||
+                    Regex.IsMatch(u.AccountDetail.Name, k.Pattern, RegexOptions.IgnoreCase) ||
+                    Regex.IsMatch(u.AccountDetail.Surname, k.Pattern, RegexOptions.IgnoreCase)
             );
         }
     }
