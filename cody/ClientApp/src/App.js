4315f757eb73cd2a62a3f14b615a7603bbb77535
Davide Messori <davide.messori@outlook.com>
2021-04-02 15:21:01 +0200

    fix git Home problem

--- cody/ClientApp/src/App.js
+++ cody/ClientApp/src/App.js
@@ -18,7 +18,7 @@ const SignUp = lazy(() => import('./pages/sign_up/SignUp'));
 const Error404Page = lazy(() => import('./pages/message_pages/Error404Page'));
 const EmailValidPage = lazy(() => import('./pages/message_pages/email_valid_page/EmailValidPage'));
 const UnauthorizedPage = lazy(() => import('./pages/message_pages/UnauthorizedPage'));
-const Home = lazy(() => import('./pages/home/Home'));
+const Home = lazy(() => import('./pages/home/home'));
 const Account = lazy(() => import('./pages/account/account'));
 const Test = lazy(() => import('./pages/test'));
 const AdminPage = lazy(() => import('./pages/admin_pages/AdminPage'));
