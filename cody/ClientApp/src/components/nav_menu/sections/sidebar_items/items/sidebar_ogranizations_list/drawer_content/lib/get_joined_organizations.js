a9623156c571462c34978fe39d7c3938fe34a325
matteo2437 <matteo.budriesi@gmail.com>
2021-04-03 14:47:15 +0200

    add get joined organizations

---
+++ cody/ClientApp/src/components/nav_menu/sections/sidebar_items/items/sidebar_ogranizations_list/drawer_content/lib/get_joined_organizations.js
@@ -0,0 +1,26 @@
+import { User } from 'src/lib/server_calls/user';
+
+export const getJoinedOrganizations = (settings) => {
+  const {
+    searchValue,
+    showOnlyBookmarked 
+  } = settings
+
+  return new Promise(resolve => {
+    if(showOnlyBookmarked === "showOnlyBookmarked"){
+      User
+        .getBookmarkedOrganizations({
+          filter: searchValue,
+        })
+        .then(data => resolve(data))
+      
+      return;
+    }
+
+    User
+      .getJoinedOrganizations({
+        filter: searchValue,
+      })
+      .then(data => resolve(data))
+  })
+}
