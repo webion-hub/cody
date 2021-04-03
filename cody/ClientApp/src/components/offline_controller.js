57e2fa2c9ae435362aaef44c41816817cb93b351
matteo2437 <matteo.budriesi@gmail.com>
2021-04-03 21:02:14 +0200

    add offline controller component

---
+++ cody/ClientApp/src/components/offline_controller.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { PageController } from 'src/lib/page_controller';
+
+import OfflinePage from 'src/pages/message_pages/OfflinePage';
+import { SnackbarAlert } from 'src/components/snackbar_alert';
+
+
+export function OfflineController(props){
+  const [openSnackBar, setOpenSnackBar] = React.useState(false)
+  const [isOffline, setIsOffline] = React.useState(false)
+  const [showOfflinePage, setShowOfflinePage] = React.useState(false)
+  
+  useEffect(() => {
+		const reactDomLoaded = new CustomEvent('reactDomLoaded');
+		document.dispatchEvent(reactDomLoaded)
+  }, [])
+
+  window.addEventListener('offline', _=> {
+    setIsOffline(true)
+    setOpenSnackBar(true)
+  });
+
+  window.addEventListener('online', _=> {
+    setIsOffline(false)
+    setShowOfflinePage(false)
+    setOpenSnackBar(true)
+  });
+
+  PageController.listen(_=> {
+    if(isOffline)
+      setShowOfflinePage(true)
+  })
+
+  return (
+    <>
+      <SnackbarAlert open={openSnackBar} onClose={() => setOpenSnackBar(false)} severity={isOffline ? "error" : "success"}>
+        {isOffline ? "Sei offline" : "Sei di nuovo online"}
+      </SnackbarAlert>
+      {showOfflinePage ? <OfflinePage/> : props.children}
+    </>
+  )
+}
