002bd73e6eb40187f265593f921587cbda6a0a4b
matteo2437 <matteo.budriesi@gmail.com>
2021-04-03 21:03:27 +0200

    fix admin page lag

--- cody/ClientApp/src/pages/admin_pages/components/data_tab.js
+++ cody/ClientApp/src/pages/admin_pages/components/data_tab.js
@@ -1,9 +1,5 @@
 export function DataTab(props){
-	return (
-		<div
-			hidden={props.index !== props.tabValue}
-		>
-			{props.children}
-		</div>
-	);
+	if(props.index !== props.tabValue)
+		return null;
+	return props.children;
 }
