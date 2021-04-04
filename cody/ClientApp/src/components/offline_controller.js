0109d5ee132e88c0735098f4ee3825b6a869eb09
matteo2437 <matteo.budriesi@gmail.com>
2021-04-04 18:27:19 +0200

    fix bug

--- cody/ClientApp/src/components/offline_controller.js
+++ cody/ClientApp/src/components/offline_controller.js
@@ -10,6 +10,11 @@ export function OfflineController(props){
   const [isOffline, setIsOffline] = React.useState(false)
   const [showOfflinePage, setShowOfflinePage] = React.useState(false)
   
+  const unListen = PageController.listen(_=> {
+    if(isOffline)
+      setShowOfflinePage(true)
+  })
+
   useEffect(() => {
 		const reactDomLoaded = new CustomEvent('reactDomLoaded');
 		document.dispatchEvent(reactDomLoaded)
@@ -21,15 +26,12 @@ export function OfflineController(props){
   });
 
   window.addEventListener('online', _=> {
+    unListen()
     setIsOffline(false)
     setShowOfflinePage(false)
     setOpenSnackBar(true)
   });
 
-  PageController.listen(_=> {
-    if(isOffline)
-      setShowOfflinePage(true)
-  })
 
   return (
     <>
