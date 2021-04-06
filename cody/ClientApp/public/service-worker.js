c3fd9246a3f5971136dba9e727d33a477f2b292a
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-06 02:37:31 +0200

    Update service worker
    Support offline page, cache static files

--- cody/ClientApp/public/service-worker.js
+++ cody/ClientApp/public/service-worker.js
@@ -1,32 +1,86 @@
-
-const cacheName = 'cody-cache-v1';
+const codyCache = 'cody-cache-v1';
 const urlsToCache = [
   '/images/forest.webp',
+  '/icons/cody-lg.webp',
+  '/icons/cody-sm.png',
+  '/offline.html',
+  '/offline.css',
+  '/index.css',
+  '/index.js',
 ];
 
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
-      .open(cacheName)
+      .open(codyCache)
       .then(cache => cache.addAll(urlsToCache))
   );
-  console.log(`sw - cached ${urlsToCache}`);
 });
 
-self.addEventListener('activate', _ => {
-  console.log('sw activated');
-});
 
 self.addEventListener('fetch', (event) => {
+  const {request} = event;
   event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => {
-        return response
-          ? response
-          : fetch(event.request);
-      }
-    )
+    getResponse(request)
   );
 });
+
+
+/**
+ * @param {RequestInfo} request 
+ * @returns {Promise<Response>}
+ */
+async function getResponse(request) {
+  return caches
+    .match(request)
+    .then(response => {
+      if (response)
+        return response;
+
+      return fetch(request)
+        .then(response => {
+          maybeCacheResponse(request.url, response.clone());
+          return response;
+        })
+        .catch(_ => getOfflinePage())
+    });
+}
+
+
+/**
+ * @param {string} requestUrl
+ * @param {Response} response
+ * @returns {Promise<void>}
+ */
+ async function maybeCacheResponse(requestUrl, response) {
+  if (!isCacheable(requestUrl))
+    return Promise.resolve();
+
+  return caches
+    .open(codyCache)
+    .then(cache => cache.put(requestUrl, response));
+}
+
+/**
+ * @param {string} requestUrl 
+ */
+function isCacheable(requestUrl) {
+  const cacheableUrls = [
+    /https:\/\/.+\/static\//,
+  ];
+
+  return cacheableUrls.some(regex => {
+    return regex.test(requestUrl);
+  });
+} 
+
+
+/**
+ * @returns {Promise<Response>}
+ */
+async function getOfflinePage() {
+  return caches
+    .open(codyCache)
+    .then(cache => cache.match('/offline.html'));
+}
