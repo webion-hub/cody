f98183d1a238accf6ff0927f353f8540a4e695fe
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-06 22:07:17 +0200

    Add get members endpoint

---
+++ cody/Controllers/Organizations/OrganizationsController.GetMembers.cs
@@ -0,0 +1,50 @@
+﻿using Cody.Extensions;
+using Cody.QueryExtensions;
+using Cody.Utilities.QueryFilters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers.Organizations
+{
+    public partial class OrganizationsController
+    {
+        [HttpGet("{organizationId}/members")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetMembers(
+            [FromRoute] int organizationId,
+            [FromQuery] string filter,
+            [FromQuery] int? limit,
+            [FromQuery] int? offset
+        ) {
+            var members = _dbContext
+                .OrganizationMembers
+                .IncludingUser()
+                .Where(om => om.OrganizationId == organizationId)
+                .OrderBy(om => om.UserAccountId)
+                .Select(om => new
+                {
+                    om.UserAccount.Id,
+                    om.UserAccount.Username,
+                    Role = om.Role.ToString(),
+                })
+                .CreateFilter(filter, FilterKind.SplitWords)
+                .OnMatchExact(rp => u =>
+                    rp.Name == "id" && u.Id.ToString() == rp.Value
+                )
+                .Filter();
+
+            var result = await SearchResult.FormatAsync(
+                results: members,
+                limit: limit,
+                offset: offset
+            );
+
+            return Ok(result);
+        }
+    }
+}
