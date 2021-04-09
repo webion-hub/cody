e8ae3f66085a9a645c6bf9471872471eacf9aea5
matteo2437 <matteo.budriesi@gmail.com>
2021-04-09 18:00:54 +0200

    create list with search

---
+++ cody/ClientApp/src/components/list_with_search/list_with_search.js
@@ -0,0 +1,125 @@
+import React, { useEffect } from 'react';
+import { useTheme, Grid, Paper, LinearProgress, Fade } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import { useMediaQuery } from '@material-ui/core'
+
+import { GenericSearchBar } from 'src/components/pickers/search_bars/generic_search_bar/generic_search_bar';
+import { ListWithScrollUpdater } from 'src/components/list_with_search/component/list_with_scroll_updater';
+import { useListWithSearch } from './hook/use_list_with_search';
+
+const useStyles = makeStyles((theme) => ({
+  listContainer: {
+    marginTop: theme.spacing(1),
+    background: theme.palette.background.paperSecondary
+  },
+  list: {
+    overflow: "overlay",
+  },
+  searchBarStyle: props => ({
+    maxWidth: `calc(100% - ${props.filterComponentWidth}px)`,
+    [theme.breakpoints.down('xs')]: {
+      width: "100%",
+      maxWidth: "calc(100vw - 16px) !important"
+    },
+  }),
+  filterComponent: {
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0
+    },
+  },
+  linearProgress: {
+    marginTop: theme.spacing(0.5),
+    borderRadius: 2
+  }
+}));
+
+export function ListWithSearch(props){
+  const filterComponentRef = React.useRef();
+  const filterComponentWidth = filterComponentRef.current
+    ? filterComponentRef.current.offsetWidth + 16
+    : 0
+    
+  const classes = useStyles({filterComponentWidth});
+	const theme = useTheme();
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'));
+  const {
+    handleScrollEnd,
+    setSearchValue,
+    searchValue,
+    loading,
+    dataList
+  } = useListWithSearch(props)
+
+  const totalLoading = loading.mainLoading || loading.searchLoading
+  const listHeight = mobileView 
+    ? props.listMobileHeight 
+    : props.listHeight
+
+  useEffect(() => {
+    if(props.cleanOnFilterChange)
+      setSearchValue("")
+  }, [props.filter])  
+  
+
+  return(
+    <>
+      <Grid
+        container
+        direction="column"
+      >
+        <Grid
+          container
+          direction="row"
+          alignContent="center"
+        >
+          <GenericSearchBar
+            className={classes.searchBarStyle}
+            background={theme.palette.background.paperSecondary}
+            onChange={setSearchValue}
+            value={searchValue}
+          />
+          {
+            React.Children.map(props.filterComponent, (child, index) => {
+              return React.cloneElement(child, {
+                ref: filterComponentRef, 
+                key: index,
+                className: classes.filterComponent
+              });
+            })
+          }
+        </Grid>
+        <Fade
+          in={totalLoading}
+        >
+          <LinearProgress 
+            color="secondary"
+            className={classes.linearProgress}
+          />
+        </Fade>
+        <Paper className={classes.listContainer}>
+          <ListWithScrollUpdater
+            loading={loading.searchLoading}
+            itemList={dataList?.values}
+            onScrollEnd={handleScrollEnd}
+            className={classes.list}
+            height={listHeight}
+            width={props.width}
+            itemSize={props.itemSize} 
+            overscanCount={10}
+            getListItem={(index, style) => {
+              return (
+                <props.listItem
+                  style={style}
+                  key={index}
+                  data={dataList?.values[index]}
+                  mobileView={mobileView}
+                />
+              )
+            }}
+          />
+        </Paper>
+      </Grid>
+    </>
+  );
+}
