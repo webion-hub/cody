cc18e16fa826771460ab22b28b7286b9b591de61
matteo2437 <matteo.budriesi@gmail.com>
2021-04-12 19:12:42 +0200

    create organization info area

---
+++ cody/ClientApp/src/pages/organization/components/organization_info_area/organization_info_area.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, IconButton } from "@material-ui/core";
+import { useTheme } from '@material-ui/core'
+import { useMediaQuery } from '@material-ui/core'
+
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+
+import OrganizationDescription from "../organization_description";
+import { OrganizationBadgeAvatar } from "./components/organization_badge_avatar";
+import { OrganizationAvatarGroup } from "./components/organization_avatar_group/organization_avatar_group";
+import { OrganizationInfo } from "./components/organization_info";
+
+export const useStyles = makeStyles((theme) => ({
+  organizationInfoContainer: {
+    background: theme.palette.background.backgroundTransparent,
+  },
+  organizationInfoList: {
+    width: "auto",
+    paddingLeft: theme.spacing(2),
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: 0,
+      paddingTop: theme.spacing(2),
+    },
+  },
+  organizationInfoBox:{
+    display: "grid",
+    marginLeft: theme.spacing(3),
+    tableLayout: "fixed",
+    width: `calc(100% - 198px)`,
+    transition: "0.25s all",
+    [theme.breakpoints.down('xs')]: {
+      width: "100%",
+      textAlign: "center",
+      marginTop: theme.spacing(2),
+      marginLeft: 0,
+    },
+  },
+	showDescription: {
+		width: "100%",
+		textAlign: "center",
+	},
+	expandIcon: props => ({
+		transform: props.showDescription && "rotate(180deg)",
+		transition: "0.25s all"
+	}),
+  descriptionBox: {
+    background: theme.palette.background.backgroundTransparent,
+  }
+}));
+
+export default function OrganizationInfoArea(props){
+	const theme = useTheme();
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'), { noSsr: true });
+  const [showDescription, setShowDescription] = React.useState(false)
+	const classes = useStyles({showDescription});
+
+  const organizationData = props.organizationData
+  
+  return (
+    <>
+			<div className={classes.organizationInfoContainer}>
+				<Grid
+          className={classes.organizationInfoList}
+          style={{position: "relative"}}
+					container
+					direction={mobileView ? "column" : "row"}
+          alignItems="center"
+				>
+					<OrganizationBadgeAvatar
+            id={props.id}
+            organizationData={organizationData}
+            loading={props.loading}
+          />
+          <OrganizationInfo
+            className={classes.organizationInfoBox}
+            organizationData={organizationData}
+            loading={props.loading}
+          />
+          <OrganizationAvatarGroup
+            organization={props.organization}
+          />
+				</Grid>
+        <div className={classes.descriptionBox}>
+          <OrganizationDescription
+            organizationData={organizationData}
+            showDescription={showDescription}
+          />
+          <div className={classes.showDescription}>
+            <IconButton 
+              onClick={_ => setShowDescription(!showDescription)}
+              disabled={props.loading}
+            >
+              <ExpandMoreRoundedIcon className={classes.expandIcon}/>
+            </IconButton>
+          </div>
+        </div>
+			</div>
+    </>
+  );
+}
