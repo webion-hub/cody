761c94be0e8f513b879e71fe6baee685af744af2
matteo2437 <matteo.budriesi@gmail.com>
2021-04-13 19:06:12 +0200

    refactoring join and leave button

---
+++ cody/ClientApp/src/components/buttons/join_organization_button.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+
+import { LoadingButton } from 'src/components/buttons/loading_button';
+import { LoadingIconButton } from 'src/components/buttons/loading_icon_button';
+import { User } from 'src/lib/server_calls/user';
+
+export function JoinOrganizationButton(props){
+  const [loading, setLoading] = React.useState(false);
+  const updateUserOrganizations = new Event('updateUserOrganizations');
+
+  const {
+    organization,
+    onJoin,
+    mobileView,
+  } = props
+
+  const handleJoin = () => {
+    setLoading(true)
+    User
+      .join(organization?.id)
+      .finally(() => {
+        setLoading(false)
+        document.dispatchEvent(updateUserOrganizations)
+        onJoin?.()
+      })
+  }
+
+  const commonProps = {
+    loading: loading,
+    color: "secondary",
+    disabled: organization?.state.hasBeenDeleted,
+    onClick: handleJoin
+  }
+
+  if(mobileView)
+    return(
+      <LoadingIconButton
+        {...commonProps}
+        icon={<AddRoundedIcon/>}
+      />
+    )
+  else
+    return (
+      <LoadingButton
+        {...commonProps}
+        variant="outlined"
+        label="Unisciti"
+        endIcon={<AddRoundedIcon/>}
+      />     
+    )
+}
---
+++ cody/ClientApp/src/components/buttons/leave_organization_button/leave_organization_button.js
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { LoadingButton } from 'src/components/buttons/loading_button';
+import { LoadingIconButton } from 'src/components/buttons/loading_icon_button';
+
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import { LeaveOrganizationDialog } from './leave_organization_dialog';
+
+import { User } from 'src/lib/server_calls/user';
+
+const useStyles = makeStyles((theme) => ({
+  leaveButton: {
+    color: theme.palette.error.main,
+    borderColor: theme.palette.error.main,
+    "&:hover": {
+      color: theme.palette.error.light,
+      borderColor: theme.palette.error.light,
+    }
+  },
+}));
+
+export function LeaveOrganizationButton(props){
+  const classes = useStyles();
+  const [openLeaveDialog, setOpenLeaveDialog] = React.useState(false);
+  const [leaveError, setLeaveError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const updateUserOrganizations = new Event('updateUserOrganizations');
+
+  const {
+    organization,
+    onLeave,
+    mobileView,
+  } = props
+
+  const handleOpenLeaveDialog = () => {
+    setOpenLeaveDialog(true)
+    setLeaveError(null)
+  }
+
+  const handleCloseLeaveDialog = () => {
+    setOpenLeaveDialog(false)
+  }
+
+  const handleLeave = () => {
+    setLeaveError(null)
+    setLoading(true)
+    User.leave({
+      organizationId: organization?.id,      
+      onSuccess: _ => setLeaveError("noError"),
+      onError: () => setLeaveError("C'è stato un errore prova più tardi"),
+      onNotFound: () => setLeaveError("Non sei membro di questa organizzazione!"),
+      onForbidden: () => setLeaveError("Non puoi uscire da un'organizzazione dove sei il propietario!"),    
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    if(leaveError === "noError"){
+      document.dispatchEvent(updateUserOrganizations)
+      onLeave?.()
+      handleCloseLeaveDialog()
+    }
+  }, [leaveError])
+
+  const commonProps = {
+    color: "inherit",
+    disabled: organization?.state.hasBeenDeleted,
+    className: classes.leaveButton,
+    onClick: handleOpenLeaveDialog
+  }
+
+  const leaveOrganizationDialog = 
+    <LeaveOrganizationDialog
+      loading={loading}
+      open={openLeaveDialog}
+      onClose={handleCloseLeaveDialog}
+      onLeave={handleLeave}
+      organizationName={organization?.name}
+      leaveError={leaveError}
+    />
+
+  if(mobileView)
+    return(
+      <>
+        <LoadingIconButton
+          {...commonProps}
+          icon={<ExitToAppRoundedIcon/>}
+        />
+        {leaveOrganizationDialog}
+      </>
+    )
+  else
+    return (
+      <>
+        <LoadingButton
+          {...commonProps}
+          variant="outlined"
+          label="Lascia"
+          endIcon={<ExitToAppRoundedIcon/>}
+        />
+        {leaveOrganizationDialog} 
+      </>
+  
+    )
+}
