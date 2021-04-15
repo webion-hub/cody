32da3e1c54d201efa7ab47e1f7bac1ed6abcbf03
matteo2437 <matteo.budriesi@gmail.com>
2021-04-15 20:11:37 +0200

    create user role icon

---
+++ cody/ClientApp/src/components/user_role_icon.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import StarsRoundedIcon from '@material-ui/icons/StarsRounded';
+import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
+
+export function UserRoleIcon({role, className, fontSize}){
+  const getIcon = () => {
+    const icons = {
+      Owner: StarsRoundedIcon,
+      Admin: AccountCircleRoundedIcon,
+      User: FaceRoundedIcon,
+    }
+
+    return icons[role]
+  }
+
+  const RoleIcon = getIcon()
+  
+  return (
+    <RoleIcon className={className} fontSize="small"/>   
+  )
+}
+
+UserRoleIcon.defaultProps = {
+  fontSize: "small"
+}
