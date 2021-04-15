b980e6b814e5865955362e6d95cf72d3ffed4837
matteo2437 <matteo.budriesi@gmail.com>
2021-04-15 20:08:46 +0200

    convert avatar button to avatar with menu

---
+++ cody/ClientApp/src/components/buttons/avatar_with_menu.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { ButtonBase, Menu } from "@material-ui/core";
+import { CustomAvatar } from "src/components/custom_avatar";
+import { makeStyles } from '@material-ui/core/styles';
+import { useBackgroundWaves } from "src/lib/hooks/use_background_waves";
+import { useMenu } from "src/lib/hooks/use_menu";
+
+export const useStyles = makeStyles((theme) => ({
+  menu: {
+    padding: 0,
+  },
+}));
+
+export function AvatarWithMenu(props){
+	const classes = useStyles();
+  const classWithWaves = useBackgroundWaves()
+  const {
+    handleOpenMenu,
+    handleCloseMenu,
+    isMenuOpen,
+  } = useMenu()
+
+  const {
+    onClick,
+    buttonClassName,
+    children,
+    placement,
+    style,
+    menuContent,
+    onMenuOpen,
+    onMenuClose,
+    ...otherProps
+  } = props
+
+  const handleClick = (e) => {
+    onMenuOpen?.()
+    onClick?.(e)
+    handleOpenMenu(e)
+  }
+  
+  const handleClose = () => {
+    onMenuClose?.()
+    handleCloseMenu()
+  }
+
+  return (
+    <>
+      <ButtonBase
+        onClick={handleClick}
+        className={buttonClassName}
+        style={style}
+      >
+        <CustomAvatar
+          disableLoadingRing
+          {...otherProps}
+        >
+          {children}
+        </CustomAvatar>
+      </ButtonBase>
+      {
+        menuContent &&
+          <Menu
+            MenuListProps={{
+              className: `${classes.menu} ${classWithWaves}`
+            }}
+            anchorEl={isMenuOpen}
+            open={Boolean(isMenuOpen)}
+            onClose={handleClose}
+          >
+            {menuContent}
+          </Menu>
+      }
+    </>
+  )
+}
+
+AvatarBuAvatarWithMenutton.defaultProps = {
+  placement: "bottom"
+}
