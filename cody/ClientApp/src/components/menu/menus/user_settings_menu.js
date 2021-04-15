c2cc33be08531e270ab8fd8cae3a0b38c817560c
matteo2437 <matteo.budriesi@gmail.com>
2021-04-15 20:10:41 +0200

    create user settings menu

---
+++ cody/ClientApp/src/components/menu/menus/user_settings_menu.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { IconButton } from '@material-ui/core'
+
+import MoreVertRoundedIcon from '@material-ui/icons/MoreVertRounded';
+import { useMenu } from "src/lib/hooks/use_menu";
+import { ReportMenuItem } from "src/components/menu/menu_items/report_menu_item";
+import { EditRoleMenuItem } from "src/pages/admin_pages/components/row_menu/edit_role_menu_item";
+import { MenuWithLoading } from "src/components/menu/menu_with_loading";
+import { KickUserMenuItem } from "src/components/menu/menu_items/kick_user_menu_item";
+
+export function UserSettingsMenu({user}){
+  const {
+    handleOpenMenu,
+    handleCloseMenu,
+    isMenuOpen,
+  } = useMenu()
+
+  return (
+    <>
+      <IconButton
+        onClick={handleOpenMenu}
+      >
+        <MoreVertRoundedIcon fontSize="small"/>
+      </IconButton>
+      <MenuWithLoading
+        anchorEl={isMenuOpen}
+        open={Boolean(isMenuOpen)}
+        onClose={handleCloseMenu}
+      >
+        <EditRoleMenuItem
+          hide={user.role === "User"}
+          id={user.id}
+          onMenuClose={handleCloseMenu}
+        />
+        <KickUserMenuItem
+          hide={user.role === "User"}
+        />
+        <ReportMenuItem/>
+      </MenuWithLoading>  
+    </>  
+  )
+} 
