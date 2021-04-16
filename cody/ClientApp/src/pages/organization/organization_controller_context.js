a7574df36af807d523ea769974580a285987a716
matteo2437 <matteo.budriesi@gmail.com>
2021-04-16 20:51:09 +0200

    create organization controller context

---
+++ cody/ClientApp/src/pages/organization/organization_controller_context.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+import Organization from "src/lib/server_calls/organization";
+import { UserOrganizationsController } from "src/lib/user_organizations_controller";
+import { useListener } from "src/lib/hooks/use_listener";
+import { User } from "src/lib/server_calls/user";
+
+export const OrganizationContext = React.createContext({
+	organizationData: null,
+	smallUserList: [],
+	callerIs: "",
+	organization: _ => {},
+	loading: true,
+});
+
+export const OrganizationContextConsumer = OrganizationContext.Consumer;
+
+export const OrganizationControllerContext = ({id, children}) => {
+	const [loading, setLoading] = React.useState(true)
+  const [organizationData, setOrganizationData] = React.useState(null)
+  const [smallUserList, setSmallUserList] = React.useState([])
+  const [callerIs, setCallerIs] = React.useState("")
+	const organization = Organization.withId(id)
+
+  const getOrganizationByPageId = async () => {
+    await organization
+      .getInfo()
+      .then(setOrganizationData)
+  } 
+
+  const getMembers = async () => {
+    await organization
+      .getMembersOf({
+        filter: "",
+        limit: 3,
+        offset: null
+      })
+      .then(setSmallUserList)
+  }
+
+  const getRole = async () => {
+    await User
+      .getRoleIn(id)
+      .then(setCallerIs)
+  }
+
+	const getOrganizationData = async () => {
+		setLoading(true)
+		await getOrganizationByPageId()
+		await getMembers()
+		await getRole()
+		setLoading(false)
+	}
+
+	useListener({
+		eventFunction: getOrganizationData,
+		controller: UserOrganizationsController,
+	}, [id])
+
+
+	const value = {
+    id,
+		organizationData,
+		smallUserList,
+		callerIs,
+		organization,
+		loading,
+	}
+
+	return (
+    <OrganizationContext.Provider value={value}>
+      {children}
+    </OrganizationContext.Provider>
+  );
+}
