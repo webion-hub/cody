19d5707b9c7f62b1f2ae94eb2b48bd9bea46df84
matteo2437 <matteo.budriesi@gmail.com>
2021-04-17 22:44:13 +0200

    start user preview in all organization user dialog

--- cody/ClientApp/src/pages/organization/components/organization_info_area/components/organization_avatar_group/components/all_organization_user_dialog.js
+++ cody/ClientApp/src/pages/organization/components/organization_info_area/components/organization_avatar_group/components/all_organization_user_dialog.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DialogBase } from "src/components/bases/dialog_base";
 import { ListWithSearch } from "src/components/list_with_search/list_with_search";
-import { Button, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
+import { Button, Grid, IconButton, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
 import { CustomAvatar } from "src/components/custom_avatar";
 import { makeStyles } from '@material-ui/core/styles';
 import { BasePhotoText } from "src/components/bases/base_photo_text";
@@ -9,6 +9,9 @@ import { UserGroup } from "src/components/illustrations/user_group";
 import { useTheme } from '@material-ui/core'
 import { useMediaQuery } from '@material-ui/core'
 import { OrganizationContext } from "src/pages/organization/organization_controller_context";
+import { UserSmallSummary } from "src/components/user_small_summary";
+
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 
 export const useStyles = makeStyles((theme) => ({
   listItem: {
@@ -17,43 +20,105 @@ export const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1)
   },
   dialogPaper: {
-    maxWidth: 632,
+    maxWidth: 648,
     width: "100%"
   },
   userList: {
     background: theme.palette.background.backgroundTransparent,
-    backdropFilter: "blur(10px)"
-  }
+    backdropFilter: "blur(10px)",
+  },
+  areaWidth: {
+    maxWidth: 300
+  },
+  image: {
+    transition: "0.25s transform"
+  },
+  hideImage: {
+    transform: "translate(-300px, 0px)"
+  },
+  userSmallSummary: {
+    marginLeft: -12,
+    position: "absolute",
+    animation: `$fade 0.25s linear`,
+  },
+  "@keyframes fade": {
+    "0%": {
+      opacity: 0,
+    },
+    "100%": {
+      opacity: 1,
+    }
+  },
 }));
 
 export function AllOrganizationUserDialog(props){
   const theme = useTheme();
   const mobileView = useMediaQuery(theme.breakpoints.down('xs'), { noSsr: true });
 	const classes = useStyles();
+  
+  const [userData, setUserData] = React.useState(null)
 	const {
     organization,
-	} = React.useContext(OrganizationContext);
+    callerIs,
+  } = React.useContext(OrganizationContext);
+
+  const handleClose = () => {
+    props.onClose()
+    setUserData(null)
+  }
 
   return (
     <DialogBase
       open={props.open}
-      onClose={props.onClose}
+      onClose={handleClose}
       paperClassName={classes.dialogPaper}
       firstButton={
         <Button
           color="primary"
           variant="contained"
-          onClick={props.onClose}
+          onClick={handleClose}
         >
           Chiudi
         </Button>
       }
     >
-      <BasePhotoText
-        imagePadding={4}
-        image={!mobileView && UserGroup}
+      <Grid
+        container
+        direction="row"
       >
+        <Grid
+          className={classes.areaWidth}
+          container
+          direction="column"
+          alignItems="center"
+        >
+          {
+            !mobileView && 
+              <UserGroup 
+                className={`${userData !== null ? classes.hideImage : ""} ${classes.image}`}
+                maxWidth={300} 
+                size="100%" 
+                padding={4}
+              />
+          }
+          {
+            userData !== null &&
+              <div className={classes.userSmallSummary}>
+                <IconButton 
+                  onClick={_ => setUserData(null)}
+                >
+                  <CloseRoundedIcon/>
+                </IconButton>
+                <UserSmallSummary
+                  user={userData}
+                  callerIs={callerIs}
+                />
+              </div>
+
+          }
+        </Grid>
         <ListWithSearch
+          className={classes.areaWidth}
           paperClassName={classes.userList}
           listHeight={400}
           listMobileHeight={window.innerHeight / 2}
@@ -62,12 +127,14 @@ export function AllOrganizationUserDialog(props){
           itemSize={56}
           getList={organization.getMembersOf}
           listItem={AvatarListItem}
+          listItemProps={{
+            onClick: data => setUserData(data)
+          }}
           noDataFoundProps={{
             hide: true,
           }}
-        />     
-      </BasePhotoText>
-
+        /> 
+      </Grid>
     </DialogBase> 
   )
 }
@@ -81,6 +148,7 @@ function AvatarListItem(props){
         className={classes.listItem}
         ContainerComponent="div" 
         button
+        onClick={_ => props.onClick?.(props.data)}
       >
         <ListItemAvatar>
           <CustomAvatar
