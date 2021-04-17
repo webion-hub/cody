079a90a918f355b855a8840585ccdbe527612f61
matteo2437 <matteo.budriesi@gmail.com>
2021-04-17 19:11:09 +0200

    add removeFirstExecution

--- cody/ClientApp/src/lib/hooks/use_listener.js
+++ cody/ClientApp/src/lib/hooks/use_listener.js
@@ -1,12 +1,14 @@
 import { useEffect } from "react"
 
-export const useListener = ({eventFunction, controller, firstExecutionDelay}, dependencies) => {
+export const useListener = ({eventFunction, controller, firstExecutionDelay, removeFirstExecution}, dependencies) => {
   
   useEffect(() => {
 		controller.listen(eventFunction)
 
     const delay = firstExecutionDelay ? firstExecutionDelay : 0
-		setTimeout(() => eventFunction(), delay);
+
+    if(!removeFirstExecution)
+		  setTimeout(() => eventFunction(), delay);
 
 		return _ => controller.unListen(eventFunction)
   }, dependencies)
