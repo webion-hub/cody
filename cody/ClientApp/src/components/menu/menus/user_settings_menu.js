008afd1fdef974843edab0894ee2c0c47209fed7
matteo2437 <matteo.budriesi@gmail.com>
2021-04-17 16:21:28 +0200

    change menu item name

--- cody/ClientApp/src/components/menu/menus/user_settings_menu.js
+++ cody/ClientApp/src/components/menu/menus/user_settings_menu.js
@@ -4,9 +4,9 @@ import { IconButton } from '@material-ui/core'
 import MoreVertRoundedIcon from '@material-ui/icons/MoreVertRounded';
 import { useMenu } from "src/lib/hooks/use_menu";
 import { ReportMenuItem } from "src/components/menu/menu_items/report_menu_item";
-import { EditUserRoleMenuItem } from "src/pages/admin_pages/components/row_menu/edit_user_role_menu_item";
 import { MenuWithLoading } from "src/components/menu/menu_with_loading";
 import { KickUserMenuItem } from "src/components/menu/menu_items/kick_user_menu_item";
+import { EditRoleMenuItem } from "src/components/menu/menu_items/edit_role_menu_item";
 
 export function UserSettingsMenu({user, callerIs}){
   const {
@@ -27,7 +27,7 @@ export function UserSettingsMenu({user, callerIs}){
         open={Boolean(isMenuOpen)}
         onClose={handleCloseMenu}
       >
-        <EditUserRoleMenuItem
+        <EditRoleMenuItem
           hide={callerIs === "User"}
           id={user.id}
           onMenuClose={handleCloseMenu}
