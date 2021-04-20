154b3b117068a2f44a13aa74b645716cdc5cd5f5
matteo2437 <matteo.budriesi@gmail.com>
2021-04-20 22:46:01 +0200

    refactoring add photo

--- cody/ClientApp/src/components/pickers/others/add_photo.js
+++ cody/ClientApp/src/components/pickers/others/add_photo.js
@@ -8,66 +8,44 @@ import { ImageCropperDialog } from 'src/components/dialogs/image_cropper_dialog'
 import { AddPhotoBadgeButton } from './components/add_photo_badge_button';
 import { CustomAvatar } from '../../custom_avatar';
 
-import { ImageOrientation } from 'src/lib/image_orientation';
+import { useAddPhoto } from './hooks/use_add_photo';
 
 export function AddPhoto(props){
-  const [image, setImage] = React.useState(null);
   const [openEditDialog, setOpenEditDialog] = React.useState(false);
-  const [croppedImage, setCroppedImage] = React.useState(props.value);
-
   const theme = useTheme();  
   const mobileView = useMediaQuery(theme.breakpoints.down('xs'));
    
   const responsiveDefaultImageSize = mobileView ? 75 : 100;
   const imageSize = props.imageSize? props.imageSize : responsiveDefaultImageSize;
 
-  useEffect(() => {
-    setCroppedImage(props.value)
-  }, [props.value])
+  const {
+    getImageUploaded,
+    setCroppedImage,
+    deleteImage,
+    image,
+    uploadedImage
+  } = useAddPhoto(props.src)
 
-  const deleteImage = () => {
-    setCroppedImage(null);
-    const {onImageChange} = props;
-    onImageChange(null);
-  };
+  useEffect(_=> {
+    props.onImageChange?.(image);
+  },[image])
 
   const handleCloseEditDialog = () => {
     setOpenEditDialog(false);
   }
 
-  
-  const getCroppedImage = (value) => {
-    setCroppedImage(value)
-    const {onImageChange} = props;
-    onImageChange(value); 
-  }
-
-  /**
-   * @param {React.ChangeEvent<HTMLInputElement>} event
-   */
-   const fileSelectedHandler = (event) => {
-    let file = event.target.files[0];
-    if(file !== null){ 
-      const imageOrientation = new ImageOrientation()
-
-      imageOrientation.
-        fixOrientation(file)
-        .then(blob => {
-          setImage(blob);
-          setOpenEditDialog(true);
-        })
-    }
-
-    event.target.value = null; //reset input file
+  const handleImageUpload = async (event) => {
+    await getImageUploaded(event)
+    setOpenEditDialog(true);
   }
 
   const badgeContent = 
     <AddPhotoBadgeButton
       imageUnderEditing={props.accountEdit}
-      thereIsAnImage={croppedImage}
+      thereIsAnImage={image}
       iconSize={props.iconSize}
       onDelete={deleteImage}
-      onImageChange={fileSelectedHandler}
+      onImageChange={handleImageUpload}
     />
 
   return (
@@ -84,15 +62,15 @@ export function AddPhoto(props){
         <CustomAvatar
           disableLoading={props.disableLoading}
           alt={props.alt}
-          src={croppedImage}
+          src={image}
           size={imageSize}
         />
       </Badge>
       <ImageCropperDialog
         open={openEditDialog}
         handleClose={handleCloseEditDialog}
-        image={image}
-        croppedImage={getCroppedImage}
+        image={uploadedImage}
+        onCroppedImage={setCroppedImage}
       />
     </>
   )
