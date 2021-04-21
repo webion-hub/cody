db04fb03535e9d45a393e9d3b4b15a51d0bed11c
matteo2437 <matteo.budriesi@gmail.com>
2021-04-21 10:02:31 +0200

    create main login dialog

---
+++ cody/ClientApp/src/pages/app/components/main_login_dialog.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { LoginDialog } from 'src/components/dialogs/login_dialog';
+import { useListener } from 'src/lib/hooks/use_listener';
+import { EventsDispatcher } from 'src/lib/events_dispatcher';
+
+export function MainLoginDialog(){
+  const [openLoginDialog, setOpenLoginDialog] = React.useState(false);
+
+  const handleOpenLoginDialog = () => {
+    setOpenLoginDialog(true)
+  }
+
+  const handleCloseLoginDialog = () => {
+    setOpenLoginDialog(false)
+  }
+
+	useListener({
+		eventFunction: handleOpenLoginDialog,
+		controller: EventsDispatcher.setEvent('openLoginDialog'),
+    removeFirstExecution: true,
+  }, [])
+  
+  return (
+    <LoginDialog
+      open={openLoginDialog}
+      onClose={handleCloseLoginDialog}
+      onSuccess={handleCloseLoginDialog}      
+    /> 
+  )
+}
