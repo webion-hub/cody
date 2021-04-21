0fd2f668666940a4f1ebaccf7e17f8b7dd854946
matteo2437 <matteo.budriesi@gmail.com>
2021-04-21 14:49:30 +0200

    create avatar_add_photo

---
+++ cody/ClientApp/src/components/pickers/others/avatar_add_photo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+import { AddPhotoOverlay } from 'src/components/pickers/others/add_photo_overlay';
+import { CustomAvatar } from 'src/components/custom_avatar';
+
+export function AvatarAddPhoto(props){
+  const theme = useTheme();  
+  const mobileView = useMediaQuery(theme.breakpoints.down('xs'));
+
+  const { alt, disableLoading, imageSize, ...other } = props
+  const avatarSize = mobileView ? 75 : 100;
+
+  return (
+    <AddPhotoOverlay
+      {...other}
+    >
+      <CustomAvatar
+        disableLoading={disableLoading}
+        alt={alt}
+        size={imageSize ? imageSize : avatarSize}
+      />
+    </AddPhotoOverlay>
+  )
+}
