6a341cf9913b0ee1de72d33a93ebea6db0dc5510
matteo2437 <matteo.budriesi@gmail.com>
2021-04-23 19:07:46 +0200

    refactoring background with lines

--- cody/ClientApp/src/components/background_with_lines/background_with_lines.js
+++ cody/ClientApp/src/components/background_with_lines/background_with_lines.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
@@ -7,7 +7,7 @@ import { Line } from './components/line';
 import { useGetSize } from 'src/lib/hooks/use_get_size';
 
 const useStyles = makeStyles((theme) => ({
-	linesBox: {
+	linesBox: props => ({
 		position: "absolute",
 		top: 0,
 		zIndex: -2,
@@ -15,33 +15,41 @@ const useStyles = makeStyles((theme) => ({
 		height: "100%",
 		overflow: "hidden",
     whiteSpace: "nowrap",
-		background: theme.palette.background.backgroundGradient
-	},
+		background: props.background ?? theme.palette.background.backgroundGradient
+	}),
 }));
 
-export function BackgroundWithLines(){
-  const classes = useStyles();
+export function BackgroundWithLines(props){
+	const background = props.background
+  const classes = useStyles({background});
 	const box = useRef();
 	const boxHeight = useGetSize(box).height;
 	const elementsNumber = Math.round(boxHeight / 100);
+
+
+	useEffect(() => {
+		console.log(box)
+	}, [box])
+	
+	const lines = [...Array(elementsNumber)]
+		.map((_, index) => (
+			<Grid
+				key={index}
+				container
+				direction="row"
+				justify="space-between"
+			>
+				<Line/>
+				<Line/>
+			</Grid>
+		))
 
   return(
 		<div
 			ref={box}
 			className={classes.linesBox}
 		>
-			{[...Array(elementsNumber)]
-					.map((_, index) => (
-						<Grid
-							key={index}
-							container
-							direction="row"
-							justify="space-between"
-						>
-							<Line/>
-							<Line/>
-						</Grid>
-				))}
+			{lines}
 		</div>
   );
 }
