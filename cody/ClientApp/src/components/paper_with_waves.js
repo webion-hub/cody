87232764332652bbb0ce7abfa1f4ccfddf960d70
matteo2437 <matteo.budriesi@gmail.com>
2021-04-23 19:09:03 +0200

    add color to paper with waves

--- cody/ClientApp/src/components/paper_with_waves.js
+++ cody/ClientApp/src/components/paper_with_waves.js
@@ -4,7 +4,7 @@ import { Paper } from '@material-ui/core';
 import { useBackgroundWaves } from 'src/lib/hooks/use_background_waves';
 
 export function PaperWithWaves(props){
-  const classWithWavedBackground = useBackgroundWaves();
+  const classWithWavedBackground = useBackgroundWaves({color: props.color});
 
   const { className, children, otherProps } = props;
   const classesName = `${classWithWavedBackground} ${className}`
