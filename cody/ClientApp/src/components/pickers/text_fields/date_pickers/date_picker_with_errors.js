1240b3f7077e175db46eeeeae24036be990870b1
matteo2437 <matteo.budriesi@gmail.com>
2021-04-28 19:05:47 +0200

    create date picker with errors

---
+++ cody/ClientApp/src/components/pickers/text_fields/date_pickers/date_picker_with_errors.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { PickerWithErrorAndLabel } from '../../picker_with_error_and_label';
+import { DatePicker } from './date_picker';
+
+export function DatePickerWithErrors(props){
+  const {
+    minBirthDateError,
+    maxBirthDateError,
+    generalError,
+    ...datePickerProps
+  } = props
+
+  const areErrors = 
+    minBirthDateError || 
+    maxBirthDateError || 
+    generalError
+
+const getError = () => {
+  if(generalError)
+    return "Data invalida"
+  if(minBirthDateError)
+    return "Data inferiore al minimo valore"
+  if(maxBirthDateError)
+    return "Data superiore al massimo valore"
+  
+  return null
+}
+
+  return (
+    <PickerWithErrorAndLabel
+      fadeError={areErrors}
+      errorMessage={getError()}
+    >
+      <DatePicker
+        {...datePickerProps}
+      />
+    </PickerWithErrorAndLabel>
+  )
+}
