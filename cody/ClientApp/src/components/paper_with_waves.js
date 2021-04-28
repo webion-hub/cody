8f070df6711cb6e554c4695f30fd66a52bc3b332
matteo2437 <matteo.budriesi@gmail.com>
2021-04-28 19:09:21 +0200

    add ref to paper with waves

--- cody/ClientApp/src/components/paper_with_waves.js
+++ cody/ClientApp/src/components/paper_with_waves.js
@@ -3,21 +3,22 @@ import React from 'react';
 import { Paper } from '@material-ui/core';
 import { useBackgroundWaves } from 'src/lib/hooks/use_background_waves';
 
-export function PaperWithWaves(props){
+export const PaperWithWaves = React.forwardRef((props, ref) => {
   const classWithWavedBackground = useBackgroundWaves({color: props.color});
 
   const { className, children, otherProps } = props;
   const classesName = `${classWithWavedBackground} ${className}`
 
   return(
-    <Paper 
+    <Paper
       {...otherProps}
+      ref={ref}
       className={classesName}
     >
       {children}
     </Paper>
   );
-}
+})
 
 PaperWithWaves.defaultProps = {
   className: ""
