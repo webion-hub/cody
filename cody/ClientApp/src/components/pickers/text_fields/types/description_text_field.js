2d6b7842a29a0e67f37a9beea9fb9178156dfa64
matteo2437 <matteo.budriesi@gmail.com>
2021-04-28 19:06:07 +0200

    create description text field

---
+++ cody/ClientApp/src/components/pickers/text_fields/types/description_text_field.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { TextField } from '@material-ui/core';
+import { FormatLengthController } from 'src/lib/format_controller/utilities/format_length_controller';
+import { PickerWithErrorAndLabel } from 'src/components/pickers/picker_with_error_and_label';
+
+export function DescriptionTextField(props){
+  const {
+    descriptionLength,
+    ...textFildProps
+  } = props
+
+  const maxDescriptionLength = FormatLengthController.set('description').max
+  const tooLength = descriptionLength > maxDescriptionLength
+
+  return(
+    <PickerWithErrorAndLabel
+      leftMessage={`${descriptionLength}/${maxDescriptionLength}`}
+      leftMessageColor={tooLength ? "error" : "textSecondary"}
+    >
+      <TextField
+        color="secondary"
+        label="Descrizione"
+        multiline
+        rows={6}
+        {...textFildProps}
+      />
+    </PickerWithErrorAndLabel>
+  );
+}
