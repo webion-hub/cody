492cc471446ba20c7a93178c9c28786f8d1959ea
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-04-30 20:17:41 +0200

    Create svg html parser

---
+++ cody/ClientApp/src/components/illustrations/svg_html_parser.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactHtmlParser, { processNodes } from 'react-html-parser';
+
+
+export const SvgHtmlParser = (svg) => {
+  return ReactHtmlParser(svg, {
+    transform: transformSvgNode,
+  });
+}
+
+
+/**
+ * @returns {SVGElement | undefined} 
+ */
+const transformSvgNode = (node) => {
+  if (!node.name)
+    return;
+  
+  maybeReplaceAttributes(node);
+
+  return <node.name {...node.attribs}>
+    {processNodes(node.children, transformSvgNode)}
+  </node.name>;
+}
+
+
+const maybeReplaceAttributes = (node) => {
+  if (!node.attribs)
+    return;
+
+  const replaceAttribute = (oldName, newName) => {
+    if (!(oldName in node.attribs))
+      return;
+
+    node.attribs[newName] = node.attribs[oldName];
+    delete node.attribs[oldName];    
+  }
+
+  replaceAttribute('viewbox', 'viewBox');
+  replaceAttribute('classname', 'className');
+}
