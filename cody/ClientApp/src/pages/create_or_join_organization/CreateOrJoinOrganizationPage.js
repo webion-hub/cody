2e7cab55fc5d5f24d8a131af6dd30e48284ea582
matteo2437 <matteo.budriesi@gmail.com>
2021-05-02 19:51:47 +0200

    refactoring create or join organization page

--- cody/ClientApp/src/pages/create_or_join_organization/CreateOrJoinOrganizationPage.js
+++ cody/ClientApp/src/pages/create_or_join_organization/CreateOrJoinOrganizationPage.js
@@ -1,67 +1,46 @@
 import React, { useEffect } from 'react';
 
 import { BackgroundWithLines } from 'src/components/background_with_lines/background_with_lines';
-
-import { selectActionSettings } from 'src/pages/create_or_join_organization/pages/select_action/select_action';
-
-import { createOrganizationSettings } from 'src/pages/create_or_join_organization/pages/create/create_organization';
-import { createTeamSettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_team';
-import { createSchoolSettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_school';
-import { createCompanySettings } from 'src/pages/create_or_join_organization/pages/create/pages/create_company';
-import { useOrganizationsInfoSettings } from 'src/pages/create_or_join_organization/pages/info/organizations_info';
-
-import { joinOrganizationSettings } from 'src/pages/create_or_join_organization/pages/join/join_organization';
 import { CenterComponentPageBase } from 'src/components/bases/center_component_page_base';
-
 import { TitleInfoContentBase } from 'src/components/bases/title_info_content_base/title_info_content_base';
 
 import { UserContext } from 'src/components/user_controller_context/user_controller_context';
 import { EventsDispatcher } from 'src/lib/events_dispatcher';
+import { useContentByHash } from './lib/use_content_by_hash';
+import { HashController } from '../../lib/hash_controller';
 
 
 export default function CreateOrJoinOrganization(){
-  const [contentSetting, setContentSetting] = React.useState(selectActionSettings);
-  const organizationsInfoSettings = useOrganizationsInfoSettings()
   const { userState } = React.useContext(UserContext);
+  const [hash, setHash] = React.useState('')
+  const content = useContentByHash(hash)
 
   useEffect(() => {
-    const hash = window.location.hash
-    const hashValue = hash.replace('#', '')
-    const isNotLogged = userState === "notLogged"
-    const wrongHashForNotLoggedUser = hashValue !== '' && hashValue !== 'info'
-
-    if(wrongHashForNotLoggedUser && isNotLogged){
-      EventsDispatcher.setEvent('openLoginDialog').update()
-      window.location.hash = '';
-      return;
-    }
-
-
-    setContentByHash(hashValue)
+    HashController
+      .setController()
+        .addForbiddenPage('join', userState !== "logged")
+        .addForbiddenPage('create', userState !== "logged")
+        .addForbiddenPage('createTeam', userState !== "logged")
+        .addForbiddenPage('createSchool', userState !== "logged")
+        .addForbiddenPage('createCompany', userState !== "logged")
+      .trySetHash()
+      .then(setHash)
+      .catch(openLoginDialog)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userState, window.location.hash])
 
-  const setContentByHash = (hash) => {
-    const action = {
-      'create': () => setContentSetting(createOrganizationSettings),
-      'createteam': () => setContentSetting(createTeamSettings),
-      'createschool': () => setContentSetting(createSchoolSettings),
-      'createcompany': () => setContentSetting(createCompanySettings),
-      'join': () => setContentSetting(joinOrganizationSettings),
-      'info': () => setContentSetting(organizationsInfoSettings),
-    }[hash];
-
-    action ? 
-      action() : setContentSetting(selectActionSettings); 
+  const openLoginDialog = () => {
+    EventsDispatcher.setEvent('openLoginDialog').update()
+    window.location.hash = '';
   }
 
   return(
     <CenterComponentPageBase>
       <TitleInfoContentBase
-        {...contentSetting}
+        {...content.settings}
       >
-        <contentSetting.component/>
+        <content.component/>
       </TitleInfoContentBase>
       <BackgroundWithLines/> 
     </CenterComponentPageBase>
