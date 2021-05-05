98b0d7224c903226e74d6659cc216f0980e781cd
matteo2437 <matteo.budriesi@gmail.com>
2021-05-05 21:46:14 +0200

    create user small summary dialog

---
+++ cody/ClientApp/src/components/user_summaries/user_small_summary_dialog.js
@@ -0,0 +1,35 @@
+import { DialogBase } from "src/components/bases/dialog_base";
+import { makeStyles } from '@material-ui/core/styles';
+import { UserSmallSummary } from "src/components/user_summaries/user_small_summary";
+
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    padding: "0px !important",
+  }
+}));
+
+export function UserSmallSummaryDialog(props){
+	const classes = useStyles();
+  const {
+    userData,
+    onClose,
+    callerIs,
+    handler,
+    onUserUpdate,
+  } = props
+
+  return(
+    <DialogBase
+      className={classes.dialog}
+      open={userData !== null}
+      onClose={onClose}
+    >
+      <UserSmallSummary
+        user={userData}
+        callerIs={callerIs}
+        handler={handler}
+        onUserUpdate={onUserUpdate}
+      />
+    </DialogBase>
+  )
+}
