7865688c4dd92018dcaecff5f4b78f542f0d3c16
matteo2437 <matteo.budriesi@gmail.com>
2021-05-08 16:31:46 +0200

    change icons

--- cody/ClientApp/src/components/list_items/user_list_items/user_list_item_with_showmore.js
+++ cody/ClientApp/src/components/list_items/user_list_items/user_list_item_with_showmore.js
@@ -3,8 +3,8 @@ import React from "react";
 import { IconButton } from "@material-ui/core";
 import { areEqual } from 'react-window';
 
-import FullscreenRoundedIcon from '@material-ui/icons/FullscreenRounded';
-import FullscreenExitRoundedIcon from '@material-ui/icons/FullscreenExitRounded';
+import MoreRoundedIcon from '@material-ui/icons/MoreRounded';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 
 import { UserListItem } from "./user_list_item";
 import { ListActiveIdsContext } from "src/components/lists/list_with_active_ids";
@@ -32,8 +32,8 @@ export const UserListItemWithShowMore = React.memo((props) => {
         >
           {
             isActive 
-              ? <FullscreenExitRoundedIcon/> 
-              : <FullscreenRoundedIcon/>
+              ? <CloseRoundedIcon/> 
+              : <MoreRoundedIcon/>
           }          
         </IconButton>
       }
