b52bc5d15bb34443ff7da7093d1547c5ff9a1b3b
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-05-16 01:00:53 +0200

    Add course query extensions

---
+++ cody/QueryExtensions/Course.Formatters.cs
@@ -0,0 +1,20 @@
+using Cody.Controllers.Responses.Formatters;
+using Cody.Models.Organizations.Courses;
+using Cody.Models.Users;
+using System.Linq;
+
+namespace Cody.QueryExtensions
+{
+  public static partial class OrganizationQueries
+    {
+        public static IQueryable<object> Format(this IQueryable<Course> self) 
+        {
+            return self.Select(c => new 
+            {
+                c.Id,
+                c.Title,
+                c.Description,
+            });
+        }
+    }
+}
