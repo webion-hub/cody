1c1bd0a38c38a50682aa1a3c7e2f2dde0cf9c866
Stefano Calabretti <StefanoCalabretti@outlook.it>
2021-05-16 11:47:17 +0200

    Add organization get /courses

---
+++ cody/Controllers/Organizations/Organization/GetCourses.cs
@@ -0,0 +1,44 @@
+using Cody.Extensions;
+using Cody.QueryExtensions;
+using Cody.Utilities.QueryFilters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cody.Controllers.Organizations
+{
+    public partial class OrganizationController
+    {
+        [HttpGet("courses")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetCourses(
+            [FromRoute] int organizationId,
+            [FromQuery] string filter,
+            [FromQuery] int? limit,
+            [FromQuery] int? offset
+        ) {
+            var members = _dbContext
+                .Organizations
+                .ThatArePublic()
+                .ThatHaveNotBeenDeleted()
+                .Where(o => o.Id == organizationId)
+
+                .Include(o => o.Courses)
+                .SelectMany(o => o.Courses)
+                
+                .CreateFilter(filter, FilterKind.SplitWords)
+                .DefaultMatch()
+                .Format();
+
+            var result = await SearchResult.FormatAsync(
+                results: members,
+                limit: limit,
+                offset: offset
+            );
+
+            return Ok(result);
+        }
+    }
+}
