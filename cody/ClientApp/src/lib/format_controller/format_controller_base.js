b987123defb707500c4f69a30f1288256fa59fc6
matteo2437 <matteo.budriesi@gmail.com>
2021-05-18 21:47:49 +0200

    add wrong length to format controller base

--- cody/ClientApp/src/lib/format_controller/format_controller_base.js
+++ cody/ClientApp/src/lib/format_controller/format_controller_base.js
@@ -1,6 +1,14 @@
+import { FormatLengthController } from "./utilities/format_length_controller";
+
 export class FormatControllerBase {
   static canSkip = (data, skip) => {
     const areNotData = data.length === 0
     return skip && areNotData 
+  }
+
+  static wrongLength= (val, what, skip = false) => {
+    return FormatLengthController
+      .set(what)
+      .wrongFormat(val, {skippable: skip});
   }
 }
