6d67c692553bc91e9f929db3a1459063154d80a9
matteo2437 <matteo.budriesi@gmail.com>
2021-05-23 19:09:07 +0200

    create info box

---
+++ cody/ClientApp/src/components/info/info_box.js
@@ -0,0 +1,74 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { VerifiedLabel } from "src/components/typography/verified_label";
+import { TypographyWithLoading } from "src/components/typography/typography_with_loading";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.down('xs')]: {
+      marginRight: 0,
+      "& > *": {
+        margin: "0 auto"
+      }
+    },
+  },
+  centerText: {
+    [theme.breakpoints.down('xs')]: {
+      "& > *": {
+        margin: "0 auto"
+      }
+    }, 
+  },
+  joinButton: {
+    marginTop: theme.spacing(1)
+  },
+  bookmarkButton: {
+    marginBottom: 2
+  },
+  shiftLeft: {
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: 48
+    }, 
+  }
+}));
+
+export function InfoBox(props){
+	const classes = useStyles();
+  const { 
+    className,
+    leftIcon,
+    title,
+    subTitle,
+    verified,
+    button,
+    loading,
+   } = props
+
+  return (
+    <div className={className}>
+      <TypographyWithLoading
+        className={`${classes.title} ${classes.centerText}`}
+        variant="h5"
+        noWrap
+        loading={loading}
+      >
+        {leftIcon}
+        <VerifiedLabel
+          label={title}
+          verified={verified}
+          iconSize={22}
+          translateIconY={0}
+        />
+      </TypographyWithLoading>
+      <TypographyWithLoading
+        variant="caption"
+        noWrap
+        loading={loading}
+        className={`${classes.centerText} ${subTitle ? classes.shiftLeft : ""}`}
+      >
+        {subTitle}
+      </TypographyWithLoading>
+      {button}
+    </div>
+  )
+}
