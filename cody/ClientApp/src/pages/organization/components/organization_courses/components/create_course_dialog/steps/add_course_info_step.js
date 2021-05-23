f28768338981c858923f19d038dc65d61842bb0e
matteo2437 <matteo.budriesi@gmail.com>
2021-05-23 17:32:18 +0200

    add next focus to add course info step

--- cody/ClientApp/src/pages/organization/components/organization_courses/components/create_course_dialog/steps/add_course_info_step.js
+++ cody/ClientApp/src/pages/organization/components/organization_courses/components/create_course_dialog/steps/add_course_info_step.js
@@ -6,6 +6,7 @@ import { BasePhotoText } from "src/components/bases/base_photo_text";
 
 import { DescriptionTextField } from "src/components/pickers/text_fields/types/description_text_field";
 import { School } from "src/components/illustrations/illustrations/illustrations";
+import { NextFocus } from 'src/lib/next_focus';
 
 const useStyles = makeStyles((theme) => ({
   description: {
@@ -18,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AddCourseInfoStep(props){
 	const classes = useStyles();
+  const nextFocus = new NextFocus(["title", "description"]);
 
   return (
     <BasePhotoText image={School}>
@@ -34,6 +36,8 @@ export default function AddCourseInfoStep(props){
         onChange={props.onTitleChange}
         error={props.errors.courseTitleError}
         defaultValue={props.values.title}
+        inputRef={nextFocus.getInput("title")} 
+        onKeyDown={nextFocus.enterPressedFocusOn("description")}
       />
       <DescriptionTextField
         className={classes.description}
@@ -43,6 +47,8 @@ export default function AddCourseInfoStep(props){
         error={props.errors.descriptionError}
         defaultValue={props.values.description}
         descriptionLength={props.values.description.length}
+        inputRef={nextFocus.getInput("description")} 
+        onKeyDown={nextFocus.removeFocus}
       />
     </BasePhotoText>
   )
