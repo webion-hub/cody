1041c4538538500f8e2568ddcd339c11a858f7ab
matteo2437 <matteo.budriesi@gmail.com>
2021-05-27 17:58:15 +0200

    implement list with pages

--- cody/ClientApp/src/pages/organization/components/organization_courses/organization_courses.js
+++ cody/ClientApp/src/pages/organization/components/organization_courses/organization_courses.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-
-import { Fade, Grid, LinearProgress } from "@material-ui/core";
 
 import { OrganizationContext } from "../../organization_controller_context";
 import { CourseAccordion } from "src/components/accordions/course_accordion/course_accordion";
 
-import { useMobileView } from "src/lib/hooks/use_mobile_view";
-import { SearchBarWithPageController } from "src/components/pickers/search_bars/search_bar_with_page_controller/search_bar_with_page_controller";
-import { usePageController } from "src/lib/hooks/use_page_controller";
 import { AddCourseButton } from "../../../../components/buttons/add_course_button";
+import { ListWithPages } from "src/components/lists/list_with_pages";
 
 const useStyles = makeStyles((theme) => ({
 	coursesBox: {
@@ -32,81 +27,26 @@ const useStyles = makeStyles((theme) => ({
 
 export function OrganizationCourses(){
 	const classes = useStyles();
-  const mobileView = useMobileView()
   
 	const {
     organization,
     callerIs,
 	} = React.useContext(OrganizationContext);
 
-	const pageController = usePageController({
-		maxPageElements: 5,
-		getData: organization
-      .courses
-      .listAll
-	})
-
-	const {
-		next,
-		back,
-		handleChange,
-		loading,
-		dataList: courses
-	} = pageController
-
-
-  const getCourses = () => {
-    if(loading)
-      return;
-      
-    if(courses.length === 0)
-      return (
-        <Typography>
-          Nessun corso trovato.
-        </Typography>
-      )
-
-    return courses.map((course, index) => 
-      <CourseAccordion
-        index={index}
-        key={index}
-        data={course}
-      />
-    )
-  }
-
   return (
     <div className={classes.coursesBox}>
-      <Grid
-        className={classes.coursesTitle}
-        container
-        direction="row"
-        alignItems="center"
-      >
-        <Typography
-          variant={mobileView ? "h5" : "h4"}
-          noWrap
-        >
-          Corsi Disponibili
-        </Typography>
-        <AddCourseButton
-          callerIs={callerIs}
-        />
-      </Grid>
-      <SearchBarWithPageController 
-        className={classes.searchBar}
-        onChange={handleChange}
-        onBack={back.handle}
-        onNext={next.handle}
-        disableBack={back.disable}
-        disableNext={next.disable}
+      <ListWithPages      
+        maxPageElements={5}
+        title="Corsi Disponibili"
+        notFoundMessage="Nessun corso trovato."
+        getData={organization.courses.listAll}
+        listItem={CourseAccordion}
+        rightIcon={
+          <AddCourseButton
+            callerIs={callerIs}
+          />
+        }
       />
-      <Fade 
-        className={classes.linearProgress}
-        in={loading}>
-        <LinearProgress color="secondary"/>
-      </Fade>
-      {getCourses()}
     </div>
   );
 }
