594eea51ff3ea241366befe1eb5f232ca34c3b7e
matteo2437 <matteo.budriesi@gmail.com>
2021-06-01 18:25:27 +0200

    update path

--- cody/ClientApp/src/pages/organization/components/organization_courses/organization_courses.js
+++ cody/ClientApp/src/pages/organization/components/organization_courses/organization_courses.js
@@ -4,8 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { OrganizationContext } from "../../organization_controller_context";
 import { CourseAccordion } from "src/components/accordions/course_accordion/course_accordion";
 
-import { AddCourseButton } from "../../../../components/buttons/add_course_button";
 import { ListWithPages } from "src/components/lists/list_with_pages";
+import { AddCourseButton } from "src/components/buttons/add_course_button";
 
 const useStyles = makeStyles((theme) => ({
 	coursesBox: {
