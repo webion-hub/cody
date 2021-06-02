a202290ab4da845134fc304739fdabdd8f74b98f
matteo2437 <matteo.budriesi@gmail.com>
2021-06-02 15:46:55 +0200

    update course page

--- cody/ClientApp/src/pages/course/Course.js
+++ cody/ClientApp/src/pages/course/Course.js
@@ -9,12 +9,15 @@ import CustomTreeView from "src/components/others/custom_treeview";
 import Editor from "@monaco-editor/react";
 import { TabContent } from "src/components/bases/layouts/tab_content";
 
-import TooltipAvatarLink from "src/components/avatars/tooltip_avatar_link";
 import { IconButton, Tab, Tabs, Tooltip, useTheme } from "@material-ui/core";
 import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 import { PageController } from "src/lib/page_controller";
 import { BackgroundWithLines } from "src/components/others/background_with_lines/background_with_lines";
 import { ListWithPages } from "src/components/lists/list_with_pages";
+import { LinkMenu } from "src/components/typography/link_menu";
+import { MenuWithWaves } from "src/components/menu/menus/menu_with_waves";
+import { UserSmallSummary } from "src/components/user_summaries/user_small_summary";
+import { User } from "src/lib/server_calls/user";
 
 const useStyles = makeStyles((theme) => ({
   lessonBox: {
@@ -30,6 +33,7 @@ export default function Course(){
   const { organizationId, courseId } = useParams();
   const [courseData, setCourseData] = React.useState()
   const [loading, setLoading] = React.useState(false)
+  const [callerIs, setCallerIs] = React.useState("noMember")
   const [tab, setTab] = React.useState(0);
 
   const organization = Organization
@@ -44,7 +48,17 @@ export default function Course(){
       .getInfo()
       .then(setCourseData)
       .finally(_ => setLoading(false))
+
+    getRole()
   }, [])
+
+  const getRole = async () => {
+    await User
+      .getRoleIn(organizationId)
+      .then(role => setCallerIs(role ?? "noMember"))
+  }
+
+  console.log(courseData?.teachers)
 
   return(
     <MultipleSectionsBase
@@ -56,12 +70,21 @@ export default function Course(){
             data={{
               title: courseData?.title,
               subTitle: courseData?.teachers?.map((teacher, index) => 
-                <>
+                <React.Fragment key={index}>
                   {index !== 0 ? ", " : "Professori: "}
-                  <TooltipAvatarLink
-                    user={teacher}
-                  />
-                </>
+                  <LinkMenu
+                    MenuComponent={MenuWithWaves}
+                    menuContent={
+                      <UserSmallSummary
+                        user={teacher}
+                        callerIs={callerIs}
+                        handler={organization}
+                      />
+                    }
+                  >
+                    {teacher.username}
+                  </LinkMenu>
+                </React.Fragment>
               )
             }}
             leftIcon={
